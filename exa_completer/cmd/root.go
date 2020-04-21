608014f40a2b34921a0cf82245b691269b97fa86
rsteube <rsteube@users.noreply.github.com>
2020-04-21 20:28:05 +0200

    updated

--- exa_completer/cmd/root.go
+++ exa_completer/cmd/root.go
@@ -2,64 +2,15 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
-	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "exa",
-}
-
-func Execute() error {
-	return rootCmd.Execute()
-}
-
 func init() {
-    // DISPLAY OPTIONS
-	rootCmd.Flags().BoolP("online", "1", false, "display one entry per line")
-	rootCmd.Flags().BoolP("long", "l", false, "display extended file metadata as table")
-	rootCmd.Flags().BoolP("grid", "G", false, "display entries as grid (default)")
-	rootCmd.Flags().BoolP("across", "x", false, "sort the grid across, rather than downwards")
-	rootCmd.Flags().BoolP("recurse", "R", false, "recurse into directories")
-	rootCmd.Flags().BoolP("tree", "T", false, "recurse inte directories as tree")
-	rootCmd.Flags().BoolP("classify", "F", false, "display type indicator by file names")
-	rootCmd.Flags().String("color", "", "when to use terminal colours (always, auto, never)")
-	rootCmd.Flags().String("colour", "", "when to use terminal colours (always, auto, never)")
-	rootCmd.Flags().String("color-scale", "", "highlight levels of file sizes distinctly")
-	rootCmd.Flags().String("colour-scale", "", "highlight levels of file sizes distinctly")
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		//"color":  carapace.ActionValues("always", "auto", "never"),
+		//"colour": carapace.ActionValues("always", "auto", "never"),
+		"sort":   carapace.ActionValues("name", "Name", "size", "extension", "Extension", "modified", "changed", "accessed", "created", "inode", "type", "none"),
+		"time":   carapace.ActionValues("modified", "accessed", "created"),
+	})
 
-    // FILTERING AND SORTING OPTIONS
-    rootCmd.Flags().BoolP("all", "a", false, "show hidden and 'dot' files")
-    rootCmd.Flags().BoolP("list-dirs", "d", false, "list directories like regular files")
-    rootCmd.Flags().Int32P("level", "L", 0, "limit the depth of recursion")
-    rootCmd.Flags().BoolP("reverse", "r", false, "reverse the sort order")
-    rootCmd.Flags().StringP("sort", "s", "", "which field to sort by")
-    rootCmd.Flags().Bool("group-directories-first", false, "list directories before other files")
-    rootCmd.Flags().BoolP("only-dirs", "D", false, "list only directories")
-    rootCmd.Flags().StringP("ignore-glob", "I", "", "glob patterns (pipe-separated) of files to ignore")
-    rootCmd.Flags().Bool("git-ignore", false, "Ignore files mentioned in '.gitignore'")
-
-    // LONG VIEW OPTIONS
-    rootCmd.Flags().BoolP("binary", "b", false, "list file sizes with binary prefixes")
-    rootCmd.Flags().BoolP("bytes", "B", false, "list file sizes in bytes, without any prefixes")
-    rootCmd.Flags().BoolP("group", "g", false, "list each file's group")
-    rootCmd.Flags().BoolP("header", "h", false, "add a header row to each column")
-    rootCmd.Flags().BoolP("links", "H", false, "list each file's number of hard links")
-    rootCmd.Flags().BoolP("inode", "i", false, "list each file's inode number")
-    rootCmd.Flags().BoolP("modified", "m", false, "use the modified timestamp field")
-    rootCmd.Flags().BoolP("blocks", "S", false, "show number of file system blocks")
-    rootCmd.Flags().StringP("time", "t", "", "which timestamp field to list (modified, accessed, created)")
-    rootCmd.Flags().BoolP("accessed", "u", false, "use the accessed timestamp field")
-    rootCmd.Flags().BoolP("created", "U", false, "use the created timestamp field")
-    rootCmd.Flags().Bool("time-style", false, "use the created timestamp field")
-    rootCmd.Flags().Bool("git", false, "list each file's Git status, if tracked or ignored")
-    rootCmd.Flags().BoolP("extended", "@", false, "list each file's extended attributes and sizes")
-
-    carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
-      "color": carapace.ActionValues("always", "auto", "never"),
-      "colour": carapace.ActionValues("always", "auto", "never"),
-      "sort" : carapace.ActionValues("name", "Name", "size", "extension", "Extension", "modified", "changed", "accessed", "created", "inode", "type", "none"),
-      "time": carapace.ActionValues("modified", "accessed", "created"),
-    })
-
-    carapace.Gen(rootCmd).Standalone()
+	carapace.Gen(rootCmd).Standalone()
 }
---
+++ exa_completer/cmd/root_generated.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "",
+	Short: "",
+    Run: func(cmd *cobra.Command, args []string) {
+    },
+}
+
+func Execute() error {
+    return rootCmd.Execute()
+}
+
+func init() {
+	rootCmd.Flags().BoolP("accessed", "u", false, "use the accessed timestamp field")
+	rootCmd.Flags().BoolP("across", "x", false, "sort the grid across, rather than downwards")
+	rootCmd.Flags().BoolP("all", "a", false, "show hidden and 'dot' files")
+	rootCmd.Flags().BoolP("binary", "b", false, "list file sizes with binary prefixes")
+	rootCmd.Flags().BoolP("blocks", "S", false, "show number of file system blocks")
+	rootCmd.Flags().BoolP("bytes", "B", false, "list file sizes in bytes, without any prefixes")
+	rootCmd.Flags().BoolP("classify", "F", false, "display type indicator by file names")
+	rootCmd.Flags().String("colo[u]r", "", "when to use terminal colours (always, auto, never)")
+	rootCmd.Flags().Bool("colo[u]r-scale", false, "highlight levels of file sizes distinctly")
+	rootCmd.Flags().BoolP("created", "U", false, "use the created timestamp field")
+	rootCmd.Flags().BoolP("extended", "@", false, "list each file's extended attributes and sizes")
+	rootCmd.Flags().Bool("git", false, "list each file's Git status, if tracked or ignored")
+	rootCmd.Flags().Bool("git-ignore", false, "Ignore files mentioned in '.gitignore'")
+	rootCmd.Flags().BoolP("grid", "G", false, "display entries as a grid (default)")
+	rootCmd.Flags().BoolP("group", "g", false, "list each file's group")
+	rootCmd.Flags().Bool("group-directories-first", false, "list directories before other files")
+	rootCmd.Flags().BoolP("header", "h", false, "add a header row to each column")
+	rootCmd.Flags().BoolP("help", "?", false, "show list of command-line options")
+	rootCmd.Flags().StringP("ignore-glob", "I", "", "glob patterns (pipe-separated) of files to ignore")
+	rootCmd.Flags().BoolP("inode", "i", false, "list each file's inode number")
+	rootCmd.Flags().StringP("level", "L", "", "limit the depth of recursion")
+	rootCmd.Flags().BoolP("links", "H", false, "list each file's number of hard links")
+	rootCmd.Flags().BoolP("list-dirs", "d", false, "list directories like regular files")
+	rootCmd.Flags().BoolP("long", "l", false, "display extended file metadata as a table")
+	rootCmd.Flags().BoolP("modified", "m", false, "use the modified timestamp field")
+	rootCmd.Flags().BoolP("oneline", "1", false, "display one entry per line")
+	rootCmd.Flags().BoolP("only-dirs", "D", false, "list only directories")
+	rootCmd.Flags().BoolP("recurse", "R", false, "recurse into directories")
+	rootCmd.Flags().BoolP("reverse", "r", false, "reverse the sort order")
+	rootCmd.Flags().StringP("sort", "s", "", "which field to sort by")
+	rootCmd.Flags().StringP("time", "t", "", "which timestamp field to list (modified, accessed, created)")
+	rootCmd.Flags().Bool("time-style", false, "how to format timestamps (default, iso, long-iso, full-iso)")
+	rootCmd.Flags().BoolP("tree", "T", false, "recurse into directories as a tree")
+	rootCmd.Flags().BoolP("version", "v", false, "show version of exa")
+}
