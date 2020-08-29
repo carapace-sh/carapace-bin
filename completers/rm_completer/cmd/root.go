0b93000b0afaea9938fc93c8afd1ebfbc9531f69
rsteube <rsteube@users.noreply.github.com>
2020-08-29 20:58:50 +0200

    fixed rm

--- completers/rm_completer/cmd/root.go
+++ completers/rm_completer/cmd/root.go
@@ -17,17 +17,24 @@ func Execute() error {
 func init() {
 	carapace.Gen(rootCmd).Standalone()
 
-	rootCmd.Flags().BoolP("I", "I", false, "prompt once before removing more than three files, or when removing recursively")
+	rootCmd.Flags().BoolS("I", "I", false, "prompt once before removing more than three files, or when removing recursively")
 	rootCmd.Flags().BoolP("dir", "d", false, "remove empty directories")
 	rootCmd.Flags().BoolP("force", "f", false, "ignore nonexistent files and arguments, never prompt")
 	rootCmd.Flags().Bool("help", false, "display this help and exit")
-	rootCmd.Flags().BoolP("i", "i", false, "prompt before every removal")
+	rootCmd.Flags().BoolS("i", "i", false, "prompt before every removal")
 	rootCmd.Flags().Bool("interactive", false, "prompt according to WHEN: never, once (-I), or always (-i); without WHEN, prompt always")
 	rootCmd.Flags().Bool("no-preserve-root", false, "do not treat '/' specially")
 	rootCmd.Flags().Bool("one-file-system", false, "when removing a hierarchy recursively, skip any directory that is on a file system different from that of the corresponding command line argument")
 	rootCmd.Flags().String("preserve-root", "", "do not remove '/' (default); with 'all', reject any command line argument on a separate device from its parent")
 	rootCmd.Flags().BoolP("verbose", "v", false, "explain what is being done")
 	rootCmd.Flags().Bool("version", false, "output version information and exit")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"interactive": carapace.ActionValuesDescribed(
+			"always", "prompt before every removal",
+			"once", "prompt when removing many files",
+		),
+	})
 
 	carapace.Gen(rootCmd).PositionalAnyCompletion(
 		carapace.ActionFiles(""),
