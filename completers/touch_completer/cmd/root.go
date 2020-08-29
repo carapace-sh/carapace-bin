76d7bdaf1ac5721198093c7d32139e17ae88b741
rsteube <rsteube@users.noreply.github.com>
2020-08-29 21:42:01 +0200

    fixed touch

--- completers/touch_completer/cmd/root.go
+++ completers/touch_completer/cmd/root.go
@@ -17,15 +17,15 @@ func Execute() error {
 func init() {
 	carapace.Gen(rootCmd).Standalone()
 
-	rootCmd.Flags().BoolP("a", "a", false, "change only the access time")
+	rootCmd.Flags().BoolS("a", "a", false, "change only the access time")
 	rootCmd.Flags().StringP("date", "d", "", "parse STRING and use it instead of current time")
-	rootCmd.Flags().BoolP("f", "f", false, "(ignored)")
+	rootCmd.Flags().BoolS("f", "f", false, "(ignored)")
 	rootCmd.Flags().Bool("help", false, "display this help and exit")
-	rootCmd.Flags().BoolP("m", "m", false, "change only the modification time")
+	rootCmd.Flags().BoolS("m", "m", false, "change only the modification time")
 	rootCmd.Flags().BoolP("no-create", "c", false, "do not create any files")
 	rootCmd.Flags().BoolP("no-dereference", "h", false, "affect each symbolic link instead of any referenced file")
 	rootCmd.Flags().StringP("reference", "r", "", "use this file's times instead of current time")
-	rootCmd.Flags().StringP("t", "t", "", "use [[CC]YY]MMDDhhmm[.ss] instead of current time")
+	rootCmd.Flags().StringS("t", "t", "", "use [[CC]YY]MMDDhhmm[.ss] instead of current time")
 	rootCmd.Flags().String("time", "", "change the specified time")
 	rootCmd.Flags().Bool("version", false, "output version information and exit")
 
@@ -33,4 +33,6 @@ func init() {
 		"reference": carapace.ActionFiles(""),
 		"time":      carapace.ActionValues("access", "atime", "modify", "mtime", "use"),
 	})
+
+    carapace.Gen(rootCmd).PositionalAnyCompletion(carapace.ActionFiles(""))
 }
