f4323bc72989c384a8c896b64e89f014b3bd6c2b
rsteube <rsteube@users.noreply.github.com>
2020-08-31 13:36:04 +0200

    added seq sleep and stat

---
+++ completers/seq_completer/cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "seq",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("equal-width", "w", false, "equalize width by padding with leading zeroes")
+	rootCmd.Flags().StringP("format", "f", "", "use printf style floating-point FORMAT")
+	rootCmd.Flags().Bool("help", false, "display this help and exit")
+	rootCmd.Flags().StringP("separator", "s", "", "use STRING to separate numbers (default: \n)")
+	rootCmd.Flags().Bool("version", false, "output version information and exit")
+}
