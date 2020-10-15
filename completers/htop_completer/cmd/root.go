3c1f2d9cc1e83689a0f0ad1480deb9fc290a08d9
rsteube <rsteube@users.noreply.github.com>
2020-10-15 23:58:10 +0200

    added htop

---
+++ completers/htop_completer/cmd/root.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/os"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "htop",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().StringP("delay", "d", "", "Set the delay between updates, in tenths of seconds")
+	rootCmd.Flags().BoolP("help", "h", false, "Print this help screen")
+	rootCmd.Flags().BoolP("no-color", "C", false, "Use a monochrome color scheme")
+	rootCmd.Flags().BoolP("no-mouse", "m", false, "Disable the mouse")
+	rootCmd.Flags().BoolP("no-unicode", "U", false, "Do not use unicode but plain ASCII")
+	rootCmd.Flags().StringP("pid", "p", "", "Show only the given PIDs")
+	rootCmd.Flags().StringP("sort-key", "s", "", "Sort by COLUMN (try --sort-key=help for a list)")
+	rootCmd.Flags().BoolP("tree", "t", false, "Show the tree view by default")
+	rootCmd.Flags().StringP("user", "u", "", "Show only processes for a given user (or $USER)")
+	rootCmd.Flags().BoolP("version", "v", false, "Print version info")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"pid": carapace.ActionMultiParts(",", func(args, parts []string) carapace.Action {
+			return os.ActionProcessIds().Invoke(args).Filter(parts).ToA()
+		}),
+		"sort-key": ActionSortKeys(),
+		"user":     os.ActionUsers(),
+	})
+}
+
+func ActionSortKeys() carapace.Action {
+	return carapace.ActionCallback(func(args []string) carapace.Action {
+		if output, err := exec.Command("htop", "--sort-key", "help").Output(); err != nil {
+			return carapace.ActionMessage(err.Error())
+		} else {
+			lines := strings.Split(string(output), "\n")
+			return carapace.ActionValues(lines[:len(lines)-1]...)
+		}
+	})
+}
---
+++ completers/htop_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/htop_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
