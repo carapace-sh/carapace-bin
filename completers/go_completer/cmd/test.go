3f2dbada839a80921c1c2410f61dba5d12da6394
rsteube <rsteube@users.noreply.github.com>
2020-11-03 22:59:29 +0100

    added go test cacheable flags

--- completers/go_completer/cmd/test.go
+++ completers/go_completer/cmd/test.go
@@ -16,9 +16,15 @@ func init() {
 
 	testCmd.Flags().Bool("args", false, "pass the remainder of the command line to the test binary")
 	testCmd.Flags().BoolS("c", "c", false, "compile the test binary to pkg.test but do not run it")
+	testCmd.Flags().Bool("cpu", false, "cacheable flag")
+	testCmd.Flags().String("exec", "", "run the test binary using xprog")
 	testCmd.Flags().BoolS("i", "i", false, "install packages that are dependencies of the test")
 	testCmd.Flags().Bool("json", false, "convert test output to JSON")
+	testCmd.Flags().Bool("list", false, "cacheable flag")
 	testCmd.Flags().StringS("o", "o", "", "compile the test binary to the named file")
-	testCmd.Flags().String("exec", "", "run the test binary using xprog")
+	testCmd.Flags().Bool("parallel", false, "cacheable flag")
+	testCmd.Flags().Bool("run", false, "cacheable flag")
+	testCmd.Flags().Bool("short", false, "cacheable flag")
+	testCmd.Flags().Bool("v", false, "cacheable flag")
 	rootCmd.AddCommand(testCmd)
 }
