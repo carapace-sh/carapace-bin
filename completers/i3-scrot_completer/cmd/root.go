acdfa9fad232ca61f5879e21bda6328a3c16cd4f
rsteube <rsteube@users.noreply.github.com>
2020-11-18 11:16:02 +0100

    added i3-scrot

---
+++ completers/i3-scrot_completer/cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "i3-scrot",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("desk", "d", false, "full screen")
+	rootCmd.Flags().BoolP("help", "h", false, "display this information")
+	rootCmd.Flags().BoolP("select", "s", false, "selection")
+	rootCmd.Flags().BoolP("window", "w", false, "active window")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionCallback(func(args []string) carapace.Action {
+			if rootCmd.Flag("desk").Changed || rootCmd.Flag("window").Changed {
+				return carapace.ActionValues("1", "2", "3", "4", "5")
+			} else {
+				return carapace.ActionValues()
+			}
+		}),
+	)
+}
