49a608db5139438ba29c39d417ffebcfb48528cb
rsteube <rsteube@users.noreply.github.com>
2020-11-20 17:08:09 +0100

    added resume-cli

---
+++ completers/resume-cli_completer/cmd/test.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var testCmd = &cobra.Command{
+	Use:   "test",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(testCmd).Standalone()
+	testCmd.Flags().BoolP("help", "h", false, "display help for command")
+	testCmd.Flags().String("schema", "", "validate against common schema")
+
+	rootCmd.AddCommand(testCmd)
+
+	carapace.Gen(testCmd).FlagCompletion(carapace.ActionMap{
+		"schema": carapace.ActionFiles(""),
+	})
+
+	carapace.Gen(testCmd).PositionalCompletion(
+		carapace.ActionValues("init", "test", "export", "serve"),
+	)
+}
