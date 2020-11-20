fe8866390ce4e86dec18d59bda4de079d880e335
rsteube <rsteube@users.noreply.github.com>
2020-11-20 20:07:59 +0100

    rm : fix flag completion

--- completers/rm_completer/cmd/root.go
+++ completers/rm_completer/cmd/root.go
@@ -22,18 +22,22 @@ func init() {
 	rootCmd.Flags().BoolP("force", "f", false, "ignore nonexistent files and arguments, never prompt")
 	rootCmd.Flags().Bool("help", false, "display this help and exit")
 	rootCmd.Flags().BoolS("i", "i", false, "prompt before every removal")
-	rootCmd.Flags().Bool("interactive", false, "prompt according to WHEN: never, once (-I), or always (-i); without WHEN, prompt always")
+	rootCmd.Flags().String("interactive", "", "prompt according to WHEN: never, once (-I), or always (-i); without WHEN, prompt always")
 	rootCmd.Flags().Bool("no-preserve-root", false, "do not treat '/' specially")
 	rootCmd.Flags().Bool("one-file-system", false, "when removing a hierarchy recursively, skip any directory that is on a file system different from that of the corresponding command line argument")
 	rootCmd.Flags().String("preserve-root", "", "do not remove '/' (default); with 'all', reject any command line argument on a separate device from its parent")
 	rootCmd.Flags().BoolP("verbose", "v", false, "explain what is being done")
 	rootCmd.Flags().Bool("version", false, "output version information and exit")
 
+	rootCmd.Flag("interactive").NoOptDefVal = " "
+	rootCmd.Flag("preserve-root").NoOptDefVal = " "
+
 	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
 		"interactive": carapace.ActionValuesDescribed(
 			"always", "prompt before every removal",
 			"once", "prompt when removing many files",
 		),
+		"preserve-root": carapace.ActionValues("all"),
 	})
 
 	carapace.Gen(rootCmd).PositionalAnyCompletion(
