197d8fd708bcf1921ca848a178c5ac545abf02a3
rsteube <rsteube@users.noreply.github.com>
2020-11-21 09:25:32 +0100

    added bats

---
+++ completers/bats_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "bats",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("count", "c", false, "Count the number of test cases without running any tests")
+	rootCmd.Flags().BoolP("help", "h", false, "Display this help message")
+	rootCmd.Flags().BoolP("pretty", "p", false, "Show results in pretty format (default for terminals)")
+	rootCmd.Flags().BoolP("tap", "t", false, "Show results in TAP format")
+	rootCmd.Flags().BoolP("version", "v", false, "Display the version number")
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(carapace.ActionFiles(""))
+}
