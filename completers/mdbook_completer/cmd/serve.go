2a10bb747b2bf93908a1a50cc8fa17de5b1d0d0e
rsteube <rsteube@users.noreply.github.com>
2020-11-27 10:19:43 +0100

    added mdbook

---
+++ completers/mdbook_completer/cmd/serve.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(serveCmd).Standalone()
+
+	serveCmd.Flags().StringP("dest-dir", "d", "", "Output directory for the book")
+	serveCmd.Flags().BoolP("help", "h", false, "Prints help information")
+	serveCmd.Flags().StringP("hostname", "n", "", "Hostname to listen on for HTTP connections [default: localhost]")
+	serveCmd.Flags().BoolP("open", "o", false, "Opens the book server in a web browser")
+	serveCmd.Flags().StringP("port", "p", "", "Port to use for HTTP connections [default: 3000]")
+	serveCmd.Flags().BoolP("version", "V", false, "Prints version information")
+	rootCmd.AddCommand(serveCmd)
+
+	carapace.Gen(serveCmd).FlagCompletion(carapace.ActionMap{
+		"dest-dir": carapace.ActionDirectories(),
+		"hostname": carapace.ActionValues("localhost", "0.0.0.0"),
+	})
+
+	carapace.Gen(serveCmd).PositionalCompletion(
+		carapace.ActionDirectories(),
+	)
+}
---
+++ completers/mdbook_completer/cmd/watch.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var watchCmd = &cobra.Command{
+	Use:   "watch",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(watchCmd).Standalone()
+
+	watchCmd.Flags().StringP("dest-dir", "d", "", "Output directory for the book")
+	watchCmd.Flags().BoolP("help", "h", false, "Prints help information")
+	watchCmd.Flags().BoolP("open", "o", false, "Open the compiled book in a web browser")
+	watchCmd.Flags().BoolP("version", "V", false, "Prints version information")
+	rootCmd.AddCommand(watchCmd)
+
+	carapace.Gen(watchCmd).FlagCompletion(carapace.ActionMap{
+		"dest-dir": carapace.ActionDirectories(),
+	})
+
+	carapace.Gen(watchCmd).PositionalCompletion(
+		carapace.ActionDirectories(),
+	)
+}
