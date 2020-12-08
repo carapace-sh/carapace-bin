72f36bedac23cda732e615e41764f8a5056cf855
rsteube <rsteube@users.noreply.github.com>
2020-12-08 03:12:00 +0100

    kubectl: add create serviceaccount

---
+++ completers/kubectl_completer/cmd/create_serviceaccount.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/completers/kubectl_completer/cmd/action"
+	"github.com/spf13/cobra"
+)
+
+var create_serviceaccountCmd = &cobra.Command{
+	Use:   "serviceaccount",
+	Short: "Create a service account with the specified name",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(create_serviceaccountCmd).Standalone()
+
+	create_serviceaccountCmd.Flags().Bool("allow-missing-template-keys", false, "If true, ignore any errors in templates when a field or map key is missing in the template. Only app")
+	create_serviceaccountCmd.Flags().String("dry-run", "", "Must be \"none\", \"server\", or \"client\". If client strategy, only print the object that would be sent,")
+	create_serviceaccountCmd.Flags().String("field-manager", "", "Name of the manager used to track field ownership.")
+	create_serviceaccountCmd.Flags().String("generator", "", "The name of the API generator to use.")
+	create_serviceaccountCmd.Flags().StringP("output", "o", "", "Output format. One of: json|yaml|name|go-template|go-template-file|template|templatefile|jsonpath|js")
+	create_serviceaccountCmd.Flags().Bool("save-config", false, "If true, the configuration of current object will be saved in its annotation. Otherwise, the annotat")
+	create_serviceaccountCmd.Flags().String("template", "", "Template string or path to template file to use when -o=go-template, -o=go-template-file. The templa")
+	create_serviceaccountCmd.Flags().Bool("validate", false, "If true, use a schema to validate the input before sending it")
+	createCmd.AddCommand(create_serviceaccountCmd)
+
+	carapace.Gen(create_serviceaccountCmd).FlagCompletion(carapace.ActionMap{
+		"dry-run":  action.ActionDryRunModes(),
+		"output":   action.ActionOutputFormats(),
+		"template": carapace.ActionFiles(""),
+	})
+}
