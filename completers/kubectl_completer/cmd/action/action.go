2879babc07db76e12410c4858c111523568f1981
rsteube <rsteube@users.noreply.github.com>
2020-12-10 00:39:05 +0100

    kubectl: added annotate

--- completers/kubectl_completer/cmd/action/action.go
+++ completers/kubectl_completer/cmd/action/action.go
@@ -91,3 +91,14 @@ func ActionLabels(namespace string, resource string) carapace.Action {
 		}
 	})
 }
+
+func ActionAnnotations(namespace string, resource string) carapace.Action {
+	return carapace.ActionCallback(func(args []string) carapace.Action {
+		if output, err := exec.Command("kubectl", "--namespace", namespace, "get", "-o", "go-template={{range $key, $value := .metadata.annotations}}{{$key}}\n{{$value}}\n{{end}}", resource).Output(); err != nil {
+			return carapace.ActionMessage(err.Error())
+		} else {
+			lines := strings.Split(string(output), "\n")
+			return carapace.ActionValuesDescribed(lines[:len(lines)-1]...)
+		}
+	})
+}
