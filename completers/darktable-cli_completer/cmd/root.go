e4e6709f8b8da49cbc271df55343574b6be0ed2e
rsteube <rsteube@users.noreply.github.com>
2021-01-17 01:12:49 +0100

    added darktable

---
+++ completers/darktable-cli_completer/cmd/root.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "darktable-cli",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().String("apply-custom-presets", "", "custom preset")
+	rootCmd.Flags().String("bpp", "", "bit depth")
+	rootCmd.Flags().String("height", "", "max height")
+	rootCmd.Flags().Bool("help,-h", false, "")
+	rootCmd.Flags().String("hq", "", "high quality resampling")
+	rootCmd.Flags().String("style", "", "style name")
+	rootCmd.Flags().Bool("style-overwrite", false, "")
+	rootCmd.Flags().String("upscale", "", "upscaling")
+	rootCmd.Flags().Bool("verbose", false, "")
+	rootCmd.Flags().Bool("version", false, "")
+	rootCmd.Flags().String("width", "", "max width")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"apply-custom-presets": carapace.ActionValues("0", "1", "false", "true"),
+		"hq":                   carapace.ActionValues("0", "1", "false", "true"),
+		"upscale":              carapace.ActionValues("0", "1", "false", "true"),
+	})
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionFiles(""),
+		carapace.ActionFiles(""),
+	)
+}
---
+++ completers/darktable_completer/cmd/root.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "darktable",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().StringS("t", "t", "", "num openmp threads")
+	rootCmd.Flags().StringS("d", "d", "", "enable debug output")
+	rootCmd.Flags().String("cachedir", "", "user cache directory")
+	rootCmd.Flags().String("conf", "", "override config values")
+	rootCmd.Flags().String("configdir", "", "user config directory")
+	rootCmd.Flags().String("datadir", "", "data directory")
+	rootCmd.Flags().Bool("disable-opencl", false, "prevent OpenCL initialization")
+	rootCmd.Flags().BoolP("help", "h", false, "")
+	rootCmd.Flags().String("library", "", "library file")
+	rootCmd.Flags().String("localedir", "", "locale directory")
+	rootCmd.Flags().String("luacmd", "", "lua command")
+	rootCmd.Flags().String("moduledir", "", "module directory")
+	rootCmd.Flags().String("noiseprofiles", "", "noiseprofiles json file")
+	rootCmd.Flags().String("tmpdir", "", "tmp directory")
+	rootCmd.Flags().Bool("version", false, "print version number")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"d":             carapace.ActionValues("all", "cache", "camctl", "camsupport", "control", "dev", "fswatch", "input", "lighttable", "lua", "masks", "memory", "nan", "opencl", "perf", "pwstorage", "print", "sql", "ioporder", "imageio"),
+		"cachedir":      carapace.ActionDirectories(),
+		"configdir":     carapace.ActionDirectories(),
+		"datadir":       carapace.ActionDirectories(),
+		"library":       carapace.ActionFiles(".db"),
+		"localedir":     carapace.ActionDirectories(),
+		"moduledir":     carapace.ActionDirectories(),
+		"noiseprofiles": carapace.ActionFiles(".json"),
+		"tmpdir":        carapace.ActionDirectories(),
+	})
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(carapace.ActionFiles(""))
+}
