51ed8d599399db8e46b0015185f4e640d0c19c4b
rsteube <rsteube@users.noreply.github.com>
2021-01-29 18:37:26 +0100

    added zcat

---
+++ completers/zcat_completer/cmd/root.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "zcat",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("force", "f", false, "force; read compressed data even from a terminal")
+	rootCmd.Flags().Bool("help", false, "display this help and exit")
+	rootCmd.Flags().BoolP("list", "l", false, "list compressed file contents")
+	rootCmd.Flags().BoolP("quiet", "q", false, "suppress all warnings")
+	rootCmd.Flags().BoolP("recursive", "r", false, "operate recursively on directories")
+	rootCmd.Flags().StringP("suffix", "S", "", "use suffix SUF on compressed files")
+	rootCmd.Flags().BoolP("test", "t", false, "test compressed file integrity")
+	rootCmd.Flags().BoolP("verbose", "v", false, "verbose mode")
+	rootCmd.Flags().Bool("version", false, "display version information and exit")
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(carapace.ActionFiles(""))
+}
