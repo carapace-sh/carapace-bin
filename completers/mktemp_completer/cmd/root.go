b054bbbcc51f51b0bf08c70ec33f19594be6adec
rsteube <rsteube@users.noreply.github.com>
2021-04-04 01:03:50 +0200

    added mktemp

---
+++ completers/mktemp_completer/cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mktemp",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolS("t", "t", false, "interpret TEMPLATE as a single file name component")
+	rootCmd.Flags().BoolP("directory", "d", false, "create a directory, not a file")
+	rootCmd.Flags().BoolP("dry-run", "u", false, "do not create anything; merely print a name (unsafe)")
+	rootCmd.Flags().Bool("help", false, "display this help and exit")
+	rootCmd.Flags().BoolP("quiet", "q", false, "suppress diagnostics about file/dir-creation failure")
+	rootCmd.Flags().String("suffix", "", "append SUFF to TEMPLATE")
+	rootCmd.Flags().StringP("tmpdir", "p", "", "interpret TEMPLATE relative to DIR")
+	rootCmd.Flags().Bool("version", false, "output version information and exit")
+
+	rootCmd.Flag("tmpdir").NoOptDefVal = " "
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"tmpdir": carapace.ActionDirectories(),
+	})
+}
---
+++ completers/mktemp_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/mktemp_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
