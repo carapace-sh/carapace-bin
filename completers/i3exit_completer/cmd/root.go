16bdd66dc1d740f82785e859d4eea844e866c0ea
rsteube <rsteube@users.noreply.github.com>
2021-04-03 18:13:12 +0200

    added i3exit

---
+++ completers/i3exit_completer/cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "i3exit",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionValues("lock", "logout", "switch_user", "suspend", "hibernate", "reboot", "shutdown"),
+	)
+}
