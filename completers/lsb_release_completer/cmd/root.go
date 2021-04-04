bd858ab262ea9e3588ece16ca0a675c9365c89b3
rsteube <rsteube@users.noreply.github.com>
2021-04-04 02:07:34 +0200

    added lsb_release

---
+++ completers/lsb_release_completer/cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "lsb_release",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("all", "a", false, "Display all of the above information.")
+	rootCmd.Flags().BoolP("codename", "c", false, "Display the codename according to the distribution release.")
+	rootCmd.Flags().BoolP("description", "d", false, "Display the single line text description of the distribution.")
+	rootCmd.Flags().BoolP("help", "h", false, "Display this message.")
+	rootCmd.Flags().BoolP("id", "i", false, "Display the string id of the distributor.")
+	rootCmd.Flags().BoolP("release", "r", false, "Display the release number of the distribution.")
+	rootCmd.Flags().BoolP("short", "s", false, "Display all of the above information in short output format.")
+	rootCmd.Flags().BoolP("version", "v", false, "Display the version of the LSB specification against which the distribution is compliant.")
+}
