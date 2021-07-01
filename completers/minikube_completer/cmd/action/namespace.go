cfcb75b3f79d80448989896fc03e72b10822422a
rsteube <rsteube@users.noreply.github.com>
2021-07-02 01:13:28 +0200

    minikube: pause completion

---
+++ completers/minikube_completer/cmd/action/namespace.go
@@ -0,0 +1,21 @@
+package action
+
+import (
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+func ActionNamespaces() carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		return carapace.ActionExecCommand("minikube", "kubectl", "get", "namespaces")(func(output []byte) carapace.Action {
+			vals := make([]string, 0)
+
+			lines := strings.Split(string(output), "\n")
+			for _, line := range lines[1 : len(lines)-1] {
+				vals = append(vals, strings.Fields(line)[0])
+			}
+			return carapace.ActionValues(vals...)
+		})
+	})
+}
--- completers/minikube_completer/cmd/pause.go
+++ completers/minikube_completer/cmd/pause.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/completers/minikube_completer/cmd/action"
 	"github.com/spf13/cobra"
 )
 
@@ -11,8 +13,15 @@ var pauseCmd = &cobra.Command{
 }
 
 func init() {
-	pauseCmd.Flags().StringSliceP("--namespaces", "n", []string{"kube-system", "kubernetes-dashboard", "storage-gluster", "istio-operator"}, "namespaces to pause")
 	pauseCmd.Flags().BoolP("all-namespaces", "A", false, "If set, pause all namespaces")
+	pauseCmd.Flags().StringSliceP("namespaces", "n", []string{"kube-system", "kubernetes-dashboard", "storage-gluster", "istio-operator"}, "namespaces to pause")
 	pauseCmd.Flags().StringP("output", "o", "text", "Format to print stdout in. Options include: [text,json]")
 	rootCmd.AddCommand(pauseCmd)
+
+	carapace.Gen(pauseCmd).FlagCompletion(carapace.ActionMap{
+		"namespaces": carapace.ActionMultiParts(",", func(c carapace.Context) carapace.Action {
+			return action.ActionNamespaces().Invoke(c).Filter(c.Parts).ToA()
+		}),
+		"output": carapace.ActionValues("text", "json"),
+	})
 }
