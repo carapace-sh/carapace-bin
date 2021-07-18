e64d949cf8e5846f2f4b3b3255308f56bf98f377
rsteube <rsteube@users.noreply.github.com>
2021-07-18 11:20:55 +0200

    added xbacklight

---
+++ completers/xbacklight_completer/cmd/root.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/os"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "xbacklight",
+	Short: "adjust backlight brightness using RandR extension",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	carapace.Override(carapace.Opts{
+		LongShorthand: true,
+	})
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().String("d", "", "Display to set backlight for.")
+	rootCmd.Flags().String("dec", "", "Decreases brightness by the specified amount.")
+	rootCmd.Flags().String("display", "", "Display to set backlight for.")
+	rootCmd.Flags().Bool("get", false, "Print  out  the  current backlight brightness of each output with such a control.")
+	rootCmd.Flags().Bool("help", false, "Print out a summary of the usage and exit.")
+	rootCmd.Flags().String("inc", "", "Increases brightness by the specified amount.")
+	rootCmd.Flags().String("set", "", "Sets each backlight brightness to the specified level.")
+	rootCmd.Flags().String("steps", "", "Number of steps to take while fading.")
+	rootCmd.Flags().String("time", "", "Length of time to spend fading the backlight between old and new value.")
+	rootCmd.Flags().Bool("version", false, "Print out the program version and exit.")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"d":       os.ActionDisplays(),
+		"display": os.ActionDisplays(),
+	})
+}
