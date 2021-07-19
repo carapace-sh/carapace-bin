c87f5d7ef76423d245d2d697ebbdf06d71cf13aa
rsteube <rsteube@users.noreply.github.com>
2021-07-19 10:57:46 +0200

    git: added gc, prune and prune-packed

--- completers/git_completer/cmd/gc.go
+++ completers/git_completer/cmd/gc.go
@@ -1,17 +1,19 @@
 package cmd
 
 import (
+	"github.com/rsteube/carapace"
 	"github.com/spf13/cobra"
 )
 
 var gcCmd = &cobra.Command{
 	Use:   "gc",
 	Short: "Cleanup unnecessary files and optimize the local repository",
-	Run: func(cmd *cobra.Command, args []string) {
-	},
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 func init() {
+	carapace.Gen(gcCmd).Standalone()
+
 	gcCmd.Flags().Bool("aggressive", false, "be more thorough (increased runtime)")
 	gcCmd.Flags().Bool("auto", false, "enable auto-gc mode")
 	gcCmd.Flags().Bool("force", false, "force running gc even if there may be another gc running")
@@ -19,4 +21,6 @@ func init() {
 	gcCmd.Flags().String("prune", "", "prune unreferenced objects")
 	gcCmd.Flags().BoolP("quiet", "q", false, "suppress progress reporting")
 	rootCmd.AddCommand(gcCmd)
+
+	gcCmd.Flag("prune").NoOptDefVal = " "
 }
