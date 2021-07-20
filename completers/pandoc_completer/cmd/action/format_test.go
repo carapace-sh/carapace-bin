5ed72dae39d702c2672635c51628d88406288be8
rsteube <rsteube@users.noreply.github.com>
2021-07-20 09:31:31 +0200

    added pandoc

---
+++ completers/pandoc_completer/cmd/action/format_test.go
@@ -0,0 +1,24 @@
+package action
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestExtensionFields(t *testing.T) {
+	tests := map[string]string{
+		"json+one-two+":      "json|+one|-two|+",
+		"":                   "",
+		"json":               "json",
+		"json-":              "json|-",
+		"json--+":            "json|-|-|+",
+		"json-one-two+three": "json|-one|-two|+three",
+	}
+	for test, expected := range tests {
+		t.Run(test, func(t *testing.T) {
+			if actual := strings.Join(extensionFields(test), "|"); actual != expected {
+				t.Errorf("expected: '%v' actual: '%v'", expected, actual)
+			}
+		})
+	}
+}
