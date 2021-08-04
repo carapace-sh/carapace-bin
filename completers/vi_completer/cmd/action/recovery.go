6c5c361607baf6a0d1af54ec09bdfa06e85355a5
rsteube <rsteube@users.noreply.github.com>
2021-08-04 21:23:36 +0200

    added vi

---
+++ completers/vi_completer/cmd/action/recovery.go
@@ -0,0 +1,24 @@
+package action
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+func ActionTemporaryFiles() carapace.Action {
+	return carapace.ActionExecCommand("sh", "-c", "vi -r 2>&1")(func(output []byte) carapace.Action {
+		lines := strings.Split(string(output), "\n")
+		r := regexp.MustCompile(`^On (?P<date>.*) saved \d+ lines of file "(?P<file>.*)"$`)
+
+		vals := make([]string, 0)
+		for _, line := range lines {
+			if r.MatchString(line) {
+				matched := r.FindStringSubmatch(line)
+				vals = append(vals, matched[2], matched[1])
+			}
+		}
+		return carapace.ActionValuesDescribed(vals...)
+	})
+}
