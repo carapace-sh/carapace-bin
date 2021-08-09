96403a619bd207c619510ec08b80fecada52ba2c
rsteube <rsteube@users.noreply.github.com>
2021-08-09 15:40:31 +0200

    flutter: build - inconsistent subcommand documentation
    seems documentation differs from actual subcommands: https://github.com/flutter/flutter/issues/80778
    
    added commands as suggested by default bash completion

---
+++ completers/flutter_completer/cmd/build_fuchsia.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var build_fuchsiaCmd = &cobra.Command{
+	Use:   "fuchsia",
+	Short: "Build the Fuchsia target",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(build_fuchsiaCmd).Standalone()
+
+	build_fuchsiaCmd.Flags().String("dart-define", "", "Additional key-value pairs that will be available as constants.")
+	build_fuchsiaCmd.Flags().Bool("debug", false, "Build a debug version of your app.")
+	build_fuchsiaCmd.Flags().BoolP("help", "h", false, "Print this usage information.")
+	build_fuchsiaCmd.Flags().Bool("no-null-assertions", false, "Perform additional null assertions on the boundaries of migrated and un-migrated code.")
+	build_fuchsiaCmd.Flags().Bool("no-tree-shake-icons", false, "Do not tree shake icon fonts so that only glyphs used by the application remain.")
+	build_fuchsiaCmd.Flags().Bool("null-assertions", false, "Perform additional null assertions on the boundaries of migrated and un-migrated code.")
+	build_fuchsiaCmd.Flags().Bool("profile", false, "Build a version of your app specialized for performance profiling.")
+	build_fuchsiaCmd.Flags().Bool("release", false, "Build a release version of your app (default mode).")
+	build_fuchsiaCmd.Flags().String("runner-source", "", "The package source to use for the flutter_runner.")
+	build_fuchsiaCmd.Flags().StringP("target", "t", "", "The main entry-point file of the application, as run on the device.")
+	build_fuchsiaCmd.Flags().String("target-platform", "", "The target platform for which the app is compiled.")
+	build_fuchsiaCmd.Flags().Bool("tree-shake-icons", false, "Tree shake icon fonts so that only glyphs used by the application remain.")
+	buildCmd.AddCommand(build_fuchsiaCmd)
+
+	carapace.Gen(build_fuchsiaCmd).FlagCompletion(carapace.ActionMap{
+		"runner-source": carapace.ActionValuesDescribed(
+			"fuchsia.com", "runner already on the device",
+			"flutter_tool", "runner distributed with Flutter",
+		),
+		"target":          carapace.ActionFiles(".dart"),
+		"target-platform": carapace.ActionValues("fuchsia-arm64", "fuchsia-x64"),
+	})
+}
---
+++ completers/flutter_completer/cmd/build_windows.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var build_windowsCmd = &cobra.Command{
+	Use:   "windows",
+	Short: "Build a Windows desktop application",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(build_windowsCmd).Standalone()
+
+	build_windowsCmd.Flags().Bool("analyze-size", false, "Produce additional profile information for artifact output size.")
+	build_windowsCmd.Flags().String("dart-define", "", "Additional key-value pairs that will be available as constants.")
+	build_windowsCmd.Flags().Bool("debug", false, "Build a debug version of your app.")
+	build_windowsCmd.Flags().BoolP("help", "h", false, "Print this usage information.")
+	build_windowsCmd.Flags().Bool("no-analyze-size", false, "Do not produce additional profile information for artifact output size.")
+	build_windowsCmd.Flags().Bool("no-null-assertions", false, "Do not perform additional null assertions on the boundaries of migrated and un-migrated code.")
+	build_windowsCmd.Flags().Bool("no-obfuscate", false, "In a release build, this flag does not removes identifiers and replaces them with randomized values for the purposes of source code obfuscation.")
+	build_windowsCmd.Flags().Bool("no-pub", false, "Do not run \"flutter pub get\" before executing this command.")
+	build_windowsCmd.Flags().Bool("no-track-widget-creation", false, "Do not track widget creation locations.")
+	build_windowsCmd.Flags().Bool("no-tree-shake-icons", false, "Do not tree shake icon fonts so that only glyphs used by the application remain.")
+	build_windowsCmd.Flags().Bool("null-assertions", false, "Perform additional null assertions on the boundaries of migrated and un-migrated code.")
+	build_windowsCmd.Flags().Bool("obfuscate", false, "In a release build, this flag removes identifiers and replaces them with randomized values for the purposes of source code obfuscation.")
+	build_windowsCmd.Flags().Bool("profile", false, "Build a version of your app specialized for performance profiling.")
+	build_windowsCmd.Flags().Bool("pub", false, "Run \"flutter pub get\" before executing this command.")
+	build_windowsCmd.Flags().Bool("release", false, "Build a release version of your app (default mode).")
+	build_windowsCmd.Flags().String("split-debug-info", "", "In a release build, this flag reduces application size by storing Dart program symbols in a separate file on the host rather than in the application.")
+	build_windowsCmd.Flags().StringP("target", "t", "", "The main entry-point file of the application, as run on the device.")
+	build_windowsCmd.Flags().Bool("track-widget-creation", false, "Track widget creation locations.")
+	build_windowsCmd.Flags().Bool("tree-shake-icons", false, "Tree shake icon fonts so that only glyphs used by the application remain.")
+	buildCmd.AddCommand(build_windowsCmd)
+
+	carapace.Gen(build_windowsCmd).FlagCompletion(carapace.ActionMap{
+		"target": carapace.ActionFiles(".dart"),
+	})
+}
