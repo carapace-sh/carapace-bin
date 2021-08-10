acd5ba4e500f40f5fe81dfbfcd909f57d6480d18
rsteube <rsteube@users.noreply.github.com>
2021-08-10 15:00:49 +0200

    flutter: fix device-user completion

--- pkg/actions/tools/adb/user.go
+++ pkg/actions/tools/adb/user.go
@@ -13,10 +13,11 @@ import (
 func ActionDeviceUsers() carapace.Action {
 	return carapace.ActionExecCommand("adb", "shell", "pm", "list", "users")(func(output []byte) carapace.Action {
 		lines := strings.Split(string(output), "\n")
-		r := regexp.MustCompile(`^\tUserInfo{(?P<id>\d+):(?P<name>[^:]+):(?P<info>[^}]*)} (?P<state>.*)$`)
+		r := regexp.MustCompile(`^\t+UserInfo{(?P<id>\d+):(?P<name>[^:]+):(?P<info>[^}]*)}( (?P<state>.*))?$`)
 
 		vals := make([]string, 0)
-		for _, line := range lines {
+		for _, line := range lines[1:] {
+			line = strings.Replace(line, "\r", "", -1)
 			if r.MatchString(line) {
 				matches := r.FindStringSubmatch(line)
 				vals = append(vals, matches[1], matches[2])
