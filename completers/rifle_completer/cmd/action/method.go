d6b547b38b71ae1798da5166a31d403430791e3d
rsteube <rsteube@users.noreply.github.com>
2021-08-11 19:06:51 +0200

    added rifle

---
+++ completers/rifle_completer/cmd/action/method.go
@@ -0,0 +1,19 @@
+package action
+
+import (
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+func ActionMethods(file string) carapace.Action {
+	return carapace.ActionExecCommand("rifle", "-l", file)(func(output []byte) carapace.Action {
+		lines := strings.Split(string(output), "\n")
+
+		vals := make([]string, 0)
+		for _, line := range lines[:len(lines)-1] {
+			vals = append(vals, strings.SplitN(line, ":", 2)...)
+		}
+		return carapace.ActionValuesDescribed(vals...)
+	})
+}
