e3d05cd4760d9c94200564df61f0888ddcc84365
rsteube <rsteube@users.noreply.github.com>
2021-09-23 20:37:47 +0200

    add dmesg

---
+++ completers/dmesg_completer/cmd/action/facility.go
@@ -0,0 +1,16 @@
+package action
+
+import "github.com/rsteube/carapace"
+
+func ActionFacilities() carapace.Action {
+	return carapace.ActionValuesDescribed(
+		"kern", "kernel messages",
+		"user", "random user-level messages",
+		"mail", "mail system",
+		"daemon", "system daemons",
+		"auth", "security/authorization messages",
+		"syslog", "messages generated internally by syslogd",
+		"lpr", "line printer subsystem",
+		"news", "network news subsystem",
+	)
+}
---
+++ completers/dmesg_completer/cmd/action/log.go
@@ -0,0 +1,16 @@
+package action
+
+import "github.com/rsteube/carapace"
+
+func ActionLogLevels() carapace.Action {
+	return carapace.ActionValuesDescribed(
+		"emerg", "system is unusable",
+		"alert", "action must be taken immediately",
+		"crit", "critical conditions",
+		"err", "error conditions",
+		"warn", "warning conditions",
+		"notice", "normal but significant condition",
+		"info", "informational",
+		"debug", "debug-level messages",
+	)
+}
