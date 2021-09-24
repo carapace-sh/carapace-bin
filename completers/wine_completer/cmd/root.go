f5664960586dcc0377cf89d25208d25ae16e2876
rsteube <rsteube@users.noreply.github.com>
2021-09-24 08:58:09 +0200

    added wine

---
+++ completers/wine_completer/cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "wine",
+	Short: "run Windows programs on Unix",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+	rootCmd.Flags().Bool("help", false, "show help")
+	rootCmd.Flags().Bool("version", false, "show version")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionFiles(),
+	)
+}
