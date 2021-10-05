df104c176302ce29b9e9ae19299a60d5a63e381a
rsteube <rsteube@users.noreply.github.com>
2021-10-05 12:00:27 +0200

    added i3status-rs

---
+++ completers/i3status-rs_completer/cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "i3status-rs",
+	Short: "A feature-rich and resource-friendly replacement for i3status, written in Rust",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().Bool("exit-on-error", false, "Exit rather than printing errors to i3bar and continuing")
+	rootCmd.Flags().BoolP("help", "h", false, "Prints help information")
+	rootCmd.Flags().Bool("never-pause", false, "Ignore any attempts by i3 to pause the bar when hidden/fullscreen")
+	rootCmd.Flags().BoolP("version", "V", false, "Prints version information")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionFiles(),
+	)
+}
