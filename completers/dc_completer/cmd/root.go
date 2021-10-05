12ec7869eda09b33d838c6a4419bbe82b872a01f
rsteube <rsteube@users.noreply.github.com>
2021-10-05 11:25:09 +0200

    added dc

---
+++ completers/dc_completer/cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "dc",
+	Short: "an arbitrary precision calculator",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().StringP("expression", "e", "", "evaluate expression")
+	rootCmd.Flags().StringP("file", "f", "", "evaluate contents of file")
+	rootCmd.Flags().BoolP("help", "h", false, "display this help and exit")
+	rootCmd.Flags().BoolP("version", "V", false, "output version information and exit")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"file": carapace.ActionFiles(),
+	})
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		carapace.ActionFiles(),
+	)
+}
