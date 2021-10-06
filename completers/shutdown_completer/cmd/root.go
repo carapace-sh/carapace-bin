411697b831d68c2915d53995a596e18a9d5b2585
rsteube <rsteube@users.noreply.github.com>
2021-10-06 11:59:48 +0200

    added shutdown

---
+++ completers/shutdown_completer/cmd/root.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/time"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "shutdown",
+	Short: "Shut down the system",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolS("c", "c", false, "Cancel a pending shutdown")
+	rootCmd.Flags().BoolS("h", "h", false, "Equivalent to --poweroff, overridden by --halt")
+	rootCmd.Flags().BoolP("halt", "H", false, "Halt the machine")
+	rootCmd.Flags().Bool("help", false, "Show this help")
+	rootCmd.Flags().BoolS("k", "k", false, "Don't halt/power-off/reboot, just send warnings")
+	rootCmd.Flags().Bool("no-wall", false, "Don't send wall message before halt/power-off/reboot")
+	rootCmd.Flags().BoolP("poweroff", "P", false, "Power-off the machine")
+	rootCmd.Flags().BoolP("reboot", "r", false, "Reboot the machine")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.Batch(
+			carapace.ActionValuesDescribed("now", "immediate shutdown"),
+			time.ActionTime(),
+			carapace.ActionMultiParts("+", func(c carapace.Context) carapace.Action {
+				if len(c.Parts) == 0 {
+					return carapace.ActionValuesDescribed("+", "shutdown in m minutes from now")
+				}
+				return carapace.ActionValues()
+			}),
+		).ToA(),
+	)
+}
--- pkg/actions/time/time.go
+++ pkg/actions/time/time.go
@@ -80,3 +80,19 @@ func ActionDays(year int, month int) carapace.Action {
 		return carapace.ActionValues(vals...)
 	})
 }
+
+// ActionTime completes `hh:mm` time
+//   00:30
+//   14:47
+func ActionTime() carapace.Action {
+	return carapace.ActionMultiParts(":", func(c carapace.Context) carapace.Action {
+		switch len(c.Parts) {
+		case 0:
+			return carapace.ActionValues("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23").Invoke(c).Suffix(":").ToA()
+		case 1:
+			return carapace.ActionValues("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59")
+		default:
+			return carapace.ActionValues()
+		}
+	})
+}
