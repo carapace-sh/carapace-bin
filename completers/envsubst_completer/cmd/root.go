62fd5b24cf0f8616568e92e3ae44c829ca8d0d21
rsteube <rsteube@users.noreply.github.com>
2021-10-06 16:09:06 +0200

    added envsubst

---
+++ completers/envsubst_completer/cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "envsubst",
+	Short: "Substitutes the values of environment variables",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("help", "h", false, "display this help and exit")
+	rootCmd.Flags().BoolP("variables", "v", false, "output the variables occurring in SHELL-FORMAT")
+	rootCmd.Flags().BoolP("version", "V", false, "output version information and exit")
+}
