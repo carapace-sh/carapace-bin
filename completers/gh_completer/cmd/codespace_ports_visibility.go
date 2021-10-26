8feb76d6d7407eeaf9b8ca35faf73af5883ccf7b
rsteube <rsteube@users.noreply.github.com>
2021-10-27 00:00:30 +0200

    gh: codespace completion

--- completers/gh_completer/cmd/codespace_ports_visibility.go
+++ completers/gh_completer/cmd/codespace_ports_visibility.go
@@ -19,7 +19,8 @@ func init() {
 		carapace.ActionMultiParts(":", func(c carapace.Context) carapace.Action {
 			switch len(c.Parts) {
 			case 0:
-				return action.ActionCodespacePorts(codespace_ports_visibilityCmd.Flag("codespace").Value.String()).Invoke(c).Suffix(":").ToA()
+				codespace := codespace_ports_visibilityCmd.Flag("codespace").Value.String()
+				return action.ActionCodespacePorts(codespace).Invoke(c).Suffix(":").ToA()
 			case 1:
 				return carapace.ActionValues("public", "private", "org")
 			default:
