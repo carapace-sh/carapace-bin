94f299596c5a9b156439ccf4d22dfed51d415973
rsteube <rsteube@users.noreply.github.com>
2021-11-07 00:52:22 +0100

    added gnome-keyring

---
+++ completers/gnome-keyring_completer/cmd/import.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var importCmd = &cobra.Command{
+	Use:   "import",
+	Short: "Import keys or certificates from a PKCS file",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(importCmd).Standalone()
+
+	rootCmd.AddCommand(importCmd)
+
+	carapace.Gen(importCmd).PositionalCompletion(
+		carapace.ActionFiles(),
+	)
+}
---
+++ completers/gnome-keyring_completer/cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "gnome-keyring",
+	Short: "The gnome-keyring commandline tool",
+	Long:  "https://wiki.gnome.org/Projects/GnomeKeyring/",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+}
