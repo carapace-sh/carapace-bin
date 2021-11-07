6b5c5f2226dd6d44b75bad0da6a6d6d14a0e4da7
rsteube <rsteube@users.noreply.github.com>
2021-11-08 00:33:55 +0100

    added bc

---
+++ completers/bc_completer/cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "bc",
+	Short: "An arbitrary precision calculator language",
+	Long:  "https://linux.die.net/man/1/bc",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("help", "h", false, "print this usage and exit")
+	rootCmd.Flags().BoolP("interactive", "i", false, "force interactive mode")
+	rootCmd.Flags().BoolP("mathlib", "l", false, "use the predefined math routines")
+	rootCmd.Flags().BoolP("quiet", "q", false, "don't print initial banner")
+	rootCmd.Flags().BoolP("standard", "s", false, "non-standard bc constructs are errors")
+	rootCmd.Flags().BoolP("version", "v", false, "print version information and exit")
+	rootCmd.Flags().BoolP("warn", "w", false, "warn about non-standard bc constructs")
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		carapace.ActionFiles(),
+	)
+}
