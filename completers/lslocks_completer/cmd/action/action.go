6c275314071be0f5549cae9bfbbb94ea08d3b5dc
rsteube <rsteube@users.noreply.github.com>
2021-12-03 20:14:15 +0100

    added lslocks

---
+++ completers/lslocks_completer/cmd/action/action.go
@@ -0,0 +1,18 @@
+package action
+
+import "github.com/rsteube/carapace"
+
+func ActionColumns() carapace.Action {
+	return carapace.ActionValuesDescribed(
+		"COMMAND", "command of the process holding the lock",
+		"PID", "PID of the process holding the lock",
+		"TYPE", "kind of lock",
+		"SIZE", "size of the lock",
+		"MODE", "lock access mode",
+		"M", "mandatory state of the lock: 0 (none), 1 (set)",
+		"START", "relative byte offset of the lock",
+		"END", "ending offset of the lock",
+		"PATH", "path of the locked file",
+		"BLOCKER", "PID of the process blocking the lock",
+	)
+}
