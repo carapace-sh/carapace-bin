6c1e59651f039b7f3254ea01609eb70fe2da25d4
Benjamin VAUDOUR <bvaudour@users.noreply.github.com>
2021-12-10 22:01:36 +0100

    Edit elvish lazy for 0.17 depreciation correction

--- cmd/carapace/cmd/lazyInit.go
+++ cmd/carapace/cmd/lazyInit.go
@@ -18,9 +18,9 @@ complete -F _carapace_lazy %v
 }
 
 func elvish_lazy(completers []string) string {
-	snippet := `put %v | each [c]{
-    edit:completion:arg-completer[$c] = [@arg]{
-        edit:completion:arg-completer[$c] = [@arg]{}
+	snippet := `put %v | each {|c|
+    edit:completion:arg-completer[$c] = {|@arg|
+        edit:completion:arg-completer[$c] = {|@arg| }
         eval (carapace $c elvish | slurp)
         $edit:completion:arg-completer[$c] $@arg
     }
