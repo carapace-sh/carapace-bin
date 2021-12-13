4c7e46299d4ec43c9b750bf763009f3a4f8ddea4
rsteube <rsteube@users.noreply.github.com>
2021-12-13 21:31:28 +0100

    git: shortlog author completion

---
+++ pkg/actions/tools/git/author.go
@@ -0,0 +1,26 @@
+package git
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+// ActionAuthors completes authors of repo
+//   Some Name (email@host.com)
+//   Another Name (email@another.com)
+func ActionAuthors() carapace.Action {
+	return carapace.ActionExecCommand("git", "shortlog", "--summary", "--email", "HEAD")(func(output []byte) carapace.Action {
+		lines := strings.Split(string(output), "\n")
+		r := regexp.MustCompile(`^ +\d+\t(?P<name>.*) <(?P<email>[^<>]+)>$`)
+
+		vals := make([]string, 0)
+		for _, line := range lines {
+			if r.MatchString(line) {
+				vals = append(vals, r.FindStringSubmatch(line)[1:]...)
+			}
+		}
+		return carapace.ActionValuesDescribed(vals...)
+	})
+}
