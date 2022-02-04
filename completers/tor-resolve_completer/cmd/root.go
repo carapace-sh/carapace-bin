fa9017adefdf4bf48bfde784294a08a83b12ae3b
rsteube <rsteube@users.noreply.github.com>
2022-02-04 14:13:46 +0100

    added tor-resolve

---
+++ completers/tor-resolve_completer/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "tor-resolve",
+	Short: "resolve a hostname to an IP address via tor",
+	Long:  "https://www.torproject.org/",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolS("4", "4", false, "Use the SOCKS4a protocol rather than the default SOCKS5 protocol")
+	rootCmd.Flags().BoolS("5", "5", false, "Use the SOCKS5 protocol")
+	rootCmd.Flags().StringS("p", "p", "", "Override the default SOCKS port without setting the hostname")
+	rootCmd.Flags().BoolS("v", "v", false, "Display verbose output")
+	rootCmd.Flags().BoolS("x", "x", false, "Perform a reverse lookup")
+}
