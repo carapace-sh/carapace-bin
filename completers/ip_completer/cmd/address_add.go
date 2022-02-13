5b0019274e3aaf2c39ec2324da24948a38073a3d
rsteube <rsteube@users.noreply.github.com>
2022-02-13 10:32:27 +0100

    tmp

---
+++ completers/ip_completer/cmd/address_add.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/net"
+	"github.com/spf13/cobra"
+)
+
+var address_addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "add ip address",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(address_addCmd).Standalone()
+
+	addressCmd.AddCommand(address_addCmd)
+
+    carapace.Gen(address_addCmd).PositionalCompletion(
+        net.ActionIpv4Addresses(),
+    )
+}
