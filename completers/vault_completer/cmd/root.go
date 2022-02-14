c64be94581453fa6d93d45b6ef5e840961c5b978
rsteube <rsteube@users.noreply.github.com>
2022-02-14 23:05:14 +0100

    added vault

---
+++ completers/vault_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/posenercomplete"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:                "vault",
+	Short:              "A tool for secrets management",
+	Long:               "https://www.vaultproject.io/",
+	Run:                func(cmd *cobra.Command, args []string) {},
+	DisableFlagParsing: true,
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		// TODO just bridging it for now
+		posenercomplete.ActionPosenerComplete("vault"),
+	)
+}
---
+++ completers/vault_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/vault_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
