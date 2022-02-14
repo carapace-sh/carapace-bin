b687ef704677a7fc6d906f7dbb96dc29a41c8edd
rsteube <rsteube@users.noreply.github.com>
2022-02-14 01:09:21 +0100

    added podman

---
+++ completers/podman_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/cobracomplete"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:                "podman",
+	Short:              "Simple management tool for pods, containers and images",
+	Long:               "https://podman.io/",
+	Run:                func(cmd *cobra.Command, args []string) {},
+	DisableFlagParsing: true,
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		// TODO just bridging it for now
+		cobracomplete.ActionComplete("podman"),
+	)
+}
