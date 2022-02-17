64bec966885b363886bf73b4c04e9f2462df593d
rsteube <rsteube@users.noreply.github.com>
2022-02-18 00:42:17 +0100

    added zathura

---
+++ completers/zathura_completer/cmd/root.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/os"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "zathura",
+	Short: "a document viewer",
+	Long:  "https://pwmt.org/projects/zathura/",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().StringP("bookmark", "b", "", "Bookmark to go to")
+	rootCmd.Flags().String("cache-dir", "", "Path to the cache directory")
+	rootCmd.Flags().StringP("config-dir", "c", "", "Path to the config directory")
+	rootCmd.Flags().StringP("data-dir", "d", "", "Path to the data directory")
+	rootCmd.Flags().StringP("find", "f", "", "Search for the given phrase and display results")
+	rootCmd.Flags().Bool("fork", false, "Fork into the background")
+	rootCmd.Flags().BoolP("help", "h", false, "Show help options")
+	rootCmd.Flags().StringP("log-level", "l", "", "Log level (debug, info, warning, error)")
+	rootCmd.Flags().String("mode", "", "Start in a non-default mode")
+	rootCmd.Flags().StringP("page", "P", "", "Page number to go to")
+	rootCmd.Flags().StringP("password", "w", "", "Document password")
+	rootCmd.Flags().StringP("plugins-dir", "p", "", "Path to the directories containing plugins")
+	rootCmd.Flags().StringP("reparent", "e", "", "Reparents to window specified by xid (X11)")
+	rootCmd.Flags().StringP("synctex-editor-command", "x", "", "Synctex editor (forwarded to the synctex command)")
+	rootCmd.Flags().String("synctex-forward", "", "Move to given synctex position")
+	rootCmd.Flags().String("synctex-pid", "", "Highlight given position in the given process")
+	rootCmd.Flags().BoolP("version", "v", false, "Print version information")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"config-dir":  carapace.ActionDirectories(),
+		"data-dir":    carapace.ActionDirectories(),
+		"log-level":   carapace.ActionValues("debug", "info", "warning", "error"),
+		"mode":        carapace.ActionValues("fullscreen", "presentation"),
+		"plugins-dir": carapace.ActionDirectories(),
+		"synctex-pid": os.ActionProcessIds(),
+	})
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		carapace.ActionFiles(),
+	)
+}
---
+++ completers/zathura_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/zathura_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
