8e13bac009ec43ad377e586922749ea2e6f09976
rsteube <rsteube@users.noreply.github.com>
2022-02-28 00:30:38 +0100

    added visudo

---
+++ completers/visudo_completer/cmd/root.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "visudo",
+	Short: "safely edit the sudoers file",
+	Long:  "https://linux.die.net/man/8/visudo",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("check", "c", false, "check-only mode")
+	rootCmd.Flags().StringP("file", "f", "", "specify sudoers file location")
+	rootCmd.Flags().BoolP("help", "h", false, "display help message and exit")
+	rootCmd.Flags().BoolP("quiet", "q", false, "less verbose (quiet) syntax error messages")
+	rootCmd.Flags().BoolP("strict", "s", false, "strict syntax checking")
+	rootCmd.Flags().BoolP("version", "V", false, "display version information and exit")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"file": carapace.ActionFiles(),
+	})
+}
---
+++ completers/visudo_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/visudo_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
