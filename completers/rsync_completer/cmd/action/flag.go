79ef3e381741a2bba2fec223b23daca29d2e624c
rsteube <rsteube@users.noreply.github.com>
2022-03-01 00:30:07 +0100

    added rsync

---
+++ completers/rsync_completer/cmd/action/flag.go
@@ -0,0 +1,23 @@
+package action
+
+import "github.com/rsteube/carapace"
+
+func ActionFlags() carapace.Action {
+	return carapace.ActionValuesDescribed(
+		"BACKUP", "Mention files backed up",
+		"COPY", "Mention files copied locally on the receiving side",
+		"DEL", "Mention deletions on the receiving side",
+		"FLIST", "Mention file-list receiving/sending (levels 1-2)",
+		"MISC", "Mention miscellaneous information (levels 1-2)",
+		"MOUNT", "Mention mounts that were found or skipped",
+		"NAME", "Mention 1) updated file/dir names, 2) unchanged names",
+		"PROGRESS", "Mention 1) per-file progress or 2) total transfer progress",
+		"REMOVE", "Mention files removed on the sending side",
+		"SKIP", "Mention files that are skipped due to options used",
+		"STATS", "Mention statistics at end of run (levels 1-3)",
+		"SYMSAFE", "Mention symlinks that are unsafe",
+		"ALL", "Set all --info options (e.g. all4)",
+		"NONE", "Silence all --info options (same as all0)",
+		"HELP", "Output this help message",
+	)
+}
---
+++ completers/rsync_completer/cmd/action/format.go
@@ -0,0 +1,37 @@
+package action
+
+import (
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+func ActionFormats() carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		if strings.HasSuffix(c.CallbackValue, "%") {
+			return carapace.ActionValuesDescribed(
+				"a", "the remote IP address",
+				"b", "the number of bytes actually transferred",
+				"B", "the permission bits of the file",
+				"c", "the total size of the block checksums received for the basis file",
+				"C", "the full-file checksum if it is known for the  file",
+				"f", "the filename",
+				"G", "the gid of the file",
+				"h", "the remote host name",
+				"i", "an itemized list of what is being updated",
+				"l", "the length of the file in bytes",
+				"L", "the string \" -> SYMLINK\", \" => HARDLINK\", or \"\"",
+				"m", "the module name",
+				"M", "the last-modified time of the file",
+				"n", "the filename",
+				"o", "the operation",
+				"p", "the process ID of this rsync session",
+				"P", "the module path",
+				"t", "the current date time",
+				"u", "the authenticated username or an empty string",
+				"U", "the uid of the file",
+			).Invoke(c).Prefix(c.CallbackValue).ToA().NoSpace()
+		}
+		return carapace.ActionValues()
+	})
+}
