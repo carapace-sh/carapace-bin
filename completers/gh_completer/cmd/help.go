9c03e94f44ee23fe5a32bac3b3823b3e277c070f
rsteube <rsteube@users.noreply.github.com>
2022-03-15 22:54:38 +0100

    gh: help topics

--- completers/gh_completer/cmd/help.go
+++ completers/gh_completer/cmd/help.go
@@ -14,4 +14,25 @@ var helpCmd = &cobra.Command{
 func init() {
 	carapace.Gen(helpCmd).Standalone()
 	rootCmd.AddCommand(helpCmd)
+
+	carapace.Gen(helpCmd).PositionalCompletion(
+		carapace.Batch(
+			carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+				vals := make([]string, 0)
+				for _, cmd := range helpCmd.Root().Commands() {
+					if !cmd.Hidden {
+						vals = append(vals, cmd.Name(), cmd.Short)
+					}
+				}
+				return carapace.ActionValuesDescribed(vals...)
+			}),
+			carapace.ActionValuesDescribed(
+				"actions", "Learn about working with GitHub Actions",
+				"environment", "Environment variables that can be used with gh",
+				"formatting", "Formatting options for JSON data exported from gh",
+				"mintty", "Information about using gh with MinTTY",
+				"reference", "A comprehensive reference of all gh commands",
+			),
+		).ToA(),
+	)
 }
