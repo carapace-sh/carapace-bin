7a09d8c1cd646cc3a6a0b5a65291f3550951e647
rsteube <rsteube@users.noreply.github.com>
2022-04-09 13:19:10 +0200

    gh: apiV3 descriptions

--- completers/gh_completer/cmd/action/api.go
+++ completers/gh_completer/cmd/action/api.go
@@ -37,70 +37,87 @@ func ActionApiPreviews() carapace.Action {
 }
 
 func ActionApiV3Paths(cmd *cobra.Command) carapace.Action {
-	return http.ActionApiPaths(v3Paths, `{(.*)}`, func(c carapace.Context, matchedData map[string]string, segment string) carapace.Action {
-		switch segment {
-		// TODO completion for other placeholders
-		case "{archive_format}":
-			return carapace.ActionValues("zip")
-		case "{artifact_id}":
-			fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
-			return ActionWorkflowArtifactIds(cmd, "")
-		case "{assignee}":
-			fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
-			return ActionAssignableUsers(cmd)
-		case "{branch}":
-			fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
-			return ActionBranches(cmd)
-		case "{coc_key}":
-			return ActionCocs(cmd)
-		case "{gist_id}":
-			return ActionGists(cmd)
-		case "{gitignore_name}":
-			return ActionGitignoreTemplates(cmd)
-		case "{license}":
-			return ActionLicenses(cmd)
-		case "{issue_number}":
-			fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
-			return ActionIssues(cmd, IssueOpts{Open: true, Closed: true})
-		case "{owner}":
-			if strings.HasPrefix(c.CallbackValue, ":") {
-				return carapace.ActionValues(":owner")
-			} else {
-				return ActionUsers(cmd, UserOpts{Users: true, Organizations: true})
-			}
-		case "{org}":
-			return ActionUsers(cmd, UserOpts{Organizations: true})
-		case "{package_type}":
-			return ActionPackageTypes()
-		case "{pull_number}":
-			fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
-			return ActionPullRequests(cmd, PullRequestOpts{Open: true, Closed: true, Merged: true})
-		case "{repo}":
-			if strings.HasPrefix(c.CallbackValue, ":") {
-				return carapace.ActionValues(":repo")
-			} else {
-				return ActionRepositories(cmd, matchedData["{owner}"], c.CallbackValue)
-			}
-		case "{tag}": // only used with releases
-			fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
-			return ActionReleases(cmd)
-		case "{template_owner}": // ignore this as it is already provided by `{owner}`
-			return carapace.ActionValues()
-		case "{template_repo}": // ignore this as it is already provided by `{repo}`
-			return carapace.ActionValues()
-		case "{username}":
-			return ActionUsers(cmd, UserOpts{Users: true})
-		case "{workflow_id}":
-			fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
-			return ActionWorkflows(cmd, WorkflowOpts{Enabled: true, Disabled: true, Id: true})
-		default:
-			// static value or placeholder not yet handled
-			return carapace.ActionValues(segment)
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		method := "get"
+		if f := cmd.Flag("method"); f != nil && f.Changed {
+			method = strings.ToLower(f.Value.String())
 		}
+
+		return http.ActionApiPathsDescribed(v3Paths[method], `{(.*)}`, func(c carapace.Context, matchedData map[string]string, segment string) carapace.Action {
+			switch segment {
+			// TODO completion for other placeholders
+			case "{archive_format}":
+				return carapace.ActionValues("zip")
+			case "{artifact_id}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionWorkflowArtifactIds(cmd, "")
+			case "{assignee}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionAssignableUsers(cmd)
+			case "{branch}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionBranches(cmd)
+			case "{coc_key}":
+				return ActionCocs(cmd)
+			case "{environment_name}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionEnvironments(cmd)
+			case "{gist_id}":
+				return ActionGists(cmd)
+			case "{gitignore_name}":
+				return ActionGitignoreTemplates(cmd)
+			case "{label}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionLabels(cmd)
+			case "{license}":
+				return ActionLicenses(cmd)
+			case "{issue_number}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionIssues(cmd, IssueOpts{Open: true, Closed: true})
+			case "{owner}":
+				if strings.HasPrefix(c.CallbackValue, ":") {
+					return carapace.ActionValues(":owner")
+				} else {
+					return ActionUsers(cmd, UserOpts{Users: true, Organizations: true})
+				}
+			case "{org}":
+				return ActionUsers(cmd, UserOpts{Organizations: true})
+			case "{package_type}":
+				return ActionPackageTypes()
+			case "{pull_number}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionPullRequests(cmd, PullRequestOpts{Open: true, Closed: true, Merged: true})
+			case "{repo}":
+				if strings.HasPrefix(c.CallbackValue, ":") {
+					return carapace.ActionValues(":repo")
+				} else {
+					return ActionRepositories(cmd, matchedData["{owner}"], c.CallbackValue)
+				}
+			case "{tag}": // only used with releases
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionReleases(cmd)
+			case "{template_owner}": // ignore this as it is already provided by `{owner}`
+				return carapace.ActionValues()
+			case "{template_repo}": // ignore this as it is already provided by `{repo}`
+				return carapace.ActionValues()
+			case "{username}":
+				return ActionUsers(cmd, UserOpts{Users: true})
+			case "{workflow_id}":
+				fakeRepoFlag(cmd, matchedData["{owner}"], matchedData["{repo}"])
+				return ActionWorkflows(cmd, WorkflowOpts{Enabled: true, Disabled: true, Id: true})
+			default:
+				// static value or placeholder not yet handled
+				return carapace.ActionValues(segment)
+			}
+		})
 	})
 }
 
 func fakeRepoFlag(cmd *cobra.Command, owner, repo string) {
 	cmd.Flags().String("repo", fmt.Sprintf("%v/%v", owner, repo), "fake repo flag")
 	cmd.Flag("repo").Changed = true
+}
+
+func ActionPackageTypes() carapace.Action {
+	return carapace.ActionValues("npm", "maven", "rubygems", "nuget", "docker", "container")
 }
---
+++ completers/gh_completer/cmd/action/apiv3.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e -o pipefail
+
+wget -nc https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.json
+
+
+echo 'package action'
+echo
+echo '// ./apiv3.sh > apiv3.go '
+echo 'var v3Paths map[string][]string = map[string][]string{'
+
+method_endpoints() {
+echo '  "'"$1"'" : {'
+cat api.github.com.json \
+  | jq --raw-output '.paths | to_entries[] | select(.value.'"$1"' != null) | select(.key != "/") | "\"\(.key[1:])\",\" \(.value.'"$1"'.summary)\","' \
+  |  sed -e 's#gitignore/templates/{name}#gitignore/templates/{gitignore_name}#' \
+         -e 's#codes_of_conduct/{key}#codes_of_conduct/{coc_key}#' \
+         -e 's#labels/{name}#labels/{label}#'
+echo '  },'
+}
+
+method_endpoints delete
+method_endpoints get
+method_endpoints patch
+method_endpoints post
+method_endpoints put
+
+echo '}'
