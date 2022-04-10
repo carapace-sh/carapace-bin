d69cdb3e81576bd1d1f27c11c15b122f426357bf
rsteube <rsteube@users.noreply.github.com>
2022-04-10 11:23:12 +0200

    actions: http style config

---
+++ pkg/actions/net/http/method.go
@@ -0,0 +1,25 @@
+package http
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/styles"
+)
+
+// ActionRequestMethods completes request methods
+//   DELETE (The DELETE method deletes the specified resource.)
+//   GET (The GET method requests a representation of the specified resource.)
+func ActionRequestMethods() carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		return carapace.ActionStyledValuesDescribed(
+			"CONNECT", "Establish a tunnel to the server identified by the target resource", styles.CarapaceBin.HttpMethodCONNECT,
+			"DELETE", "Delete the specified resource", styles.CarapaceBin.HttpMethodDELETE,
+			"GET", "Request a representation of the specified resource", styles.CarapaceBin.HttpMethodGET,
+			"HEAD", "Identical to a GET request, but without the response body", styles.CarapaceBin.HttpMethodHEAD,
+			"OPTIONS", "Describe the communication options for the target resource", styles.CarapaceBin.HttpMethodOPTIONS,
+			"PATCH", "Apply partial modifications to a resource", styles.CarapaceBin.HttpMethodPATCH,
+			"POST", "Submit an entity to the specified resource", styles.CarapaceBin.HttpMethodPOST,
+			"PUT", "Replace all current representations of the target resource", styles.CarapaceBin.HttpMethodPUT,
+			"TRACE", "Perform a message loop-back test along the path to the target resource", styles.CarapaceBin.HttpMethodTRACE,
+		)
+	})
+}
--- pkg/styles/carapace-bin.go
+++ pkg/styles/carapace-bin.go
@@ -3,17 +3,37 @@ package styles
 import "github.com/rsteube/carapace/pkg/style"
 
 var CarapaceBin = struct {
-	HttpInformational string `desc:"http informational"`
-	HttpSuccessful    string `desc:"http successful"`
-	HttpRedirection   string `desc:"http redirection"`
-	HttpClientError   string `desc:"http client error"`
-	HttpServerError   string `desc:"http server error"`
+	HttpStatusInformational string `desc:"http informational"`
+	HttpStatusSuccessful    string `desc:"http successful"`
+	HttpStatusRedirection   string `desc:"http redirection"`
+	HttpStatusClientError   string `desc:"http client error"`
+	HttpStatusServerError   string `desc:"http server error"`
+
+	HttpMethodCONNECT string `desc:"http CONNECT method"`
+	HttpMethodDELETE  string `desc:"http DELETE method"`
+	HttpMethodGET     string `desc:"http GET method"`
+	HttpMethodHEAD    string `desc:"http HEAD method"`
+	HttpMethodOPTIONS string `desc:"http OPTIONS method"`
+	HttpMethodPATCH   string `desc:"http PATCH method"`
+	HttpMethodPOST    string `desc:"http POST method"`
+	HttpMethodPUT     string `desc:"http PUT method"`
+	HttpMethodTRACE   string `desc:"http TRACE method"`
 }{
-	HttpInformational: style.Blue,
-	HttpSuccessful:    style.Green,
-	HttpRedirection:   style.Yellow,
-	HttpClientError:   style.Red,
-	HttpServerError:   style.Magenta,
+	HttpStatusInformational: style.Blue,
+	HttpStatusSuccessful:    style.Green,
+	HttpStatusRedirection:   style.Yellow,
+	HttpStatusClientError:   style.Red,
+	HttpStatusServerError:   style.Magenta,
+
+	HttpMethodCONNECT: style.Default,
+	HttpMethodDELETE:  style.Red,
+	HttpMethodGET:     style.Blue,
+	HttpMethodHEAD:    style.Magenta,
+	HttpMethodOPTIONS: style.Of(style.Dim, style.Blue),
+	HttpMethodPATCH:   style.Green,
+	HttpMethodPOST:    style.Of(style.Dim, style.Green),
+	HttpMethodPUT:     style.Yellow,
+	HttpMethodTRACE:   style.Default,
 }
 
 func init() {
