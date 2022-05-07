99ceda33412afd3b11b67b890357e313ec040478
rsteube <rsteube@users.noreply.github.com>
2022-05-07 12:29:29 +0200

    doc: pass bin as artifact

--- .github/macros.sh
+++ .github/macros.sh
@@ -5,7 +5,6 @@ scriptdir=$(dirname $(readlink -f $0))
 echo "# Macros"
 echo
 
-grep -A1000 macroDescriptions $scriptdir/../cmd/carapace/cmd/macros.go \
-  | grep "\t" \
-  | sed 's_^\t"\([^"]*\.\)\([^. ]\+\)": \+"\(.*\)",$_- [\1\2]\(https://pkg.go.dev/github.com/rsteube/carapace-bin/pkg/actions/\1#Action\2) \3_' \
+$scriptdir/../cmd/carapace/carapace --macros \
+  | sed 's_^\([^ ]*\.\)\([^. ]\+\) \+\(.*\)_- [\1\2]\(https://pkg.go.dev/github.com/rsteube/carapace-bin/pkg/actions/\1#Action\2) \3_' \
   | sed -e ':loop' -e 's_\(carapace-bin/pkg/actions/[^#]*\)[.]_\1/_' -e 't loop' 
