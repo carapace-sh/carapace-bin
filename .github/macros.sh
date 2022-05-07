cc965e22eb001498fdfd8d74d4eb65c55b95d635
rsteube <rsteube@users.noreply.github.com>
2022-05-07 11:48:27 +0200

    tmp

---
+++ .github/macros.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "# Completers"
+echo
+
+carapace --macros \
+  | sed 's_^\([^ ]*\)\.\([^. ]\+\) \+\(.*\)_- [\1\2]\(https://pkg.go.dev/github.com/rsteube/carapace-bin/pkg/actions/\1#Action\2) \3_' \
+  | sed -e ':loop' -e 's_\(carapace-bin/pkg/actions/[^#]*\)[.]_\1/ _' -e 't loop' 
