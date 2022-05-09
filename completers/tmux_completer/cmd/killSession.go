92c128d76210c7d62accfb3d72072b02f60b31a4
rsteube <rsteube@users.noreply.github.com>
2022-05-09 23:26:07 +0200

    tmux: kill-session

--- completers/tmux_completer/cmd/killSession.go
+++ completers/tmux_completer/cmd/killSession.go
@@ -8,17 +8,20 @@ import (
 
 var killSessionCmd = &cobra.Command{
 	Use:   "kill-session",
-	Short: "",
+	Short: "destroy a given session",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 func init() {
 	carapace.Gen(killSessionCmd).Standalone()
 
-	killSessionCmd.Flags().StringS("t", "t", "", "target-session")
+	killSessionCmd.Flags().BoolS("C", "C", false, "clear alerts (bell, activity, silence) in all windows linked to the session")
+	killSessionCmd.Flags().BoolS("a", "a", false, "kill all session except the one specified by -t")
+	killSessionCmd.Flags().StringS("t", "t", "", "specify target session")
+
 	rootCmd.AddCommand(killSessionCmd)
 
 	carapace.Gen(killSessionCmd).FlagCompletion(carapace.ActionMap{
-		"t": tmux.ActionWindows(),
+		"t": tmux.ActionSessions(),
 	})
 }
