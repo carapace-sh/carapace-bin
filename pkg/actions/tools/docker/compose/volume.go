8787f94c8dfc52cc098938ac5048dbc9de0d5f08
rsteube <rsteube@users.noreply.github.com>
2022-05-12 23:59:19 +0200

    docker-compose: expose volumes macro

---
+++ pkg/actions/tools/docker/compose/volume.go
@@ -0,0 +1,19 @@
+package compose
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/styles"
+)
+
+// ActionVolumes completes volumes
+//   go (carapace-bin_go)
+//   example (carapace-bin_example)
+func ActionVolumes(files ...string) carapace.Action {
+	return actionConfig(files, func(c config) carapace.Action {
+		vals := make([]string, 0)
+		for name, volume := range c.Volumes {
+			vals = append(vals, name, volume.Name)
+		}
+		return carapace.ActionValuesDescribed(vals...).StyleR(&styles.Docker.Volume)
+	})
+}
