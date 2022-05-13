20db068f07810755d583743528a631a60f07f9b2
rsteube <rsteube@users.noreply.github.com>
2022-05-13 20:17:56 +0200

    gh: added ActionPinnedIssues

--- completers/gh_completer/cmd/action/issue.go
+++ completers/gh_completer/cmd/action/issue.go
@@ -71,6 +71,43 @@ func ActionIssues(cmd *cobra.Command, opts IssueOpts) carapace.Action {
 	})
 }
 
+type pinnedIssueQuery struct {
+	Data struct {
+		Repository struct {
+			PinnedIssues struct {
+				Nodes []struct {
+					Issue issue
+				}
+			}
+		}
+	}
+}
+
+func ActionPinnedIssues(cmd *cobra.Command) carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		var queryResult pinnedIssueQuery
+		return GraphQlAction(cmd, `repository(owner: $owner, name: $repo){ pinnedIssues(first: 3) { nodes { issue { number, title, state } } } }`, &queryResult, func() carapace.Action {
+			issues := queryResult.Data.Repository.PinnedIssues.Nodes
+			vals := make([]string, 0)
+			for _, issue := range issues {
+				s := style.Default
+				switch issue.Issue.State {
+				case "OPEN":
+					s = styles.Gh.StateOpen
+				case "CLOSED":
+					s = styles.Gh.StateClosed
+				case "MERGED":
+					s = styles.Gh.StateMerged
+				default:
+				}
+
+				vals = append(vals, strconv.Itoa(issue.Issue.Number), issue.Issue.Title, s)
+			}
+			return carapace.ActionStyledValuesDescribed(vals...)
+		})
+	})
+}
+
 func ActionIssueFields() carapace.Action {
 	return carapace.ActionValues(
 		"assignees",
