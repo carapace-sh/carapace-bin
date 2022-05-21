0b6b808ecf7cd60dd1fe0a5bfe3f0ca4e1f88c79
rsteube <rsteube@users.noreply.github.com>
2022-05-21 12:00:56 +0200

    added termux-apt-repo

---
+++ completers/termux-apt-repo_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "termux-apt-repo",
+	Short: "Create a repository with deb files",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("help", "h", false, "show this help message and exit")
+	rootCmd.Flags().Bool("use-hard-links", false, "use hard links instead of copying deb files. Will not work")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionDirectories(),
+		carapace.ActionDirectories(),
+	)
+}
---
+++ completers/termux-apt-repo_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/termux-apt-repo_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
