61a273ca5fe29d2aa1006dbf8b278f448c89f222
rsteube <rsteube@users.noreply.github.com>
2022-05-31 10:30:08 +0200

    added fc-conflist

---
+++ completers/fc-conflist_completer/cmd/root.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "fc-conflist",
+	Short: "list the configuration files processed by Fontconfig",
+	Long:  "https://manpages.debian.org/testing/fontconfig/fc-conflist.1.en.html",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("help", "h", false, "display this help and exit")
+	rootCmd.Flags().BoolP("version", "V", false, "display font config version and exit")
+}
