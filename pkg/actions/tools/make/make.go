c5bc84feb72917a784f5236bcdd67df0e6d839b8
rsteube <rsteube@users.noreply.github.com>
2022-06-07 23:42:05 +0200

    make: expose macro

---
+++ pkg/actions/tools/make/make.go
@@ -0,0 +1,44 @@
+package make
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+// ActionTargets completes targets
+//   build
+//   check
+func ActionTargets(file string) carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		if file == "" {
+			file = "Makefile"
+		}
+
+		cmd := c.Command("make", "-qp", "--file", file)
+		if output, err := cmd.Output(); err != nil && cmd.ProcessState.ExitCode() != 1 {
+			return carapace.ActionMessage(err.Error())
+		} else {
+			lines := strings.Split(string(output), "\n")
+			r := regexp.MustCompile(`^(?P<target>[a-zA-Z0-9][^$#\/\t=]*):([^=]|$)`)
+
+			vals := make([]string, 0)
+			skip := false
+			for _, line := range lines {
+				if strings.HasPrefix(line, "# Not a target:") {
+					skip = true
+					continue
+				} else if skip {
+					skip = false
+					continue
+				}
+
+				if r.MatchString(line) {
+					vals = append(vals, r.FindStringSubmatch(line)[1])
+				}
+			}
+			return carapace.ActionValues(vals...)
+		}
+	})
+}
