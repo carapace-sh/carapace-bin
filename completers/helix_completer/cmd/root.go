e774d7ad22a194d487f1b4a304ae7e3b9b4f3f60
rsteube <rsteube@users.noreply.github.com>
2022-06-27 12:45:44 +0200

    added helix

---
+++ completers/helix_completer/cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "helix",
+	Short: "A post-modern text editor",
+	Long:  "https://helix-editor.com/",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().StringP("grammar", "g", "", "Fetches or builds tree-sitter grammars listed in languages.toml")
+	rootCmd.Flags().String("health", "", "Checks for potential errors in editor setup")
+	rootCmd.Flags().BoolP("help", "h", false, "Prints help information")
+	rootCmd.Flags().Bool("tutor", false, "Loads the tutorial")
+	rootCmd.Flags().BoolS("v", "v", false, "Increases logging verbosity each use for up to 3 times")
+	rootCmd.Flags().BoolP("version", "V", false, "Prints version information")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"grammar": carapace.ActionValues("fetch", "build"),
+	})
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		carapace.ActionFiles(),
+	)
+}
