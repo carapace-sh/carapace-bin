79afaa0f2b66b9eb6071237e70ea890e7aad5ab6
rsteube <rsteube@users.noreply.github.com>
2022-08-08 23:43:30 +0200

    added slides

---
+++ completers/slides_completer/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "slides",
+	Short: "Terminal based presentation tool",
+	Long:  "https://maaslalani.com/slides/",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("help", "h", false, "help for slides")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionFiles(".md"),
+	)
+}
