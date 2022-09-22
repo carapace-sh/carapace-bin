0a8b588aa92dcc68b62153ab4b34dd2a1c67ee85
rsteube <rsteube@users.noreply.github.com>
2022-09-22 21:14:41 +0200

    added git-abort

---
+++ completers/git-abort_completer/cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "git-abort",
+	Short: "Abort current rebase, merge or cherry-pick, without the need to find exact command in history",
+	Long:  "https://github.com/tj/git-extras/blob/master/Commands.md#git-abort",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().Bool("help", false, "show help")
+}
