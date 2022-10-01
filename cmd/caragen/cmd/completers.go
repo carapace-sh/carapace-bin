1beeaf0bddba7325e928f6bb4d47028b754a5a9d
rsteube <rsteube@users.noreply.github.com>
2022-10-01 16:37:53 +0200

    tmp

--- cmd/caragen/cmd/completers.go
+++ cmd/caragen/cmd/completers.go
@@ -17,7 +17,7 @@ var completersCmd = &cobra.Command{
 	Args:      cobra.ExactArgs(1),
 	ValidArgs: []string{"common", "darwin", "linux", "unix", "windows"},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		completers, err := readCompleters(args[0])
+		completers, err := applicableCompleters(args[0])
 		if err != nil {
 			return err
 		}
@@ -27,7 +27,7 @@ var completersCmd = &cobra.Command{
 			return err
 		}
 
-		fmt.Println(cmd.OutOrStdout(), string(m))
+		fmt.Fprintln(cmd.OutOrStdout(), string(m))
 		return nil
 	},
 }
@@ -40,8 +40,8 @@ func init() {
 	carapace.Gen(completersCmd).PositionalCompletion(
 		carapace.ActionValuesDescribed(
 			"common", "all ${GOOS}",
-			"darwin", "common and macOS",
-			"linux", "common and linux",
+			"darwin", "common, unix and macOS",
+			"linux", "common, unix and linux",
 			"unix", "common, linux and macOS",
 			"windows", "common and windows",
 		),
@@ -51,6 +51,35 @@ func init() {
 type completer struct {
 	Description string
 	Path        string
+}
+
+func applicableCompleters(goos string) (map[string]completer, error) {
+	gooses := make([]string, 0)
+	switch goos {
+	case "common":
+		gooses = append(gooses, "common")
+	case "darwin":
+		gooses = append(gooses, "common", "unix", "darwin")
+	case "linux":
+		gooses = append(gooses, "common", "unix", "linux")
+	case "unix":
+		gooses = append(gooses, "common", "unix")
+	case "windows":
+		gooses = append(gooses, "common", "windows")
+	}
+
+	mergedCompleters := make(map[string]completer, 0)
+	for _, goos := range gooses {
+		completers, err := readCompleters(goos)
+		if err != nil {
+			return nil, err
+		}
+
+		for name, completer := range completers {
+			mergedCompleters[name] = completer
+		}
+	}
+	return mergedCompleters, nil
 }
 
 func readCompleters(goos string) (map[string]completer, error) {
@@ -67,7 +96,7 @@ func readCompleters(goos string) (map[string]completer, error) {
 				if file.IsDir() && strings.HasSuffix(file.Name(), "_completer") {
 					name := strings.TrimSuffix(file.Name(), "_completer")
 					completers[name] = completer{
-						Description: readDescription(root, file.Name()),
+						Description: readDescription(fmt.Sprintf("%v/%v", dir, file.Name())),
 						Path:        dir,
 					}
 				}
@@ -77,8 +106,8 @@ func readCompleters(goos string) (map[string]completer, error) {
 	return completers, nil
 }
 
-func readDescription(root string, completer string) string {
-	if content, err := os.ReadFile(fmt.Sprintf("%v/completers/%v/cmd/root.go", root, completer)); err == nil {
+func readDescription(path string) string {
+	if content, err := os.ReadFile(fmt.Sprintf("%v/cmd/root.go", path)); err == nil {
 		re := regexp.MustCompile("^\tShort: +\"(?P<description>.*)\",$")
 		for _, line := range strings.Split(string(content), "\n") {
 			if re.MatchString(line) {
