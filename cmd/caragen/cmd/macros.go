73671840b3a359f25ddcca0aea630d526a341d71
rsteube <rsteube@users.noreply.github.com>
2022-10-02 15:05:21 +0200

    tmp

--- cmd/caragen/cmd/macros.go
+++ cmd/caragen/cmd/macros.go
@@ -47,36 +47,33 @@ func (m macro) FlagParsingDisabled() bool {
 	return m.Package == "bridge"
 }
 
-func macros() []macro {
+func macros() map[string]macro {
 	root, err := rootDir()
 	if err != nil {
 		panic(err.Error)
 	}
 
-	macros := make([]macro, 0)
+	macros := make(map[string]macro)
 	descriptions := make(map[string]string)
-
 	r := regexp.MustCompile(`^func Action(?P<name>[^(]+)\((?P<arg>[^(]*)\) carapace.Action {$`)
 	filepath.WalkDir(root+"/pkg/actions", func(path string, d fs.DirEntry, err error) error {
 		if !d.IsDir() && strings.HasSuffix(path, ".go") {
 			file, err := os.Open(path)
 			if err != nil {
-				log.Fatal(err)
+				log.Fatal(err.Error())
 			}
 			defer file.Close()
 
 			scanner := bufio.NewScanner(file)
 			for scanner.Scan() {
-				_macro := &macro{}
+				_macro := macro{}
 				_macro.Package = strings.Replace(filepath.Dir(strings.TrimPrefix(path, root+"/pkg/actions/")), "/", ".", -1)
 
 				if t := scanner.Text(); strings.HasPrefix(t, "func Action") {
 					if r.MatchString(t) {
 						matches := r.FindStringSubmatch(t)
-						//_macro.ImportPath = fmt.Sprintf(`	%v "github.com/rsteube/carapace-bin/pkg/actions/%v"`, strings.Replace(_macro.Package, ".", "_", -1), strings.Replace(_macro.Package, ".", "/", -1))
 						_macro.ImportPath = fmt.Sprintf("github.com/rsteube/carapace-bin/pkg/actions/%v", strings.Replace(_macro.Package, ".", "/", -1))
-						_macro.Function = fmt.Sprintf("%v.Action%v", strings.Replace(_macro.Package, ".", "_", -1), matches[1])
-						macros = append(macros, *_macro)
+						macros[matches[1]] = _macro
 					}
 				} else if strings.HasPrefix(t, "// Action") {
 					if splitted := strings.SplitN(strings.TrimPrefix(t, "// Action"), " ", 2); len(splitted) > 1 {
