33934b8293d7e76d0cd26f40fe4f4415192ebddd
rsteube <rsteube@users.noreply.github.com>
2022-10-05 20:50:55 +0200

    added soft

---
+++ completers/soft_completer/cmd/serve.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the server",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(serveCmd).Standalone()
+	rootCmd.AddCommand(serveCmd)
+}
