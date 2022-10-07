815e1675eabdb0ac4b82ae4e13e69dc78c19ca39
Bruno Roque <brunoroque06@gmail.com>
2022-10-07 22:06:04 +0200

    Fix pulumi url completion

--- completers/pulumi_completer/cmd/action/urn.go
+++ completers/pulumi_completer/cmd/action/urn.go
@@ -1,60 +1,53 @@
 package action
 
 import (
-	"path/filepath"
-	"regexp"
-	"strings"
-	"time"
-
+	"encoding/json"
 	"github.com/rsteube/carapace"
 	"github.com/rsteube/carapace/pkg/cache"
 	"github.com/spf13/cobra"
+	"path/filepath"
+	"time"
 )
 
-type resource struct {
-	Urn string
-	Id  string
+type StackExport struct {
+	Deployment struct {
+		Resources []struct {
+			Urn string `json:"urn"`
+		} `json:"resources"`
+	} `json:"deployment"`
 }
 
 func ActionUrns(cmd *cobra.Command) carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-		return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-			cwd := cmd.Flag("cwd").Value.String()
-			stack := cmd.Flag("stack").Value.String()
+		cwd := cmd.Flag("cwd").Value.String()
+		stack := cmd.Flag("stack").Value.String()
 
-			c.Setenv("PULUMI_SKIP_UPDATE_CHECK", "1")
-			return carapace.ActionExecCommand("pulumi", "--cwd", cwd, "stack", "--stack", stack, "--show-urns")(func(output []byte) carapace.Action {
-				reUrn := regexp.MustCompile(`URN: (?P<urn>urn:pulumi.*)`)
-				reId := regexp.MustCompile(`ID: (?P<id>.*)`)
+		c.Setenv("PULUMI_SKIP_UPDATE_CHECK", "1")
+		return carapace.ActionExecCommand("pulumi", "stack", "export", "--cwd", cwd, "--stack", stack)(func(output []byte) carapace.Action {
+			var exp StackExport
+			if err := json.Unmarshal(output, &exp); err != nil {
+				return carapace.ActionMessage(err.Error())
+			}
+			urns := make([]string, len(exp.Deployment.Resources))
+			for i, r := range exp.Deployment.Resources {
+				urns[i] = r.Urn
+			}
+			return carapace.ActionValues(urns...)
+		}).Cache(5*time.Second,
+			func() (string, error) {
+				workdir := c.Dir
+				if cmd.Flag("cwd").Changed { // TODO use preinvoke?
+					workdir = cmd.Flag("cwd").Value.String()
+				}
+				stack := cmd.Flag("stack").Value.String()
 
-				resources := make([]resource, 0)
-				for _, line := range strings.Split(string(output), "\n") {
-					if reUrn.MatchString(line) {
-						resources = append(resources, resource{Urn: reUrn.FindStringSubmatch(line)[1]})
-					}
-					if reId.MatchString(line) {
-						resources[len(resources)-1].Id = reId.FindStringSubmatch(line)[1]
-					}
+				absWd, err := filepath.Abs(workdir)
+				if err != nil {
+					return "", err
 				}
 
-				vals := make([]string, 0)
-				for _, resource := range resources {
-					vals = append(vals, resource.Urn, resource.Id)
-				}
-				return carapace.ActionValuesDescribed(vals...)
-			}).Invoke(c).ToA()
-		}).Cache(5*time.Second, func() (string, error) {
-			workdir := c.Dir
-			if cmd.Flag("cwd").Changed { // TODO use preinvoke?
-				workdir = cmd.Flag("cwd").Value.String()
-			}
-			stack := cmd.Flag("stack").Value.String()
-
-			absWd, err := filepath.Abs(workdir)
-			if err != nil {
-				return "", err
-			}
-			return cache.String(absWd, stack)()
-		}).Invoke(c).ToMultiPartsA("::")
+				return cache.String(absWd, stack)()
+			},
+		).Invoke(c).ToA()
 	})
 }
