372c0563b608f64430e9b2e236d3a0e959cd9aa7
rsteube <rsteube@users.noreply.github.com>
2022-10-27 19:28:26 +0200

    helix: updated flags

--- completers/helix_completer/cmd/root.go
+++ completers/helix_completer/cmd/root.go
@@ -25,16 +25,22 @@ func ExecuteHx() error {
 func init() {
 	carapace.Gen(rootCmd).Standalone()
 
+	rootCmd.Flags().StringP("config", "c", "", "Specifies a file to use for configuration")
 	rootCmd.Flags().StringP("grammar", "g", "", "Fetches or builds tree-sitter grammars listed in languages.toml")
 	rootCmd.Flags().String("health", "", "Checks for potential errors in editor setup")
 	rootCmd.Flags().BoolP("help", "h", false, "Prints help information")
+	rootCmd.Flags().Bool("hsplit", false, "Splits all given files horizontally into different windows")
+	rootCmd.Flags().String("log", "", "Specifies a file to use for logging")
 	rootCmd.Flags().Bool("tutor", false, "Loads the tutorial")
-	rootCmd.Flags().BoolS("v", "v", false, "Increases logging verbosity each use for up to 3 times")
+	rootCmd.Flags().CountS("v", "v", "Increases logging verbosity each use for up to 3 times")
 	rootCmd.Flags().BoolP("version", "V", false, "Prints version information")
+	rootCmd.Flags().Bool("vsplit", false, "Splits all given files vertically into different windows")
 
 	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"config":  carapace.ActionFiles(),
 		"grammar": carapace.ActionValues("fetch", "build"),
 		"health":  helix.ActionLanguages(),
+		"log":     carapace.ActionFiles(),
 	})
 
 	carapace.Gen(rootCmd).PositionalAnyCompletion(
