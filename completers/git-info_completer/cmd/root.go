b8bf03156add638dfff8e0be8dee723bfb977879
rsteube <rsteube@users.noreply.github.com>
2023-01-02 16:44:40 +0100

    added git-info

---
+++ completers/git-info_completer/cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "git-info",
+	Short: "Returns information on current repository",
+	Long:  "https://github.com/tj/git-extras/blob/master/Commands.md#git-info",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("color", "c", false, "Use color for information titles")
+	rootCmd.Flags().Bool("help", false, "Display help message")
+	rootCmd.Flags().Bool("no-config", false, "Don't show list all variables set in config file, along with their values")
+}
