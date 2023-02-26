c5634dc7c2c9956bc7ca1884d197c5e8e986fede
rsteube <rsteube@users.noreply.github.com>
2023-02-26 16:45:37 +0100

    git: write-tree

---
+++ completers/git_completer/cmd/writeTree.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var writeTreeCmd = &cobra.Command{
+	Use:     "write-tree",
+	Short:   "Create a tree object from the current index",
+	Run:     func(cmd *cobra.Command, args []string) {},
+	GroupID: groups[group_low_level_manipulator].ID,
+}
+
+func init() {
+	carapace.Gen(writeTreeCmd).Standalone()
+
+	writeTreeCmd.Flags().Bool("missing-ok", false, "allow missing objects")
+	writeTreeCmd.Flags().String("prefix", "", "write tree object for a subdirectory <prefix>")
+	rootCmd.AddCommand(writeTreeCmd)
+}
