f9efa4997fb614b3853e8542e60dfe913bb55cc5
rsteube <rsteube@users.noreply.github.com>
2023-03-07 10:30:59 +0100

    goreleaser: updates from v1.16.0

---
+++ completers/goreleaser_completer/cmd/healthcheck.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var healthcheckCmd = &cobra.Command{
+	Use:     "healthcheck",
+	Short:   "Checks if needed tools are installed",
+	Aliases: []string{"hc"},
+	Run:     func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(healthcheckCmd).Standalone()
+
+	healthcheckCmd.Flags().StringP("config", "f", "", "Configuration file")
+	healthcheckCmd.Flags().BoolP("quiet", "q", false, "Quiet mode: no output")
+	rootCmd.AddCommand(healthcheckCmd)
+
+	carapace.Gen(healthcheckCmd).FlagCompletion(carapace.ActionMap{
+		"config": carapace.ActionFiles(),
+	})
+}
