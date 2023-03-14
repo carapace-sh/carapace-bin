38cb4f95e1b279392e98cab6d40d2fd5cfea0cf2
rsteube <rsteube@users.noreply.github.com>
2023-03-14 08:57:59 +0100

    doctl: fix flags

--- completers/doctl_completer/cmd/root.go
+++ completers/doctl_completer/cmd/root.go
@@ -7,10 +7,11 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "doctl",
-	Short: "doctl is a command line interface (CLI) for the DigitalOcean API",
-	Long:  "https://docs.digitalocean.com/reference/doctl/",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Use:                "doctl",
+	Short:              "doctl is a command line interface (CLI) for the DigitalOcean API",
+	Long:               "https://docs.digitalocean.com/reference/doctl/",
+	Run:                func(cmd *cobra.Command, args []string) {},
+	DisableFlagParsing: true,
 }
 
 func Execute() error {
