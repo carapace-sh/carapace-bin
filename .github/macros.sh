dd50677035e0853b6db9f594156771c5432b3b0d
rsteube <rsteube@users.noreply.github.com>
2023-03-31 13:29:36 +0200

    doc: fix url for bridge macros

--- .github/macros.sh
+++ .github/macros.sh
@@ -8,4 +8,5 @@ echo
 $scriptdir/../cmd/carapace/carapace --macros \
   | sed -r 's_(https://[^ ]+)_[\1](\1)_' \
   | sed 's_^\([^ ]*\.\)\([^. ]\+\) \+\(.*\)_- [\1\2]\(https://pkg.go.dev/github.com/rsteube/carapace-bin/pkg/actions/\1#Action\2) \3_' \
-  | sed -e ':loop' -e 's_\(carapace-bin/pkg/actions/[^#]*\)[.]_\1/_' -e 't loop' 
+  | sed -e ':loop' -e 's_\(carapace-bin/pkg/actions/[^#]*\)[.]_\1/_' -e 't loop' \
+  | sed 's_https://pkg.go.dev/github.com/rsteube/carapace-bin/pkg/actions/bridge_https://pkg.go.dev/github.com/rsteube/carapace-bridge/pkg/actions/bridge_'
