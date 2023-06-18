16d12e703e4af9c0bbeae1fdffcef3b8f4650c44
rsteube <rsteube@users.noreply.github.com>
2023-06-18 20:09:41 +0200

    gh: updates from 2.30.0

--- completers/gh_completer/cmd/codespace_list.go
+++ completers/gh_completer/cmd/codespace_list.go
@@ -19,10 +19,9 @@ func init() {
 
 	codespace_listCmd.Flags().StringP("jq", "q", "", "Filter JSON output using a jq `expression`")
 	codespace_listCmd.Flags().StringSlice("json", []string{}, "Output JSON with the specified `fields`")
-	codespace_listCmd.Flags().IntP("limit", "L", 30, "Maximum number of codespaces to list")
+	codespace_listCmd.Flags().StringP("limit", "L", "", "Maximum number of codespaces to list")
 	codespace_listCmd.Flags().StringP("org", "o", "", "The `login` handle of the organization to list codespaces for (admin-only)")
 	codespace_listCmd.Flags().StringP("repo", "R", "", "Repository name with owner: user/repo")
-	codespace_listCmd.Flags().StringP("repo-deprecated", "r", "", "(Deprecated) Shorthand for --repo")
 	codespace_listCmd.Flags().StringP("template", "t", "", "Format JSON output using a Go template; see \"gh help formatting\"")
 	codespace_listCmd.Flags().StringP("user", "u", "", "The `username` to list codespaces for (used with --org)")
 	codespace_listCmd.Flags().BoolP("web", "w", false, "List codespaces in the web browser, cannot be used with --user or --org")
