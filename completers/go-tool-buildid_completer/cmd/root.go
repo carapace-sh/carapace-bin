d237a62c637385447c654d6da4f6d16d06d184dc
rsteube <rsteube@users.noreply.github.com>
2023-07-17 16:14:11 +0200

    added go-tool-buildid

---
+++ completers/go-tool-buildid_completer/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "go-tool-buildid",
+	Short: "Buildid displays or updates the build ID stored in a Go package or binary.",
+	Long:  "https://pkg.go.dev/cmd/buildid",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolS("w", "w", false, "write build ID")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionFiles(),
+	)
+}
---
+++ completers/go-tool-buildid_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/go-tool-buildid_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
