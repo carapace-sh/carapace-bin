e29c8c9a2c76736903382b326c9fdac8d0c3e3b2
rsteube <rsteube@users.noreply.github.com>
2023-07-18 22:37:36 +0200

    go: doc - supported unexported
    - added missing flags

--- pkg/actions/tools/golang/symbol.go
+++ pkg/actions/tools/golang/symbol.go
@@ -10,43 +10,56 @@ import (
 	"github.com/rsteube/carapace/pkg/style"
 )
 
+type SymbolOpts struct {
+	Package    string
+	Unexported bool
+}
+
 // ActionSymbols completes symbols of given package
 //
 //	Action
 //	ActionCallback
-func ActionSymbols(pkg string) carapace.Action {
-	return carapace.ActionExecCommand("go", "doc", "-short", pkg)(func(output []byte) carapace.Action {
-		lines := strings.Split(string(output), "\n")
-		r := regexp.MustCompile(`^ *(?P<type>var|func|type|const) (?P<symbol>[^( =]+).*`) // TODO incomplete (e.g. generics))
-
-		styleFor := func(s string) string {
-			switch s {
-			case "var":
-				return styles.Golang.Variable
-			case "func":
-				return styles.Golang.Function
-			case "type":
-				return styles.Golang.Type
-			case "const":
-				return styles.Golang.Constant
-			default:
-				return style.Default
-			}
+func ActionSymbols(opts SymbolOpts) carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		args := []string{"doc", "-short"}
+		if opts.Unexported {
+			args = append(args, "-u")
 		}
+		args = append(args, opts.Package)
+		return carapace.ActionExecCommand("go", args...)(func(output []byte) carapace.Action {
+			lines := strings.Split(string(output), "\n")
+			r := regexp.MustCompile(`^ *(?P<type>var|func|type|const) (?P<symbol>[^( =]+).*`) // TODO incomplete (e.g. generics))
 
-		vals := make([]string, 0)
-		for _, line := range lines {
-			if matches := r.FindStringSubmatch(line); matches != nil {
-				vals = append(vals, matches[2], styleFor(matches[1]))
+			styleFor := func(s string) string {
+				switch s {
+				case "var":
+					return styles.Golang.Variable
+				case "func":
+					return styles.Golang.Function
+				case "type":
+					return styles.Golang.Type
+				case "const":
+					return styles.Golang.Constant
+				default:
+					return style.Default
+				}
 			}
-		}
-		return carapace.ActionStyledValues(vals...)
+
+			vals := make([]string, 0)
+			for _, line := range lines {
+				if matches := r.FindStringSubmatch(line); matches != nil {
+					vals = append(vals, matches[2], styleFor(matches[1]))
+				}
+			}
+			return carapace.ActionStyledValues(vals...)
+		})
 	})
 }
 
 type MethodOrFieldOpts struct {
-	Package string
-	Symbol  string
+	Package    string
+	Symbol     string
+	Unexported bool
 }
 
 // ActionMethodOrFields completes methods and fields of given symbol
@@ -54,43 +67,50 @@ type MethodOrFieldOpts struct {
 //	Cache
 //	Chdir
 func ActionMethodOrFields(opts MethodOrFieldOpts) carapace.Action {
-	return carapace.ActionExecCommand("go", "doc", "-short", opts.Package, opts.Symbol)(func(output []byte) carapace.Action {
-		lines := strings.Split(string(output), "\n")
-		r := regexp.MustCompile(fmt.Sprintf(`^func \([^ ]+ \*?%v\) (?P<method>[^( =]+).*`, opts.Symbol))
-
-		methods := make([]string, 0)
-		for _, line := range lines {
-			if matches := r.FindStringSubmatch(line); matches != nil {
-				methods = append(methods, matches[1])
-			}
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		args := []string{"doc", "-short"}
+		if opts.Unexported {
+			args = append(args, "-u")
 		}
+		args = append(args, opts.Package, opts.Symbol)
+		return carapace.ActionExecCommand("go", args...)(func(output []byte) carapace.Action {
+			lines := strings.Split(string(output), "\n")
+			r := regexp.MustCompile(fmt.Sprintf(`^func \([^ ]+ \*?%v\) (?P<method>[^( =]+).*`, opts.Symbol))
 
-		found := false
-		fields := make([]string, 0)
-		for _, line := range lines {
-			if strings.HasPrefix(line, fmt.Sprintf("type %v ", opts.Symbol)) {
-				found = true
-				continue
+			methods := make([]string, 0)
+			for _, line := range lines {
+				if matches := r.FindStringSubmatch(line); matches != nil {
+					methods = append(methods, matches[1])
+				}
 			}
 
-			if !found {
-				continue
+			found := false
+			fields := make([]string, 0)
+			for _, line := range lines {
+				if strings.HasPrefix(line, fmt.Sprintf("type %v ", opts.Symbol)) && strings.HasSuffix(line, "{") {
+					found = true
+					continue
+				}
+
+				if !found {
+					continue
+				}
+
+				if strings.HasPrefix(line, "}") {
+					break
+				}
+
+				line = strings.SplitN(line, "//", 2)[0]
+				line = strings.TrimSpace(line)
+				if f := strings.Fields(line); len(f) > 1 {
+					fields = append(fields, f[0])
+				}
 			}
 
-			if strings.HasPrefix(line, "}") {
-				break
-			}
-
-			line = strings.SplitN(line, "//", 2)[0]
-			line = strings.TrimSpace(line)
-			if f := strings.Fields(line); len(f) > 1 {
-				fields = append(fields, f[0])
-			}
-		}
-
-		return carapace.Batch(
-			carapace.ActionValues(methods...).Style(styles.Golang.Function),
-			carapace.ActionValues(fields...).Style(styles.Golang.Field),
-		).ToA()
+			return carapace.Batch(
+				carapace.ActionValues(methods...).Style(styles.Golang.Function),
+				carapace.ActionValues(fields...).Style(styles.Golang.Field),
+			).ToA()
+		})
 	})
 }
