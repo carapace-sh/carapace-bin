f82a2d5dcceb3febf57159c8648f1332bb21620e
rsteube <rsteube@users.noreply.github.com>
2023-07-26 10:25:11 +0200

    added paru

---
+++ completers/paru_completer/cmd/upgrade.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var upgradeCmd = &cobra.Command{
+	Use:     "upgrade",
+	Aliases: []string{"U"},
+	Short:   "Upgrade or add packages",
+	Run:     func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(upgradeCmd).Standalone()
+
+	upgradeCmd.Flags().BoolP("install", "i", false, "Install package as well as building")
+
+	carapace.Gen(upgradeCmd).PositionalAnyCompletion(
+		carapace.ActionFiles(),
+	)
+}
