5886065ba36a3ae7670d36ae404b441277168cac
rsteube <rsteube@users.noreply.github.com>
2023-07-26 23:07:30 +0200

    added go-tool-fix

---
+++ completers/go-tool-fix_completer/cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/golang"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "fix",
+	Short: "Fix finds Go programs that use old APIs",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolS("diff", "diff", false, "display diffs instead of rewriting files")
+	rootCmd.Flags().StringS("force", "force", "", "force these fixes to run even if the code looks updated")
+	rootCmd.Flags().StringS("go", "go", "", "go language version for files")
+	rootCmd.Flags().StringS("r", "r", "", "restrict the rewrites to this comma-separated list")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"force": golang.ActionRewrites().UniqueList(","),
+		"go":    golang.ActionVersions(),
+		"r":     golang.ActionRewrites().UniqueList(","),
+	})
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		carapace.ActionFiles(),
+	)
+}
