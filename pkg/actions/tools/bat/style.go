06d7055567fbcc1596addb05ba9a66209ea95dfb
rsteube <rsteube@users.noreply.github.com>
2023-09-10 00:34:53 +0200

    bat: updates from 0.23.0

---
+++ pkg/actions/tools/bat/style.go
@@ -0,0 +1,24 @@
+package bat
+
+import "github.com/rsteube/carapace"
+
+// ActionStyles completes styles
+//
+//	auto (same as 'default', unless the output is piped)
+//	changes (show Git modification markers)
+func ActionStyles() carapace.Action {
+	return carapace.ActionValuesDescribed(
+		"default", "enables recommended style components (default)",
+		"full", "enables all available components",
+		"auto", "same as 'default', unless the output is piped",
+		"plain", "disables all available components",
+		"changes", "show Git modification markers",
+		"header", "alias for 'header-filename'",
+		"header-filename", "show filenames before the content",
+		"header-filesize", "show file sizes before the content",
+		"grid", "vertical/horizontal lines to separate side bar and the header from the content",
+		"rule", "horizontal lines to delimit files",
+		"numbers", "show line numbers in the side bar",
+		"snip", "draw separation lines between distinct line ranges",
+	)
+}
