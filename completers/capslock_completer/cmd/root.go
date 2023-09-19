3fcace49ad7cf0a48608daa957ad5a1e118097b8
rsteube <rsteube@users.noreply.github.com>
2023-09-19 10:44:08 +0200

    added capslock

---
+++ completers/capslock_completer/cmd/root.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/golang"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "capslock",
+	Short: "Capslock is a capability analysis CLI for Go packages ",
+	Long:  "https://github.com/google/capslock",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().StringS("buildtags", "buildtags", "", "command-separated list of build tags to use when loading packages")
+	rootCmd.Flags().StringS("goarch", "goarch", "", "GOARCH value to use when loading packages")
+	rootCmd.Flags().StringS("goos", "goos", "", "GOOS value to use when loading packages")
+	rootCmd.Flags().BoolS("noisy", "noisy", false, "include output on unanalyzed function calls")
+	rootCmd.Flags().StringS("output", "output", "", "output mode to use")
+	rootCmd.Flags().StringS("packages", "packages", "", "target patterns to be analysed")
+
+	carapace.Gen(rootCmd).FlagCompletion(carapace.ActionMap{
+		"buildtags": golang.ActionBuildTags(),
+		"goarch":    golang.ActionArchitectures(),
+		"goos":      golang.ActionOperatingSystems(),
+		"output":    carapace.ActionValues("json", "m", "v", "graph", "compare"),
+		"packages":  golang.ActionPackages(),
+	})
+}
---
+++ completers/capslock_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rsteube/carapace-bin/completers/capslock_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
