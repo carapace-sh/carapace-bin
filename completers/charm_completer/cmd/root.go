6d50ed4cf98309ac2094114f382c68a0840f8d32
rsteube <rsteube@users.noreply.github.com>
2023-10-03 18:37:06 +0200

    charm: added missing link

--- completers/charm_completer/cmd/root.go
+++ completers/charm_completer/cmd/root.go
@@ -8,6 +8,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "charm",
 	Short: "Do Charm stuff",
+	Long:  "https://github.com/charmbracelet/charm",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
