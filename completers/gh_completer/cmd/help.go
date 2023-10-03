6fd6911eee100e10190609b9ea978159da920bad
rsteube <rsteube@users.noreply.github.com>
2023-10-03 15:25:45 +0200

    gh: added help completion

--- completers/gh_completer/cmd/help.go
+++ completers/gh_completer/cmd/help.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
-	"github.com/rsteube/carapace/pkg/style"
 	"github.com/spf13/cobra"
 )
 
@@ -19,15 +18,7 @@ func init() {
 
 	carapace.Gen(helpCmd).PositionalCompletion(
 		carapace.Batch(
-			carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-				vals := make([]string, 0)
-				for _, cmd := range helpCmd.Root().Commands() {
-					if !cmd.Hidden {
-						vals = append(vals, cmd.Name(), cmd.Short)
-					}
-				}
-				return carapace.ActionValuesDescribed(vals...)
-			}).Tag("commands"),
+			carapace.ActionCommands(rootCmd),
 			carapace.ActionValuesDescribed(
 				"actions", "Learn about working with GitHub Actions",
 				"environment", "Environment variables that can be used with gh",
@@ -35,7 +26,11 @@ func init() {
 				"formatting", "Formatting options for JSON data exported from gh",
 				"mintty", "Information about using gh with MinTTY",
 				"reference", "A comprehensive reference of all gh commands",
-			).Style(style.Blue).Tag("help topics"),
+			).Tag("help topics"),
 		).ToA(),
+	)
+
+	carapace.Gen(helpCmd).PositionalAnyCompletion(
+		carapace.ActionCommands(rootCmd),
 	)
 }
