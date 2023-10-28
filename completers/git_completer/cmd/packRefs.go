fc515a7bf15e06aaff291abba22dbf82f7752460
rsteube <rsteube@users.noreply.github.com>
2023-10-28 11:18:39 +0200

    git: pack-refs

---
+++ completers/git_completer/cmd/packRefs.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var packRefsCmd = &cobra.Command{
+	Use:     "pack-refs",
+	Short:   "Pack heads and tags for efficient repository access",
+	GroupID: groups[group_manipulator].ID,
+	Run:     func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(packRefsCmd).Standalone()
+
+	packRefsCmd.Flags().Bool("all", false, "pack everything")
+	packRefsCmd.Flags().String("exclude", "", "references to exclude")
+	packRefsCmd.Flags().String("include", "", "references to include")
+	packRefsCmd.Flags().Bool("prune", false, "prune loose refs (default)")
+	rootCmd.AddCommand(packRefsCmd)
+}
