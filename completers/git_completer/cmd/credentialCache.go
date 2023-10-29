d55806a3a26ea7ec5d3215830f1ac1cbcdc66b8c
rsteube <rsteube@users.noreply.github.com>
2023-10-29 10:26:50 +0100

    git: credential-cache

---
+++ completers/git_completer/cmd/credentialCache.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var credentialCacheCmd = &cobra.Command{
+	Use:     "credential-cache",
+	Short:   "Helper to temporarily store passwords in memory",
+	Run:     func(cmd *cobra.Command, args []string) {},
+	GroupID: groups[group_low_level_helper].ID,
+}
+
+func init() {
+	carapace.Gen(credentialCacheCmd).Standalone()
+
+	credentialCacheCmd.Flags().String("socket", "", "path of cache-daemon socket")
+	credentialCacheCmd.Flags().String("timeout", "", "number of seconds to cache credentials")
+	rootCmd.AddCommand(credentialCacheCmd)
+
+	carapace.Gen(credentialCacheCmd).FlagCompletion(carapace.ActionMap{
+		"socket": carapace.ActionFiles(),
+	})
+}
