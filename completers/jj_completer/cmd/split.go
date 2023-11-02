a751598fabd88f2cb21bba92a6e520bc32f2778a
rsteube <rsteube@users.noreply.github.com>
2023-11-02 16:09:56 +0100

    jj: split

--- completers/jj_completer/cmd/split.go
+++ completers/jj_completer/cmd/split.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var splitCmd = &cobra.Command{
-	Use:   "split",
+	Use:   "split [OPTIONS] [PATHS]...",
 	Short: "Split a revision in two",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -18,4 +19,14 @@ func init() {
 	splitCmd.Flags().BoolP("interactive", "i", false, "Interactively choose which parts to split. This is the default if no paths are provided")
 	splitCmd.Flags().StringP("revision", "r", "", "The revision to split")
 	rootCmd.AddCommand(splitCmd)
+
+	carapace.Gen(splitCmd).FlagCompletion(carapace.ActionMap{
+		"revision": jj.ActionRevs(jj.RevsOption{}.Default()),
+	})
+
+	carapace.Gen(splitCmd).PositionalAnyCompletion(
+		carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+			return jj.ActionRevFiles(splitCmd.Flag("revision").Value.String())
+		}),
+	)
 }
