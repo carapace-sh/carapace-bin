5908a3261ebf993ed8b5a4f6955a0841ad3b0904
rsteube <rsteube@users.noreply.github.com>
2023-11-02 23:03:36 +0100

    jj: abandon and backout

--- completers/jj_completer/cmd/abandon.go
+++ completers/jj_completer/cmd/abandon.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var abandonCmd = &cobra.Command{
-	Use:   "abandon",
+	Use:   "abandon [OPTIONS] [REVISIONS]...",
 	Short: "Abandon a revision",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -17,4 +18,8 @@ func init() {
 	abandonCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	abandonCmd.Flags().BoolP("summary", "s", false, "Do not print every abandoned commit on a separate line")
 	rootCmd.AddCommand(abandonCmd)
+
+	carapace.Gen(abandonCmd).PositionalAnyCompletion(
+		jj.ActionRevs(jj.RevOption{}.Default()).FilterArgs(),
+	)
 }
