4f903fa54e15a7d9b9fd7c1f68999120d4960253
rsteube <rsteube@users.noreply.github.com>
2023-11-02 23:37:17 +0100

    jj: merge and move

--- completers/jj_completer/cmd/merge.go
+++ completers/jj_completer/cmd/merge.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var mergeCmd = &cobra.Command{
-	Use:   "merge",
+	Use:   "merge [OPTIONS] [REVISIONS]...",
 	Short: "Merge work from multiple branches",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -19,4 +20,8 @@ func init() {
 	mergeCmd.Flags().BoolP("insert-before", "B", false, "Insert the new change between the target commit(s) and their parents")
 	mergeCmd.Flags().StringSliceP("message", "m", []string{}, "The change description to use")
 	rootCmd.AddCommand(mergeCmd)
+
+	carapace.Gen(mergeCmd).PositionalAnyCompletion(
+		jj.ActionRevs(jj.RevOption{}.Default()).FilterArgs(),
+	)
 }
