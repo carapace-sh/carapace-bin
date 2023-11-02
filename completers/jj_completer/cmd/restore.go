507b5b71a27041e689087afdb3188f2fc5a6e771
rsteube <rsteube@users.noreply.github.com>
2023-11-03 00:24:03 +0100

    jj: restore

--- completers/jj_completer/cmd/restore.go
+++ completers/jj_completer/cmd/restore.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var restoreCmd = &cobra.Command{
-	Use:   "restore",
+	Use:   "restore [OPTIONS] [PATHS]...",
 	Short: "Restore paths from another revision",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -15,8 +16,22 @@ func init() {
 	carapace.Gen(restoreCmd).Standalone()
 
 	restoreCmd.Flags().StringP("changes-in", "c", "", "Undo the changes in a revision as compared to the merge of its parents")
-	restoreCmd.Flags().String("from", "", "Revision to restore from (source)")
+	restoreCmd.Flags().String("from", "@", "Revision to restore from (source)")
 	restoreCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
-	restoreCmd.Flags().String("to", "", "Revision to restore into (destination)")
+	restoreCmd.Flags().String("to", "@", "Revision to restore into (destination)")
 	rootCmd.AddCommand(restoreCmd)
+
+	carapace.Gen(restoreCmd).FlagCompletion(carapace.ActionMap{
+		"from": jj.ActionRevs(jj.RevOption{}.Default()),
+		"to":   jj.ActionRevs(jj.RevOption{}.Default()),
+	})
+
+	carapace.Gen(restoreCmd).PositionalAnyCompletion(
+		carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+			return jj.ActionRevDiffs(
+				restoreCmd.Flag("from").Value.String(),
+				restoreCmd.Flag("to").Value.String(),
+			).FilterArgs()
+		}),
+	)
 }
