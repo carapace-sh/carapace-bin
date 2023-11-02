0aaa12643be0afd66f653a497d85e61e2772ac1f
rsteube <rsteube@users.noreply.github.com>
2023-11-02 23:10:19 +0100

    jj: commit

--- completers/jj_completer/cmd/commit.go
+++ completers/jj_completer/cmd/commit.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var commitCmd = &cobra.Command{
-	Use:     "commit",
+	Use:     "commit [OPTIONS] [PATHS]...",
 	Short:   "Update the description and create a new change on top",
 	Aliases: []string{"ci"},
 	Run:     func(cmd *cobra.Command, args []string) {},
@@ -19,4 +20,8 @@ func init() {
 	commitCmd.Flags().BoolP("interactive", "i", false, "Interactively choose which changes to include in the first commit")
 	commitCmd.Flags().StringSliceP("message", "m", []string{}, "The change description to use (don't open editor)")
 	rootCmd.AddCommand(commitCmd)
+
+	carapace.Gen(commitCmd).PositionalAnyCompletion(
+		jj.ActionRevDiffs().FilterArgs(),
+	)
 }
