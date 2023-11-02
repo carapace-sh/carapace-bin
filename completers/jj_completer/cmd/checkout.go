874120bcc5b11cca04e49c42cabd5e4cdd3283ac
rsteube <rsteube@users.noreply.github.com>
2023-11-02 23:07:59 +0100

    jj: checkout

--- completers/jj_completer/cmd/checkout.go
+++ completers/jj_completer/cmd/checkout.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var checkoutCmd = &cobra.Command{
-	Use:     "checkout",
+	Use:     "checkout [OPTIONS] <REVISION>",
 	Short:   "Create a new, empty change and edit it in the working copy",
 	Aliases: []string{"co"},
 	Run:     func(cmd *cobra.Command, args []string) {},
@@ -18,4 +19,8 @@ func init() {
 	checkoutCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	checkoutCmd.Flags().StringSliceP("message", "m", []string{}, "The change description to use")
 	rootCmd.AddCommand(checkoutCmd)
+
+	carapace.Gen(checkoutCmd).PositionalCompletion(
+		jj.ActionRevs(jj.RevOption{}.Default()),
+	)
 }
