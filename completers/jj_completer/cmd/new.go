a7e18216524a6626f0718b036cdff5480a5624c7
rsteube <rsteube@users.noreply.github.com>
2023-11-02 23:54:29 +0100

    jj: new

--- completers/jj_completer/cmd/new.go
+++ completers/jj_completer/cmd/new.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var newCmd = &cobra.Command{
-	Use:   "new",
+	Use:   "new [OPTIONS] [REVISIONS]...",
 	Short: "Create a new, empty change and edit it in the working copy",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -19,4 +20,8 @@ func init() {
 	newCmd.Flags().BoolP("insert-before", "B", false, "Insert the new change between the target commit(s) and their parents")
 	newCmd.Flags().StringSliceP("message", "m", []string{}, "The change description to use")
 	rootCmd.AddCommand(newCmd)
+
+	carapace.Gen(newCmd).PositionalAnyCompletion(
+		jj.ActionRevs(jj.RevOption{}.Default()).FilterArgs(),
+	)
 }
