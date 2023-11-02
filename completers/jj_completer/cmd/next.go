2726181880bdebbfbfbeeb6e7a7a6c7eb4fd15d7
rsteube <rsteube@users.noreply.github.com>
2023-11-02 23:55:17 +0100

    jj: next

--- completers/jj_completer/cmd/next.go
+++ completers/jj_completer/cmd/next.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var nextCmd = &cobra.Command{
-	Use:   "next",
+	Use:   "next [OPTIONS] [AMOUNT]",
 	Short: "Move the current working copy commit to the next child revision in the repository.",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -17,4 +18,8 @@ func init() {
 	nextCmd.Flags().Bool("edit", false, "Instead of creating a new working-copy commit on top of the target commit (like `jj new`), edit the target commit directly (like `jj edit`)")
 	nextCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	rootCmd.AddCommand(nextCmd)
+
+	carapace.Gen(nextCmd).PositionalCompletion(
+		jj.ActionNextCommits(100),
+	)
 }
