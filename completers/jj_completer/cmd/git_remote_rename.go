7483ea9dcf8ae4c2ac3baa2c278c8802b55b5717
rsteube <rsteube@users.noreply.github.com>
2023-11-03 01:34:22 +0100

    jj: remote

--- completers/jj_completer/cmd/git_remote_rename.go
+++ completers/jj_completer/cmd/git_remote_rename.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var git_remote_renameCmd = &cobra.Command{
-	Use:   "rename",
+	Use:   "rename [OPTIONS] <OLD> <NEW>",
 	Short: "Rename a Git remote",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -16,4 +17,9 @@ func init() {
 
 	git_remote_renameCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	git_remoteCmd.AddCommand(git_remote_renameCmd)
+
+	carapace.Gen(git_remote_renameCmd).PositionalCompletion(
+		jj.ActionRemotes(),
+		jj.ActionRemotes(),
+	)
 }
