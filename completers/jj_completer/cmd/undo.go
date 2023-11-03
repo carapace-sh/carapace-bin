54516c86a8dc52fff3b9a17d04564ddb502d804a
rsteube <rsteube@users.noreply.github.com>
2023-11-03 14:05:19 +0100

    jj: undo

--- completers/jj_completer/cmd/undo.go
+++ completers/jj_completer/cmd/undo.go
@@ -2,19 +2,23 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bridge/pkg/actions/bridge"
 	"github.com/spf13/cobra"
 )
 
 var undoCmd = &cobra.Command{
-	Use:   "undo",
-	Short: "Undo an operation (shortcut for `jj op undo`)",
-	Run:   func(cmd *cobra.Command, args []string) {},
+	Use:                "undo",
+	Short:              "Undo an operation (shortcut for `jj op undo`)",
+	Run:                func(cmd *cobra.Command, args []string) {},
+	DisableFlagParsing: true,
 }
 
 func init() {
 	carapace.Gen(undoCmd).Standalone()
 
-	undoCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
-	undoCmd.Flags().StringSlice("what", []string{}, "What portions of the local state to restore (can be repeated)")
 	rootCmd.AddCommand(undoCmd)
+
+	carapace.Gen(undoCmd).PositionalAnyCompletion(
+		bridge.ActionCarapaceBin("jj", "op", "undo"),
+	)
 }
