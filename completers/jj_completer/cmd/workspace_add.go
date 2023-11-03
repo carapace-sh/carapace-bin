0ffd8a2b89463b06c58daf40be5a29d71ddd22da
rsteube <rsteube@users.noreply.github.com>
2023-11-03 14:45:14 +0100

    jj: workspace add and forget

--- completers/jj_completer/cmd/workspace_add.go
+++ completers/jj_completer/cmd/workspace_add.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var workspace_addCmd = &cobra.Command{
-	Use:   "add",
+	Use:   "add [OPTIONS] <DESTINATION>",
 	Short: "Add a workspace",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -18,4 +19,13 @@ func init() {
 	workspace_addCmd.Flags().String("name", "", "A name for the workspace")
 	workspace_addCmd.Flags().StringP("revision", "r", "", "The revision that the workspace should be created at; a new working copy commit will be created on top of it")
 	workspaceCmd.AddCommand(workspace_addCmd)
+
+	carapace.Gen(workspace_addCmd).FlagCompletion(carapace.ActionMap{
+		"name":     jj.ActionWorkspaces(),
+		"revision": jj.ActionRevs(jj.RevOption{}.Default()),
+	})
+
+	carapace.Gen(workspace_addCmd).PositionalCompletion(
+		carapace.ActionDirectories(),
+	)
 }
--- completers/jj_completer/cmd/workspace_forget.go
+++ completers/jj_completer/cmd/workspace_forget.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var workspace_forgetCmd = &cobra.Command{
-	Use:   "forget",
+	Use:   "forget [OPTIONS] [WORKSPACES]...",
 	Short: "Stop tracking a workspace's working-copy commit in the repo",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -16,4 +17,8 @@ func init() {
 
 	workspace_forgetCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	workspaceCmd.AddCommand(workspace_forgetCmd)
+
+	carapace.Gen(workspace_forgetCmd).PositionalAnyCompletion(
+		jj.ActionWorkspaces().FilterArgs(),
+	)
 }
