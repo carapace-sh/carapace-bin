86f0c68f0e5d9cb14ae5afb25d9b454012bb34ed
rsteube <rsteube@users.noreply.github.com>
2023-11-03 10:16:05 +0100

    jj: duplicate and edit

--- completers/jj_completer/cmd/edit.go
+++ completers/jj_completer/cmd/edit.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var editCmd = &cobra.Command{
-	Use:   "edit",
+	Use:   "edit [OPTIONS] <REVISION>",
 	Short: "Edit a commit in the working copy",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -16,4 +17,8 @@ func init() {
 
 	editCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	rootCmd.AddCommand(editCmd)
+
+	carapace.Gen(editCmd).PositionalCompletion(
+		jj.ActionRevs(jj.RevOption{}.Default()),
+	)
 }
