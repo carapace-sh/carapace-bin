4c5ae44166eccbfa0ab808588d9e18b621a62283
rsteube <rsteube@users.noreply.github.com>
2023-11-03 12:07:16 +0100

    jj: operation

--- completers/jj_completer/cmd/operation_undo.go
+++ completers/jj_completer/cmd/operation_undo.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/rsteube/carapace"
+	"github.com/rsteube/carapace-bin/pkg/actions/tools/jj"
 	"github.com/spf13/cobra"
 )
 
 var operation_undoCmd = &cobra.Command{
-	Use:   "undo",
+	Use:   "undo [OPTIONS] [OPERATION]",
 	Short: "Create a new operation that undoes an earlier operation",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -17,4 +18,12 @@ func init() {
 	operation_undoCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	operation_undoCmd.Flags().StringSlice("what", []string{}, "What portions of the local state to restore (can be repeated)")
 	operationCmd.AddCommand(operation_undoCmd)
+
+	carapace.Gen(operation_undoCmd).FlagCompletion(carapace.ActionMap{
+		"what": carapace.ActionValues("repo", "remote-tracking"),
+	})
+
+	carapace.Gen(operation_undoCmd).PositionalCompletion(
+		jj.ActionOperations(100),
+	)
 }
---
+++ pkg/actions/tools/jj/operation.go
@@ -0,0 +1,24 @@
+package jj
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+// ActionOperations completes operations
+//
+//	c9f7e4956c01 (initialize repo)
+//	902f106af7d3 (check out git remote's default branch)
+func ActionOperations(limit int) carapace.Action {
+	return carapace.ActionExecCommand("jj", "op", "log", "--no-graph", "--template", `id.short() ++ "\t" ++ description ++ "\n"`, "--limit", strconv.Itoa(limit))(func(output []byte) carapace.Action {
+		lines := strings.Split(string(output), "\n")
+
+		vals := make([]string, 0)
+		for _, line := range lines[:len(lines)-1] {
+			vals = append(vals, strings.SplitN(line, "\t", 2)...)
+		}
+		return carapace.ActionValuesDescribed(vals...)
+	})
+}
