3801e601a1aaa094c6aa326688d27f4d9a5206b9
rsteube <rsteube@users.noreply.github.com>
2023-11-10 15:31:03 +0100

    ffmpeg: restructure command

---
+++ pkg/actions/tools/ffmpeg/demuxer.go
@@ -0,0 +1,29 @@
+package ffmpeg
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/rsteube/carapace"
+)
+
+func ActionDemuxers() carapace.Action {
+	return carapace.ActionExecCommand("ffmpeg", "-demuxers")(func(output []byte) carapace.Action {
+		lines := strings.Split(string(output), "\n")
+		r := regexp.MustCompile(`^ (?P<type>.).{1} (?P<name>[^ ]+) +(?P<description>.*)$`)
+
+		found := false
+		vals := make([]string, 0)
+		for _, line := range lines {
+			if !found {
+				found = line == " --"
+				continue
+			}
+
+			if matches := r.FindStringSubmatch(line); matches != nil {
+				vals = append(vals, matches[2], matches[3])
+			}
+		}
+		return carapace.ActionValuesDescribed(vals...)
+	}).Tag("demuxers")
+}
