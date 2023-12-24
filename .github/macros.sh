5497d87908483bb6fbb47d7b5bf76af89aba5db8
rsteube <rsteube@users.noreply.github.com>
2023-12-24 10:48:00 +0100

    doc: fix macro list

--- .github/macros.sh
+++ .github/macros.sh
@@ -5,7 +5,7 @@ scriptdir=$(dirname $(readlink -f $0))
 echo "# Macros"
 echo
 
-$scriptdir/../cmd/carapace/carapace --macros \
+$scriptdir/../cmd/carapace/carapace --macro \
   | sed -r 's_(https://[^ ]+)_[\1](\1)_' \
   | sed 's_^\([^ ]*\.\)\([^. ]\+\) \+\(.*\)_- [\1\2]\(https://pkg.go.dev/github.com/rsteube/carapace-bin/pkg/actions/\1#Action\2) \3_' \
   | sed -e ':loop' -e 's_\(carapace-bin/pkg/actions/[^#]*\)[.]_\1/_' -e 't loop' \
