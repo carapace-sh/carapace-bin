8ba69df628aa0a90a44ea52b221d9cd1805f52ac
rsteube <rsteube@users.noreply.github.com>
2024-02-04 15:02:41 +0100

    git: added maintenance

---
+++ completers/git_completer/cmd/maintenance.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/rsteube/carapace"
+	"github.com/spf13/cobra"
+)
+
+var maintenanceCmd = &cobra.Command{
+	Use:     "maintenance",
+	Short:   "Run tasks to optimize Git repository data",
+	Run:     func(cmd *cobra.Command, args []string) {},
+	GroupID: groups[group_main].ID,
+}
+
+func init() {
+	carapace.Gen(maintenanceCmd).Standalone()
+
+	rootCmd.AddCommand(maintenanceCmd)
+}
---
+++ pkg/actions/tools/git/maintenance.go
@@ -0,0 +1,18 @@
+package git
+
+import "github.com/rsteube/carapace"
+
+// ActionMaintenanceTasks completes maintenance tasks
+//
+//	commit-graph (Update the commit-graph files incrementally)
+//	prefetch (Updates the object directory with the latest objects from all registered remotes)
+func ActionMaintenanceTasks() carapace.Action {
+	return carapace.ActionValuesDescribed(
+		"commit-graph", "Update the commit-graph files incrementally",
+		"prefetch", "Updates the object directory with the latest objects from all registered remotes",
+		"gc", "Clean up unnecessary files and optimize the local repository",
+		"loose-objects", "Clean up loose objects and places them into pack-files",
+		"incremental-repack", "Repack the object directory using the multi-pack-index feature",
+		"pack-refs", "Collect the loose reference files and collects them into a single file",
+	)
+}
