491db3537c1ea822f11c614f17e7da0d09312f5a
aftix <aftix@aftix.xyz>
2024-03-30 16:34:08 -0500

    Added completer for transmission-show

---
+++ completers/transmission-show_completer/cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "transmission-show [flags] torrentfile",
+	Short: "A command-line utility to show .torrent file metadata",
+	Long:  "https://transmissionbt.com/",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().BoolP("help", "h", false, "Show a short help page and exit")
+	rootCmd.Flags().BoolP("magnent", "m", false, "Show a magnent link for the specified .torrent file")
+	rootCmd.Flags().BoolP("scrape", "s", false, "Ask the torrent's trackers how many peers are in the torrent's swarm")
+	rootCmd.Flags().BoolP("version", "V", false, "Show version number and exit")
+
+	carapace.Gen(rootCmd).PositionalCompletion(carapace.ActionFiles(".torrent"))
+}
