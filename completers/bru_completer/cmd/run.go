cc36cbc56c602893534449b10c53c2c9844248cf
rsteube <rsteube@users.noreply.github.com>
2024-04-02 23:27:12 +0200

    added bru

---
+++ completers/bru_completer/cmd/run.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run a request",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(runCmd).Standalone()
+
+	runCmd.Flags().Bool("bail", false, "Stop execution after a failure of a request, test, or assertion")
+	runCmd.Flags().String("cacert", "", "CA certificate to verify peer against")
+	runCmd.Flags().String("env", "", "Environment variables")
+	runCmd.Flags().String("env-var", "", "Overwrite a single environment variable, multiple usages possible")
+	runCmd.Flags().StringP("format", "f", "", "Format of the file results")
+	runCmd.Flags().BoolP("help", "h", false, "Show help")
+	runCmd.Flags().Bool("insecure", false, "Allow insecure server connections")
+	runCmd.Flags().StringP("output", "o", "", "Path to write file results to")
+	runCmd.Flags().BoolS("r", "r", false, "Indicates a recursive run")
+	runCmd.Flags().Bool("tests-only", false, "Only run requests that have a test")
+	runCmd.Flags().Bool("version", false, "Show version number")
+	rootCmd.AddCommand(runCmd)
+
+	carapace.Gen(runCmd).FlagCompletion(carapace.ActionMap{
+		"cacert":  carapace.ActionFiles(),
+		"env":     carapace.ActionValues(), // TODO
+		"env-var": carapace.ActionValues(), // TODO
+		"format":  carapace.ActionValues("json", "junit", "html"),
+		"output":  carapace.ActionFiles(),
+	})
+
+	carapace.Gen(runCmd).PositionalCompletion(
+		carapace.ActionFiles(".bru"),
+	)
+}
