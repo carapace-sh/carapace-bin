94ba5c558f10eb2d63872cd2a1e0085e5a16b9fe
Samuel Allan <samuelengineer@fastmail.com>
2024-06-29 23:09:12 +0930

    Switch to ActionExecCommand for tox listing environments

--- pkg/actions/tools/tox/tox.go
+++ pkg/actions/tools/tox/tox.go
@@ -10,15 +10,14 @@ import (
 // ActionEnvironments completes tox environments
 func ActionEnvironments() carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-		cmd := c.Command("tox", "--listenvs-all")
-		if output, err := cmd.Output(); err != nil || cmd.ProcessState.ExitCode() != 0 {
-			return carapace.ActionMessage(err.Error())
-		} else if strings.Contains(string(output), "ROOT: No tox.ini") {
-				return carapace.ActionValues()
-		} else {
-			lines := strings.Split(string(output), "\n")
-			lines = append(lines, "ALL")
-			return carapace.ActionValues(lines...)
-		}
+		return carapace.ActionExecCommand("tox", "--listenvs-all")(func(output []byte) carapace.Action {
+			if strings.Contains(string(output), "ROOT: No tox.ini") {
+					return carapace.ActionValues()
+			} else {
+				lines := strings.Split(string(output), "\n")
+				lines = append(lines, "ALL")
+				return carapace.ActionValues(lines...)
+			}
+		})
 	})
 }
