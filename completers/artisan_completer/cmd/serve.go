588b9ef471555499972b0c1005b31f1298bdf441
rsteube <rsteube@users.noreply.github.com>
2024-07-28 19:51:36 +0200

    artisan: complete, horizonpurge, serve

--- completers/artisan_completer/cmd/serve.go
+++ completers/artisan_completer/cmd/serve.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bin/pkg/actions/net"
 	"github.com/spf13/cobra"
 )
 
@@ -19,4 +20,9 @@ func init() {
 	serveCmd.Flags().String("port", "", "The port to serve the application on")
 	serveCmd.Flags().String("tries", "", "The max number of ports to attempt to serve from")
 	rootCmd.AddCommand(serveCmd)
+
+	carapace.Gen(serveCmd).FlagCompletion(carapace.ActionMap{
+		"host": carapace.ActionValues("localhost", "127.0.0.1"),
+		"port": net.ActionPorts(),
+	})
 }
