9f3def6d951f73f74e7c983b1f34f12f2f243135
rsteube <rsteube@users.noreply.github.com>
2024-08-17 17:29:29 +0200

    git: shell

---
+++ completers/git_completer/cmd/shell.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var shellCmd = &cobra.Command{
+	Use:   "shell",
+	Short: "Restricted login shell for Git-only SSH access",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func init() {
+	carapace.Gen(shellCmd).Standalone()
+
+	shellCmd.Flags().Bool("c", false, "run command")
+	rootCmd.AddCommand(shellCmd)
+}
