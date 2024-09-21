efef3d8f90f0ca90ff962db503ee974522a81676
rsteube <rsteube@users.noreply.github.com>
2024-09-21 23:47:01 +0200

    ffmpeg: added `ActionDecodableCodecs` and `ActionEncodableCodecs`

--- pkg/actions/tools/ffmpeg/codec.go
+++ pkg/actions/tools/ffmpeg/codec.go
@@ -9,19 +9,19 @@ import (
 )
 
 type CodecOpts struct {
-	Audio    bool
-	Subtitle bool
-	Video    bool
-	Decoding bool
-	Encoding bool
+	Attachment bool
+	Audio      bool
+	Data       bool
+	Subtitle   bool
+	Video      bool
 }
 
 func (o CodecOpts) Default() CodecOpts {
+	o.Attachment = true
 	o.Audio = true
+	o.Data = true
 	o.Subtitle = true
 	o.Video = true
-	o.Decoding = true
-	o.Encoding = true
 	return o
 }
 
@@ -30,6 +30,30 @@ func (o CodecOpts) Default() CodecOpts {
 //	4gv (4GV (Fourth Generation Vocoder))
 //	4xm (4X Movie)
 func ActionCodecs(opts CodecOpts) carapace.Action {
+	return actionCodecs(opts, nil)
+}
+
+// ActionEncodableCodecs completes codecs with encoding support
+//
+//	amv (AMV Video)
+//	anull (Null audio codec)
+func ActionEncodableCodecs(opts CodecOpts) carapace.Action {
+	return actionCodecs(opts, func(s string) bool {
+		return s[1] != 'E'
+	})
+}
+
+// ActionDecodableCodecs completes codecs with decoding support
+//
+//	avrn (Avid AVI Codec)
+//	avrp (Avid 1:1 10-bit RGB Packer)
+func ActionDecodableCodecs(opts CodecOpts) carapace.Action {
+	return actionCodecs(opts, func(s string) bool {
+		return s[0] != 'D'
+	})
+}
+
+func actionCodecs(opts CodecOpts, filter func(s string) bool) carapace.Action {
 	return carapace.ActionExecCommand("ffmpeg", "-hide_banner", "-codecs")(func(output []byte) carapace.Action {
 		_, content, ok := strings.Cut(string(output), " -------")
 		if !ok {
@@ -37,25 +61,13 @@ func ActionCodecs(opts CodecOpts) carapace.Action {
 		}
 
 		lines := strings.Split(content, "\n")
-		r := regexp.MustCompile(`^ .(?P<decoding>.)(?P<encoding>.)(?P<type>.).{3} (?P<codec>[^ ]+) +(?P<description>.*)$`)
+		r := regexp.MustCompile(`^ (?P<decoding>.)(?P<encoding>.)(?P<type>.).{3} (?P<codec>[^ ]+) +(?P<description>.*)$`)
 
 		vals := make([]string, 0)
 		for _, line := range lines[10 : len(lines)-1] {
 			if matches := r.FindStringSubmatch(line); matches != nil {
-				decoding := matches[1] == "D"
-				encoding := matches[2] == "E"
-
-				if opts.Decoding && opts.Encoding {
-					if !(decoding || encoding) {
-						continue
-					}
-				} else {
-					if opts.Decoding && !decoding {
-						continue
-					}
-					if opts.Encoding && !encoding {
-						continue
-					}
+				if filter != nil && filter(line[1:7]) {
+					continue
 				}
 
 				switch matches[3] {
@@ -63,9 +75,17 @@ func ActionCodecs(opts CodecOpts) carapace.Action {
 					if opts.Audio {
 						vals = append(vals, matches[4], matches[5], style.Yellow)
 					}
+				case "D":
+					if opts.Data {
+						vals = append(vals, matches[4], matches[5], style.Cyan)
+					}
 				case "S":
 					if opts.Subtitle {
 						vals = append(vals, matches[4], matches[5], style.Magenta)
+					}
+				case "T":
+					if opts.Attachment {
+						vals = append(vals, matches[4], matches[5], style.Green)
 					}
 				case "V":
 					if opts.Video {
@@ -74,7 +94,8 @@ func ActionCodecs(opts CodecOpts) carapace.Action {
 				}
 			}
 		}
-		if opts.Decoding {
+
+		if filter == nil || !filter("D      ") {
 			vals = append(vals, "copy", "copy the codec of the input", style.Default)
 		}
 		return carapace.ActionStyledValuesDescribed(vals...)
