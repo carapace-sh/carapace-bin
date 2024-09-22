401c675bac71b6ba900ac166fbd53b6e73fd2940
Ralf Steube <rsteube@users.noreply.github.com>
2024-09-22 11:56:16 +0200

    added git-clear-soft (#2538)

---
+++ completers/git-clear-soft_completer/cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "git-clear-soft",
+	Short: "Soft clean up a repository",
+	Long:  "https://github.com/tj/git-extras/blob/master/Commands.md#git-clear-soft",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().Bool("help", false, "show help")
+}
