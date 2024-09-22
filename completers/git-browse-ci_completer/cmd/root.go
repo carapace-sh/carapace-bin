ede34025b4a5c185f9cc59213d23d2710b989c1b
Ralf Steube <rsteube@users.noreply.github.com>
2024-09-22 11:41:46 +0200

    added git-browse-ci (#2536)

---
+++ completers/git-browse-ci_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bin/pkg/actions/tools/git"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "git-browse-ci",
+	Short: "Opens the current git repository CI page in your default web browser",
+	Long:  "https://github.com/tj/git-extras/blob/master/Commands.md#git-browse-ci",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().Bool("help", false, "show help")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		git.ActionRemotes(),
+	)
+}
