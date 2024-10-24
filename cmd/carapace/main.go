de74b4c19fb4287076f3a57aac9d654df48b3c99
rsteube <rsteube@users.noreply.github.com>
2024-10-24 20:37:13 +0200

    include snapshot date in version

--- cmd/carapace/main.go
+++ cmd/carapace/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/carapace-sh/carapace-bin/cmd/carapace/cmd"
 )
 
+var date string
 var version = "develop"
 
 func main() {
+	if strings.HasSuffix(version, "-next") {
+		version += fmt.Sprintf(" (%v)", date)
+	}
 	cmd.Execute(version)
 }
