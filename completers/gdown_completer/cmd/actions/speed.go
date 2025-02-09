529689e69f3e7c0afe3f8d8b0632f0dbe5f92a44
Saurabh Kushwah <110624181+Saurabh825@users.noreply.github.com>
2025-02-10 00:50:10 +0530

    Update completers/gdown_completer/cmd/actions/speed.go
    Co-authored-by: Ralf Steube <rsteube@users.noreply.github.com>

--- completers/gdown_completer/cmd/actions/speed.go
+++ completers/gdown_completer/cmd/actions/speed.go
@@ -4,16 +4,9 @@ import "github.com/carapace-sh/carapace"
 
 func ActionSpeed() carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-		if len(c.Value) > 0 && c.Value[0] >= '0' && c.Value[0] <= '9' {
-			num := ""
-			for _, char := range c.Value {
-				if char >= '0' && char <= '9' {
-					num += string(char)
-				} else {
-					break
-				}
-			}
-			return carapace.ActionValues("KB", "MB", "GB").Invoke(c).Prefix(num).ToA()
+		r := regexp.MustCompile(`^(?P<digits>\d+).*$`)
+		if matches := r.FindStringSubmatch(c.Value); matches != nil {
+			return carapace.ActionValues("KB", "MB", "GB").Prefix(matches[1])
 		}
 		return carapace.ActionValues()
 	})
