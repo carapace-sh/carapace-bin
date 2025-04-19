1d7a894ef5be9310f819f3233958f36bbce2684f
Saurabh Kushwah <saurabhkushwahjnv@gmail.com>
2025-04-19 22:22:43 +0530

    watch: updated to procps-ng 4.0.5

--- completers/watch_completer/cmd/root.go
+++ completers/watch_completer/cmd/root.go
@@ -8,13 +8,14 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "watch",
 	Short: "execute a program periodically, showing output fullscreen",
-	Long:  "https://linux.die.net/man/1/watch",
+	Long:  "https://man7.org/linux/man-pages/man1/watch.1.html",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 func Execute() error {
 	return rootCmd.Execute()
 }
+
 func init() {
 	carapace.Gen(rootCmd).Standalone()
 
@@ -22,13 +23,17 @@ func init() {
 	rootCmd.Flags().BoolP("chgexit", "g", false, "exit when output from command changes")
 	rootCmd.Flags().BoolP("color", "c", false, "interpret ANSI color and style sequences")
 	rootCmd.Flags().StringP("differences", "d", "", "highlight changes between updates")
+	rootCmd.Flags().StringP("equexit", "q", "", "exit when output from command does not change")
 	rootCmd.Flags().BoolP("errexit", "e", false, "exit if command has a non-zero exit")
 	rootCmd.Flags().BoolP("exec", "x", false, "pass command to exec instead of \"sh -c\"")
 	rootCmd.Flags().BoolP("help", "h", false, "display this help and exit")
 	rootCmd.Flags().StringP("interval", "n", "", "seconds to wait between updates")
+	rootCmd.Flags().BoolP("no-color", "C", false, "do not interpret ANSI color and style sequences")
+	rootCmd.Flags().BoolP("no-rerun", "r", false, "do not rerun program on window resize")
 	rootCmd.Flags().BoolP("no-title", "t", false, "turn off header")
 	rootCmd.Flags().BoolP("no-wrap", "w", false, "turn off line wrapping")
 	rootCmd.Flags().BoolP("precise", "p", false, "attempt run command in precise intervals")
+	rootCmd.Flags().BoolP("shotsdir", "s", false, "directory to store screenshots")
 	rootCmd.Flags().BoolP("version", "v", false, "output version information and exit")
 
 	rootCmd.Flag("differences").NoOptDefVal = " "
