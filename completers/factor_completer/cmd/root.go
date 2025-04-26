10c2e6610916147bc1a433795b6e765ce83ee846
Saurabh Kushwah <saurabhkushwahjnv@gmail.com>
2025-04-26 16:29:42 +0530

    factor: updated to GNU coreutils 9.7

--- completers/factor_completer/cmd/root.go
+++ completers/factor_completer/cmd/root.go
@@ -8,16 +8,18 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "factor",
 	Short: "factor numbers",
-	Long:  "https://linux.die.net/man/1/factor",
+	Long:  "https://man7.org/linux/man-pages/man1/factor.1.html",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 func Execute() error {
 	return rootCmd.Execute()
 }
+
 func init() {
 	carapace.Gen(rootCmd).Standalone()
 
+	rootCmd.Flags().BoolP("exponents", "h", false, "print repeated factors in form p^e unless e is 1")
 	rootCmd.Flags().Bool("help", false, "display this help and exit")
 	rootCmd.Flags().Bool("version", false, "output version information and exit")
 }
