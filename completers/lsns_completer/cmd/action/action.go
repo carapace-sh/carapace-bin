a784350647702c8a5c116b6cfd00287278c3e88c
Saurabh Kushwah <saurabhkushwahjnv@gmail.com>
2025-04-28 22:45:13 +0530

    lsns: updated to util-linux 2.41

--- completers/lsns_completer/cmd/action/action.go
+++ completers/lsns_completer/cmd/action/action.go
@@ -1,21 +1,24 @@
 package action
 
-import "github.com/carapace-sh/carapace"
+import (
+	"regexp"
+	"strings"
+
+	"github.com/carapace-sh/carapace"
+)
 
 func ActionColumns() carapace.Action {
-	return carapace.ActionValuesDescribed(
-		"NS", "namespace identifier (inode number)",
-		"TYPE", "kind of namespace",
-		"PATH", "path to the namespace",
-		"NPROCS", "number of processes in the namespace",
-		"PID", "lowest PID in the namespace",
-		"PPID", "PPID of the PID",
-		"COMMAND", "command line of the PID",
-		"UID", "UID of the PID",
-		"USER", "username of the PID",
-		"NETNSID", "namespace ID as used by network subsystem",
-		"NSFS", "nsfs mountpoint (usually used network subsystem)",
-		"PNS", "parent namespace identifier (inode number)",
-		"ONS", "owner namespace identifier (inode number)",
-	)
+	return carapace.ActionExecCommand("lsns", "--list-columns")(func(output []byte) carapace.Action {
+		re := regexp.MustCompile(`^\s*(\S+)\s+<[^>]+>\s+(.*)$`)
+		var values []string
+
+		for _, line := range strings.Split(string(output), "\n") {
+			if matches := re.FindStringSubmatch(line); len(matches) == 3 {
+				name, description := matches[1], matches[2]
+				values = append(values, name, description)
+			}
+		}
+
+		return carapace.ActionValuesDescribed(values...)
+	})
 }
