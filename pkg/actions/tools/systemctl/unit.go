072e775050452d8d0a362100ccf48f366000ed56
rsteube <rsteube@users.noreply.github.com>
2025-05-08 20:17:55 +0200

    systemctl: complete inactive units

--- pkg/actions/tools/systemctl/unit.go
+++ pkg/actions/tools/systemctl/unit.go
@@ -4,16 +4,29 @@ import (
 	"strings"
 
 	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace/pkg/style"
 )
+
+type UnitOpts struct {
+	User     bool
+	Active   bool
+	Inactive bool
+}
+
+func (o UnitOpts) Default() UnitOpts {
+	o.Active = true
+	o.Inactive = true
+	return o
+}
 
 // ActionUnits completes units
 //
 //	NetworkManager.service (Network Manager)
 //	apparmor.service (Load AppArmor profiles)
-func ActionUnits(user bool) carapace.Action {
+func ActionUnits(opts UnitOpts) carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-		args := []string{"list-units"}
-		if user {
+		args := []string{"list-units", "--all"}
+		if opts.User {
 			args = append(args, "--user")
 		}
 		return carapace.ActionExecCommand("systemctl", args...)(func(output []byte) carapace.Action {
@@ -25,9 +38,14 @@ func ActionUnits(user bool) carapace.Action {
 					break
 				}
 				fields := strings.Fields(line)
-				vals = append(vals, fields[0], strings.Join(fields[4:], " "))
+				switch state := fields[2]; {
+				case opts.Active && state == "active":
+					vals = append(vals, fields[0], strings.Join(fields[4:], " "), style.Carapace.KeywordPositive)
+				case opts.Inactive && state == "inactive":
+					vals = append(vals, fields[0], strings.Join(fields[4:], " "), style.Carapace.KeywordNegative)
+				}
 			}
-			return carapace.ActionValuesDescribed(vals...)
+			return carapace.ActionStyledValuesDescribed(vals...)
 		})
 	})
 }
