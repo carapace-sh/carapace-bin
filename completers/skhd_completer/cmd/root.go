41abe3111f78993589f081ee64a162ed591fcce1
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2025-05-19 13:42:05 +0200

    skhd: completer

---
+++ completers/skhd_completer/cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "skhd",
+	Short: "Simple hotkey daemon for macOS",
+	Long:  "https://github.com/koekeishiya/skhd",
+	Run:   func(*cobra.Command, []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().Bool("install-service", false, "Install launchd service file into ~/Library/LaunchAgents/com.koekeishiya.skhd.plist")
+	rootCmd.Flags().Bool("uninstall-service", false, "Remove launchd service file ~/Library/LaunchAgents/com.koekeishiya.skhd.plist")
+	rootCmd.Flags().Bool("start-service", false, "Run skhd as a service through launchd")
+	rootCmd.Flags().Bool("restart-service", false, "Restart skhd service")
+	rootCmd.Flags().Bool("stop-service", false, "Stop skhd service from running")
+	rootCmd.Flags().BoolP("verbose", "B", false, "Output debug information")
+	rootCmd.Flags().BoolP("profile", "P", false, "Output profiling information")
+	rootCmd.Flags().BoolP("version", "v", false, "Print version number to stdout")
+	rootCmd.Flags().BoolP("config", "c", false, "Specify location of config file")
+	rootCmd.Flags().BoolP("observe", "o", false, "Output keycode and modifiers of event. Ctrl+C to quit")
+	rootCmd.Flags().BoolP("reload", "r", false, "Signal a running instance of skhd to reload its config file")
+	rootCmd.Flags().BoolP("no-hotload", "h", false, "Disable system for hotloading config file")
+	rootCmd.Flags().BoolP("key", "k", false, "Synthesize a keypress (same syntax as when defining a hotkey)")
+	rootCmd.Flags().BoolP("text", "t", false, "Synthesize a line of text")
+}
