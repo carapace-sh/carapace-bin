8713bfb8e238ea544be258e9bb85f8918eaefee4
Ralf Steube <rsteube@users.noreply.github.com>
2025-05-23 21:47:21 +0200

    added git-unlock (#2813)

---
+++ completers/common/git-unlock_completer/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "git-unlock",
+	Short: "Unlock a file excluded from version control",
+	Long:  "https://github.com/tj/git-extras/blob/master/Commands.md#git-unlock",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.Flags().Bool("help", false, "show help")
+
+	carapace.Gen(rootCmd).PositionalCompletion(
+		carapace.ActionFiles(),
+	)
+}
