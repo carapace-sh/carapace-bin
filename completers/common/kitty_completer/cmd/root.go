2f4ccb07edf7b634f8a5c221c930f7f4c7987a9f
rsteube <rsteube@users.noreply.github.com>
2025-07-20 16:08:08 +0200

    kitty: support workdir

--- completers/common/kitty_completer/cmd/root.go
+++ completers/common/kitty_completer/cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"github.com/carapace-sh/carapace"
 	"github.com/carapace-sh/carapace-bridge/pkg/actions/bridge"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var rootCmd = &cobra.Command{
@@ -17,10 +18,28 @@ var rootCmd = &cobra.Command{
 func Execute() error {
 	return rootCmd.Execute()
 }
+
 func init() {
 	carapace.Gen(rootCmd).Standalone()
 
 	carapace.Gen(rootCmd).PositionalAnyCompletion(
 		bridge.ActionKitten("kitty"),
 	)
+
+	fs := pflag.NewFlagSet("directory", pflag.ContinueOnError)
+	fs.StringP("directory", "s", "", "")
+	fs.String("working-directory", "", "")
+	carapace.Gen(rootCmd).PreRun(func(cmd *cobra.Command, args []string) {
+		fs.Parse(args)
+	})
+
+	carapace.Gen(rootCmd).PreInvoke(func(cmd *cobra.Command, _ *pflag.Flag, action carapace.Action) carapace.Action {
+		if f := fs.Lookup("directory"); f.Changed {
+			return action.Chdir(f.Value.String())
+		}
+		if f := fs.Lookup("working-directory"); f.Changed {
+			return action.Chdir(f.Value.String())
+		}
+		return action
+	})
 }
