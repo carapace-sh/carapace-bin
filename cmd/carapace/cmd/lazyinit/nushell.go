3256b535b2d779df8a0af841d026f895fca3d63a
RangHo Lee <hello@rangho.me>
2025-08-05 00:36:24 +0900

    nushell: modularize Nushell init script

--- cmd/carapace/cmd/lazyinit/nushell.go
+++ cmd/carapace/cmd/lazyinit/nushell.go
@@ -10,33 +10,34 @@ func Nushell(completers []string) string {
 	if runtime.GOOS == "windows" {
 		windowsSnippet = " | str replace --regex  '\\.exe$' ''"
 	}
-	snippet := `%v%v
+	snippet := `export-env {
+  %v
 
-let carapace_completer = {|spans|
-  # if the current command is an alias, get it's expansion
-  let expanded_alias = (scope aliases | where name == $spans.0 | $in.0?.expansion?)
+  let carapace_completer = {|spans|
+    # if the current command is an alias, get it's expansion
+    let expanded_alias = (scope aliases | where name == $spans.0 | $in.0?.expansion?)
 
-  # overwrite
-  let spans = (if $expanded_alias != null  {
-    # put the first word of the expanded alias first in the span
-    $spans | skip 1 | prepend ($expanded_alias | split row " " | take 1%v)
-  } else {
-    $spans | skip 1 | prepend ($spans.0%v)
-  })
+    # overwrite
+    let spans = (if $expanded_alias != null  {
+      # put the first word of the expanded alias first in the span
+      $spans | skip 1 | prepend ($expanded_alias | split row " " | take 1%v)
+    } else {
+      $spans | skip 1 | prepend ($spans.0%v)
+    })
 
-  carapace $spans.0 nushell ...$spans
-  | from json
-}
+    carapace $spans.0 nushell ...$spans
+    | from json
+  }
 
-mut current = (($env | default {} config).config | default {} completions)
-$current.completions = ($current.completions | default {} external)
-$current.completions.external = ($current.completions.external
-| default true enable
-# backwards compatible workaround for default, see nushell #15654
-| upsert completer { if $in == null { $carapace_completer } else { $in } })
+  mut current = (($env | default {} config).config | default {} completions)
+  $current.completions = ($current.completions | default {} external)
+  $current.completions.external = ($current.completions.external
+  | default true enable
+  # backwards compatible workaround for default, see nushell #15654
+  | upsert completer { if $in == null { $carapace_completer } else { $in } })
 
-$env.config = $current
-    `
+  $env.config = $current
+}%v`
 
-	return fmt.Sprintf(snippet, pathSnippet("nushell"), envSnippet("nushell"), windowsSnippet, windowsSnippet)
+	return fmt.Sprintf(snippet, pathSnippet("nushell"), windowsSnippet, windowsSnippet, envSnippet("nushell"))
 }
