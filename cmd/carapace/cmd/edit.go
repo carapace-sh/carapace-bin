6322a89ee7f5e50262b8b9d59db341777f6ea1d9
rsteube <rsteube@users.noreply.github.com>
2025-08-04 23:47:29 +0200

    edit: add header for new specs

--- cmd/carapace/cmd/edit.go
+++ cmd/carapace/cmd/edit.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"time"
 
 	"github.com/carapace-sh/carapace"
 	shlex "github.com/carapace-sh/carapace-shlex"
@@ -27,6 +28,31 @@ var editCmd = &cobra.Command{
 		e, err := editor()
 		if err != nil {
 			return err
+		}
+
+		for _, arg := range args {
+			switch filepath.Dir(filepath.Clean(arg)) {
+			case "overlays", "specs":
+				if filepath.Ext(arg) != ".yaml" {
+					return errors.New("specs should have `.yaml` extension")
+				}
+			default: // not a spec directory
+				continue
+			}
+
+			f := filepath.Join(dir, arg)
+			if _, err := os.Stat(f); !os.IsNotExist(err) {
+				continue // already exists
+			}
+			if err := os.WriteFile(f, []byte("# yaml-language-server: $schema=https://carapace.sh/schemas/command.json\n"), 0644); err != nil {
+				return err
+			}
+			t := time.Now()
+			defer func(f string, t time.Time) {
+				if info, err := os.Stat(f); err == nil && !info.ModTime().After(t) {
+					os.Remove(f) //  not modified
+				}
+			}(f, t)
 		}
 
 		args = append(e, args...)
