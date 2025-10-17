eb28b905490fecc16c204de185ad8ad14f39ad18
rsteube <rsteube@users.noreply.github.com>
2025-10-17 17:33:54 +0200

    added shell actions

---
+++ pkg/actions/shell/shell.go
@@ -0,0 +1,44 @@
+package shell
+
+import (
+	"os"
+	"slices"
+	"strings"
+
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace/pkg/style"
+)
+
+// TODO support more shells
+
+// ActionBuiltins completes shell builtins
+func ActionBuiltins() carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		action := carapace.ActionValues(strings.Fields(os.Getenv("CARAPACE_SHELL_BUILTINS"))...)
+		switch os.Getenv("CARAPACE_SHELL") {
+		case "bash":
+			action = action.Filter(".", ":", "[")
+		case "fish":
+			action = action.Filter("-", ".", ":", "[", "_")
+		case "zsh":
+			action = action.Filter("-", ".", ":", "[")
+		}
+		return action
+	}).Tag("shell builtins")
+}
+
+// ActionFunctions completes shell functions
+func ActionFunctions(hidden bool) carapace.Action {
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		vals := strings.Fields(os.Getenv("CARAPACE_SHELL_FUNCTIONS"))
+		if !hidden && !strings.HasPrefix(c.Value, "_") {
+			vals = slices.DeleteFunc(vals, func(s string) bool { return strings.HasPrefix(s, "_") })
+		}
+		return carapace.ActionValues(vals...).StyleF(func(s string, sc style.Context) string {
+			if strings.HasPrefix(s, "_") {
+				return style.Dim
+			}
+			return style.Default
+		})
+	}).Tag("shell functions")
+}
