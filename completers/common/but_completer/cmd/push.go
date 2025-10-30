f467d05d1cb07ea0b4efab20d3b52f727a63dd85
rsteube <rsteube@users.noreply.github.com>
2025-10-30 17:55:31 +0100

    but: more completions

--- completers/common/but_completer/cmd/push.go
+++ completers/common/but_completer/cmd/push.go
@@ -2,11 +2,12 @@ package cmd
 
 import (
 	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bin/pkg/actions/tools/but"
 	"github.com/spf13/cobra"
 )
 
 var pushCmd = &cobra.Command{
-	Use:   "push",
+	Use:   "push BRANCH_ID",
 	Short: "Push a branch/stack to remote",
 	Run:   func(cmd *cobra.Command, args []string) {},
 }
@@ -19,4 +20,8 @@ func init() {
 	pushCmd.Flags().BoolP("skip-force-push-protection", "s", false, "Skip force push protection checks")
 	pushCmd.Flags().BoolP("with-force", "f", false, "Force push even if it's not fast-forward")
 	rootCmd.AddCommand(pushCmd)
+
+	carapace.Gen(pushCmd).PositionalCompletion(
+		but.ActionLocalBranches(),
+	)
 }
