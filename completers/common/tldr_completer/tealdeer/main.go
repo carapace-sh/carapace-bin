34db3886e0c2bd363d1be684745cfa0541b111bd
rsteube <rsteube@users.noreply.github.com>
2025-12-05 18:39:49 +0100

    tmp

--- completers/common/tldr_completer/tealdeer/main.go
+++ completers/common/tldr_completer/tealdeer/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/carapace-sh/carapace-bin/completers/common/tldr.tealdeer_completer/cmd"
+import "github.com/carapace-sh/carapace-bin/completers/common/tldr_completer/tealdeer/cmd"
 
 func main() {
 	cmd.Execute()
