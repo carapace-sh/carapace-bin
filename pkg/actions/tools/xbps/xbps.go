5e9eabed620840de710094b75c758ddc8d164fc8
Fabian Gietzen <fabian.gietzen@web.de>
2025-12-22 16:38:27 +0100

    Add xbps-install completer

---
+++ pkg/actions/tools/xbps/xbps.go
@@ -0,0 +1,30 @@
+// package xbps contains Advanced Package Tool related actions
+package xbps
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/carapace-sh/carapace"
+)
+
+// ActionPackageSearch completes installable packages
+func ActionPackageSearch() carapace.Action {
+	re := regexp.MustCompile(`\[.\] (\S+)-\S+\s+(.*)`)
+
+	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
+		return carapace.ActionExecCommand("xbps-query", "-Rs", "--regex", "^"+c.Value)(func(output []byte) carapace.Action {
+			lines := strings.Split(string(output), "\n")
+			vals := make([]string, 0)
+
+			for _, line := range lines {
+				match := re.FindStringSubmatch(line)
+
+				if len(match) > 1 {
+					vals = append(vals, match[1], match[2])
+				}
+			}
+			return carapace.ActionValuesDescribed(vals...)
+		})
+	})
+}
