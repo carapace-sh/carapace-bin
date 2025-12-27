bf68e5131248d1e6e04ed95c87a433bc37989a1c
rsteube <rsteube@users.noreply.github.com>
2025-12-27 16:24:53 +0100

    doc: fix completers description

--- .github/completers.sh
+++ .github/completers.sh
@@ -4,4 +4,4 @@ set -e
 echo "# Completers"
 echo
 
-carapace --list | jq --raw-output  '.[][] | select(.group != "bridge" and .group != "user" and .group != "system") | "- [\(.name)\(if .variant then "/\(.variant)" else "" end)\(if .group != "common" then "@\(.group)" else "" end)](\(.url))"'
+carapace --list | jq --raw-output  '.[][] | select(.group != "bridge" and .group != "user" and .group != "system") | "- [\(.name)\(if .variant then "/\(.variant)" else "" end)\(if .group != "common" then "@\(.group)" else "" end)](\(.url)) \(.description)"'
