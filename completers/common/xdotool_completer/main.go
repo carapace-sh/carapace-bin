6a09d460eea8a2be4a45b6af1077b5b2b2c0050f
rsteube <rsteube@users.noreply.github.com>
2025-12-28 00:23:08 +0100

    moved completers

--- completers/common/xdotool_completer/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/carapace-sh/carapace-bin/completers/common/xdotool_completer/cmd"
-
-func main() {
-	cmd.Execute()
-}
---
+++ completers/linux/gftp_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/carapace-sh/carapace-bin/completers/linux/gftp_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
