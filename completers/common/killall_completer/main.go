aa279f06141db843cf9931a08c10fe6491d29198
rsteube <rsteube@users.noreply.github.com>
2025-12-31 17:41:39 +0100

    moved completers

--- completers/common/killall_completer/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/carapace-sh/carapace-bin/completers/common/killall_completer/cmd"
-
-func main() {
-	cmd.Execute()
-}
---
+++ completers/linux/groupmod_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/carapace-sh/carapace-bin/completers/linux/groupmod_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
