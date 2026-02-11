e0e27c3bd68bdc314d4d291f632fc1f27527d859
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-10 18:56:10 -0600

    Added `chezmoi`

---
+++ completers/common/chezmoi_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bridge/pkg/actions/bridge"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:                "chezmoi",
+	Short:              "Manage your dotfiles across multiple diverse machines, securely",
+	Long:               "https://chezmoi.io/",
+	Run:                func(cmd *cobra.Command, args []string) {},
+	DisableFlagParsing: true,
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		bridge.ActionCobra("chezmoi"),
+	)
+}
