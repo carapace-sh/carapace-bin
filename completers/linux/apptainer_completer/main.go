463d4b3c105110a035495e79e76a8c37e4b6019c
rsteube <rsteube@users.noreply.github.com>
2026-02-12 10:55:09 +0100

    moved completes to known bridges

--- completers/linux/apptainer_completer/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/carapace-sh/carapace-bin/completers/linux/apptainer_completer/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
--- completers/linux/toolbox_completer/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/carapace-sh/carapace-bin/completers/linux/toolbox_completer/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
