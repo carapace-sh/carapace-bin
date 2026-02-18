672f28beb52d28584b2b5b488f9662f467bd8fc0
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-18 12:21:48 -0600

    Added the other jetbrains IDEs as aliases to intellij idea

---
+++ completers/common/dataspell_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/carapace-sh/carapace-bin/completers/common/dataspell_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ completers/common/phpstorm_completer/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/carapace-sh/carapace-bin/completers/common/phpstorm_completer/cmd"
+
+func main() {
+	cmd.Execute()
+}
