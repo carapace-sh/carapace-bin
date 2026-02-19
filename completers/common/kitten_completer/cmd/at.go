8df9312cd8fab0a05287041b47f6101e0c297096
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2026-02-19 14:01:35 +0100

    feat: wip desktop-ui subcmd

--- completers/common/kitten_completer/cmd/at.go
+++ completers/common/kitten_completer/cmd/at.go
@@ -11,6 +11,30 @@ var atCmd = &cobra.Command{
 	Short: "Control kitty remotely",
 }
 
+func init() {
+	rootCmd.AddCommand(atCmd)
+	carapace.Gen(atCmd).Standalone()
+
+	atCmd.Flags().String("to", "", "An address for the kitty instance to control. Corresponds to the address given to the kitty instance via the --listen-on option or the listen_on setting in kitty.conf. If not specified, the environment variable KITTY_LISTEN_ON is checked. If that is also not found, messages are sent to the controlling terminal for this process, i.e. they will only work if this process is run within a kitty window.")
+	atCmd.Flags().String("password", "", "A password to use when contacting kitty. This will cause kitty to ask the user for permission to perform the specified action, unless the password has been accepted before or is pre-configured in kitty.conf. To use a blank password specify --use-password as always.")
+	atCmd.Flags().String("password-file", "", "A password to use when contacting kitty. This will cause kitty to ask the user for permission to perform the specified action, unless the password has been accepted before or is pre-configured in kitty.conf. To use a blank password specify --use-password as always.")
+	atCmd.Flags().String("password-env", "", "The name of an environment variable to read the password from. Used if no --password-file is supplied. Defaults to checking the environment variable KITTY_RC_PASSWORD.")
+	atCmd.Flags().String("use-password", "", "If no password is available, kitty will usually just send the remote control command without a password. This option can be used to force it to always or never use the supplied password. If set to always and no password is provided, the blank password is used.")
+	atCmd.Flags().BoolP("help", "h", false, "Show help for this command")
+
+	atCmd.MarkFlagsMutuallyExclusive("password-file", "password-env")
+
+	carapace.Gen(atCmd).FlagCompletion(carapace.ActionMap{
+		"password-file": carapace.Batch(
+			carapace.ActionFiles(),
+			carapace.ActionValuesDescribed("-", "Read from STDIN"),
+			carapace.ActionValuesDescribed("fd:", "Read from a file descriptor").NoSpace(':'),
+		).ToA(),
+		"password-env": os.ActionEnvironmentVariables(),
+		"use-password": carapace.ActionValues("if-available", "always", "never"),
+	})
+}
+
 // TODO: subcommands
 // action:Run the specified mappable action
 // close-tab:Close the specified tabs
@@ -51,27 +75,3 @@ var atCmd = &cobra.Command{
 // set-window-logo:Set the window logo
 // set-window-title:Set the window title
 // signal-child:Send a signal to the foreground process in the specified windows
-
-func init() {
-	rootCmd.AddCommand(atCmd)
-	carapace.Gen(atCmd).Standalone()
-
-	atCmd.Flags().String("to", "", "An address for the kitty instance to control. Corresponds to the address given to the kitty instance via the --listen-on option or the listen_on setting in kitty.conf. If not specified, the environment variable KITTY_LISTEN_ON is checked. If that is also not found, messages are sent to the controlling terminal for this process, i.e. they will only work if this process is run within a kitty window.")
-	atCmd.Flags().String("password", "", "A password to use when contacting kitty. This will cause kitty to ask the user for permission to perform the specified action, unless the password has been accepted before or is pre-configured in kitty.conf. To use a blank password specify --use-password as always.")
-	atCmd.Flags().String("password-file", "", "A password to use when contacting kitty. This will cause kitty to ask the user for permission to perform the specified action, unless the password has been accepted before or is pre-configured in kitty.conf. To use a blank password specify --use-password as always.")
-	atCmd.Flags().String("password-env", "", "The name of an environment variable to read the password from. Used if no --password-file is supplied. Defaults to checking the environment variable KITTY_RC_PASSWORD.")
-	atCmd.Flags().String("use-password", "", "If no password is available, kitty will usually just send the remote control command without a password. This option can be used to force it to always or never use the supplied password. If set to always and no password is provided, the blank password is used.")
-	atCmd.Flags().BoolP("help", "h", false, "Show help for this command")
-
-	atCmd.MarkFlagsMutuallyExclusive("password-file", "password-env")
-
-	carapace.Gen(atCmd).FlagCompletion(carapace.ActionMap{
-		"password-file": carapace.Batch(
-			carapace.ActionFiles(),
-			carapace.ActionValuesDescribed("-", "Read from STDIN"),
-			carapace.ActionValuesDescribed("fd:", "Read from a file descriptor").NoSpace(':'),
-		).ToA(),
-		"password-env": os.ActionEnvironmentVariables(),
-		"use-password": carapace.ActionValues("if-available", "always", "never"),
-	})
-}
---
+++ completers/common/kitten_completer/cmd/desktopUi.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bin/pkg/actions/os"
+	"github.com/spf13/cobra"
+)
+
+var desktopUiCmd = &cobra.Command{
+	Use:   "desktop-ui",
+	Short: "Implement various desktop components for use with lightweight compositors/window managers on Linux",
+}
+
+func init() {
+	rootCmd.AddCommand(desktopUiCmd)
+	carapace.Gen(desktopUiCmd).Standalone()
+
+	desktopUiCmd.Flags().BoolP("help", "h", false, "Show help for this command")
+}
+
+// TODO: subcommands
+// Usage: kitten desktop-ui
+
+// Implement various desktop components for use with lightweight compositors/window
+// managers on Linux
+
+// Commands:
+//    run-server
+//     Start the various servers used to integrate with the Linux desktop
+//    enable-portal
+//     This will create or edit the various files needed so that the portal from
+//     this kitten is used by xdg-desktop-portal
+//    set-color-scheme
+//     Change the color scheme
+//    set-accent-color
+//     Change the accent color
+//    set-contrast
+//     Change the contrast. Can be high or normal.
+//    set-setting
+//     Change an arbitrary setting
+//    show-settings
+//     Print the current values of the desktop settings
+
+// Get help for an individual command by running:
+//     kitten desktop-ui command -h
+
+// Options:
+//   --help, -h [=no]
+//     Show help for this command
+
+// kitten desktop-ui 0.45.0 created by Kovid Goyal
