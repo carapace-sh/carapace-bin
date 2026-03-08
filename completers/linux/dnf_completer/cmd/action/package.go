952dc4a17da70b53527dade97f8d1bbf8ac25a01
rsteube <rsteube@users.noreply.github.com>
2026-03-08 22:52:55 +0100

    dnf: added package search

--- completers/linux/dnf_completer/cmd/action/package.go
+++ completers/linux/dnf_completer/cmd/action/package.go
@@ -1,18 +1,36 @@
 package action
 
 import (
+	"strings"
+
 	"github.com/carapace-sh/carapace"
 	"github.com/spf13/cobra"
 )
 
 func ActionPackageSearch(cmd *cobra.Command) carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-		rootCmd := cmd.Root()
+		if len(c.Value) == 0 {
+			return carapace.ActionMessage("package search needs at least one character") // it works but would overwhelm the completion menus
+		}
 
-		_ = rootCmd.Flag("repo")
-		_ = rootCmd.Flag("setopt")
-		_ = rootCmd.Flag("installroot")
+		args := []string{"--quiet", "--cacheonly"}
+		for _, name := range []string{"repo", "setopt", "installroot"} {
+			if f := cmd.Root().Flag(name); f.Changed {
+				args = append(args, "--"+f.Name, f.Value.String())
+			}
+		}
+		args = append(args, "list", "--available", c.Value+"*")
 
-		return carapace.ActionValues() // TODO: implement dnf5 package search
+		return carapace.ActionExecCommand("dnf", args...)(func(output []byte) carapace.Action {
+			lines := strings.Split(string(output), "\n")
+			vals := make([]string, 0)
+
+			for _, line := range lines {
+				if fields := strings.Fields(line); len(fields) == 3 {
+					vals = append(vals, fields[0], fields[2])
+				}
+			}
+			return carapace.ActionValuesDescribed(vals...)
+		})
 	})
 }
