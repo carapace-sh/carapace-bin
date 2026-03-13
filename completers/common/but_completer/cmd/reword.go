75d6276f6fd9978312e1312f3538a6cba5bb459a
rsteube <rsteube@users.noreply.github.com>
2026-03-13 10:41:23 +0100

    but: updates from 0.19.5

--- completers/common/but_completer/cmd/reword.go
+++ completers/common/but_completer/cmd/reword.go
@@ -15,9 +15,11 @@ var rewordCmd = &cobra.Command{
 func init() {
 	carapace.Gen(rewordCmd).Standalone()
 
+	rewordCmd.Flags().Bool("diff", false, "Always show diff inside the editor")
 	rewordCmd.Flags().BoolP("format", "f", false, "Format the existing commit message to 72-char line wrapping without opening an editor")
 	rewordCmd.Flags().BoolP("help", "h", false, "Print help (see more with '--help')")
 	rewordCmd.Flags().StringP("message", "m", "", "The new commit message or branch name. If not provided, opens an editor")
+	rewordCmd.Flags().Bool("no-diff", false, "Never show the diff inside the editor")
 	rootCmd.AddCommand(rewordCmd)
 
 	carapace.Gen(rewordCmd).PositionalCompletion(
