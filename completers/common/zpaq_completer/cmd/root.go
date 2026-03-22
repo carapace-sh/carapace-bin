ca48ee0a835a29af400b56317383a10b3016b367
239 <239@pm.me>
2026-03-22 20:22:19 +0100

    add zpaq completer

---
+++ completers/common/zpaq_completer/cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "zpaq",
+	Short: "",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	rootCmd.PersistentFlags().BoolS("f", "f", false, "-force")
+	rootCmd.PersistentFlags().BoolS("force", "force", false, "")
+	rootCmd.PersistentFlags().StringS("key", "key", "", "access encrypted archive with password X")
+	rootCmd.PersistentFlags().BoolS("noattributes", "noattributes", false, "ignore file attributes or permissions")
+	rootCmd.PersistentFlags().StringSliceS("not", "not", nil, "exclude (* and ? match any string or char)")
+	rootCmd.PersistentFlags().StringSliceS("only", "only", nil, "include only matches (default: *)")
+	rootCmd.PersistentFlags().BoolS("s1", "s1", false, "-summary 1")
+	rootCmd.PersistentFlags().BoolS("sN", "sN", false, "-summary N")
+	rootCmd.PersistentFlags().StringS("summary", "summary", "", "")
+	rootCmd.PersistentFlags().BoolS("t1", "t1", false, "-threads 1")
+	rootCmd.PersistentFlags().BoolS("tN", "tN", false, "-threads N")
+	rootCmd.PersistentFlags().StringS("threads", "threads", "", "use N threads (default: 0 = all cores)")
+	rootCmd.PersistentFlags().StringSliceS("to", "to", nil, "rename files... to P... or all to P/all")
+	rootCmd.PersistentFlags().StringS("until", "until", "", "roll back archive to N'th update (-N from end) or set date N")
+}
