4e5ed76a780df329a6635e0ddec9f6072764d3d5
rsteube <rsteube@users.noreply.github.com>
2026-03-28 10:00:09 +0100

    elvish: one-step completer registration

--- cmd/carapace/cmd/lazyinit/elvish.go
+++ cmd/carapace/cmd/lazyinit/elvish.go
@@ -16,9 +16,21 @@ func Elvish(completers []string) string {
 
 put %v | each {|c|
     set edit:completion:arg-completer[$c] = {|@arg|
-        set edit:completion:arg-completer[$c] = {|@arg| }
-        eval (carapace $c elvish | slurp)
-        $edit:completion:arg-completer[$c] $@arg
+        carapace $c elvish (all $arg) | from-json | each {|completion|
+    		put $completion[Messages] | all (one) | each {|m|
+    			edit:notify (styled "error: " red)$m
+    		}
+    		if (not-eq $completion[Usage] "") {
+    			edit:notify (styled "usage: " $completion[DescriptionStyle])$completion[Usage]
+    		}
+    		put $completion[Candidates] | all (one) | peach {|c|
+    			if (eq $c[Description] "") {
+    		    	edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style]) &code-suffix=$c[CodeSuffix]
+    			} else {
+    		    	edit:complex-candidate $c[Value] &display=(styled $c[Display] $c[Style])(styled " " $completion[DescriptionStyle]" bg-default")(styled "("$c[Description]")" $completion[DescriptionStyle]) &code-suffix=$c[CodeSuffix]
+    			}
+    		}
+        }
     }%v
 }
 `
