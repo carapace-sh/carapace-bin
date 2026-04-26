c251edea4dbece93ca844fde9057376cf29886ad
rsteube <rsteube@users.noreply.github.com>
2026-04-26 22:09:35 +0200

    git: ActionRemotes - added description

--- pkg/actions/tools/git/remote.go
+++ pkg/actions/tools/git/remote.go
@@ -8,13 +8,23 @@ import (
 
 // ActionRemotes completes remote names
 //
-//	origin
-//	upstream
+//	origin (git@github.com:some/origin.git)
+//	upstream (git@github.com:some/upstream.git)
 func ActionRemotes() carapace.Action {
-	return carapace.ActionExecCommand("git", "remote")(func(output []byte) carapace.Action {
+	return carapace.ActionExecCommand("git", "remote", "--verbose")(func(output []byte) carapace.Action {
 		lines := strings.Split(string(output), "\n")
-		return carapace.ActionValues(lines[:len(lines)-1]...)
-	})
+
+		vals := make([]string, 0)
+		for _, line := range lines {
+			if !strings.HasSuffix(line, " (push)") {
+				continue
+			}
+			if remote, url, ok := strings.Cut(line, "\t"); ok {
+				vals = append(vals, remote, strings.TrimSuffix(url, " (push)"))
+			}
+		}
+		return carapace.ActionValuesDescribed(vals...)
+	}).Tag("remotes").UidF(Uid("remote"))
 }
 
 // ActionRemoteUrls completes remote urls
