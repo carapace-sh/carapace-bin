52ae25f7a63590cfc79f4bcc0cc20744f5ac6f8d
ahmadmustafaanis <ahmadanis5050@gmail.com>
2026-05-13 01:18:18 +0500

    Add talosctl completer

---
+++ completers/common/talosctl_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bridge/pkg/actions/bridge"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:                "talosctl",
+	Short:              "A CLI for out-of-band management of Kubernetes nodes created by Talos",
+	Long:               "https://docs.siderolabs.com/talos/latest/learn-more/talosctl",
+	Run:                func(cmd *cobra.Command, args []string) {},
+	DisableFlagParsing: true,
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		bridge.ActionCobra("talosctl"),
+	)
+}
