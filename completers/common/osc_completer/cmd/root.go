2e2c4eabb40c2b9e0a056eacadaf416d5328e754
ahmadmustafaanis <ahmadanis5050@gmail.com>
2026-05-13 04:01:03 +0500

    Add osc clap bridge completer

---
+++ completers/common/osc_completer/cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bridge/pkg/actions/bridge"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:                "osc",
+	Short:              "OpenStack client rewritten in Rust",
+	Long:               "https://github.com/gtema/openstack",
+	Run:                func(cmd *cobra.Command, args []string) {},
+	DisableFlagParsing: true,
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		bridge.ActionClap("osc"),
+	)
+}
