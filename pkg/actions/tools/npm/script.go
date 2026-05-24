00ad205f6805bdc95648697e3cb35e01e43a1235
rsteube <rsteube@users.noreply.github.com>
2026-05-25 01:06:21 +0200

    bun: run - added script completion

--- pkg/actions/tools/npm/script.go
+++ pkg/actions/tools/npm/script.go
@@ -3,16 +3,19 @@ package npm
 import "github.com/carapace-sh/carapace"
 
 // ActionScripts completes scripts
+//
+//	test (bun test --isolate)
+//	test:coverage (bun test --isolate --coverage)
 func ActionScripts() carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
 		if pj, err := loadPackageJson(c); err != nil {
 			return carapace.ActionMessage(err.Error())
 		} else {
 			vals := make([]string, 0)
-			for name := range pj.Scripts {
-				vals = append(vals, name)
+			for name, command := range pj.Scripts {
+				vals = append(vals, name, command)
 			}
-			return carapace.ActionValues(vals...)
+			return carapace.ActionValuesDescribed(vals...)
 		}
-	})
+	}).Tag("scripts")
 }
