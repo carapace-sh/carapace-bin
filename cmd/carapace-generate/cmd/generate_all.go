4a0144131ce3cd132e9a17041199e4c673fcbbab
rsteube <rsteube@users.noreply.github.com>
2026-06-02 22:14:13 +0200

    add generate-all subcommand to scan completers directory once
    Replace 16 individual `completers` invocations (each re-scanning the
    same directory tree) with a single `generate-all` command that scans
    once and produces all 16 output files.
    
    Assisted-by: Crush:glm-5.1

---
+++ cmd/carapace-generate/cmd/generate_all.go
@@ -0,0 +1,108 @@
+package cmd
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/carapace-sh/carapace"
+	genCompleter "github.com/carapace-sh/carapace-bin/cmd/carapace-generate/pkg/completer"
+	"github.com/carapace-sh/carapace-bin/pkg/completer"
+	"github.com/carapace-sh/carapace-bridge/pkg/choices"
+	"github.com/spf13/cobra"
+)
+
+var generateAllCmd = &cobra.Command{
+	Use:   "generate-all DIR",
+	Short: "",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dir := args[0]
+		if err := genCompleter.Optimize(dir); err != nil {
+			return err
+		}
+
+		allCompleters, err := genCompleter.ReadCompleters(dir, "force_all")
+		if err != nil {
+			return err
+		}
+
+		allReleaseCompleters, err := genCompleter.ReadCompleters(dir+"_release", "force_all")
+		if err != nil {
+			return err
+		}
+
+		type target struct {
+			tag     string
+			goos    string
+			release bool
+			output  string
+		}
+
+		targets := []target{
+			{"!release && !force_all &&  android", "android", false, "cmd/completers/completers_termux.go"},
+			{"!release && !force_all && !android", "linux", false, "cmd/completers/completers_linux.go"},
+			{"!release && !force_all", "darwin", false, "cmd/completers/completers_darwin.go"},
+			{"!release && !force_all", "freebsd", false, "cmd/completers/completers_freebsd.go"},
+			{"!release && !force_all", "netbsd", false, "cmd/completers/completers_netbsd.go"},
+			{"!release && !force_all", "openbsd", false, "cmd/completers/completers_openbsd.go"},
+			{"!release && !force_all", "windows", false, "cmd/completers/completers_windows.go"},
+			{"!release &&  force_all", "force_all", false, "cmd/completers/completers_all.go"},
+			{"release && !force_all &&  android", "android", true, "cmd/completers/completers_release_termux.go"},
+			{"release && !force_all && !android", "freebsd", true, "cmd/completers/completers_release_freebsd.go"},
+			{"release && !force_all && !android", "linux", true, "cmd/completers/completers_release_linux.go"},
+			{"release && !force_all && !android", "netbsd", true, "cmd/completers/completers_release_netbsd.go"},
+			{"release && !force_all && !android", "openbsd", true, "cmd/completers/completers_release_openbsd.go"},
+			{"release && !force_all", "darwin", true, "cmd/completers/completers_release_darwin.go"},
+			{"release && !force_all", "windows", true, "cmd/completers/completers_release_windows.go"},
+			{"release &&  force_all", "force_all", true, "cmd/completers/completers_release_all.go"},
+		}
+
+		for _, t := range targets {
+			var base completer.CompleterMap
+			if t.release {
+				base = allReleaseCompleters
+			} else {
+				base = allCompleters
+			}
+
+			filtered := filterByGoos(base, t.goos)
+			s := filtered.Format("completers", t.tag)
+			if err := os.WriteFile(t.output, []byte(s), 0644); err != nil {
+				return err
+			}
+			if err := exec.Command("go", "fmt", t.output).Run(); err != nil {
+				return err
+			}
+		}
+		return nil
+	},
+}
+
+func filterByGoos(all completer.CompleterMap, goos string) completer.CompleterMap {
+	groups := map[string][]string{
+		"android":   {"common", "unix", "linux", "android"},
+		"darwin":    {"common", "unix", "bsd", "darwin"},
+		"freebsd":   {"common", "unix", "bsd", "freebsd"},
+		"linux":     {"common", "unix", "linux"},
+		"netbsd":    {"common", "unix", "bsd", "netbsd"},
+		"openbsd":   {"common", "unix", "bsd", "openbsd"},
+		"windows":   {"common", "windows"},
+		"force_all": {"common", "unix", "linux", "bsd", "darwin", "android", "windows", "freebsd", "netbsd", "openbsd"},
+	}
+
+	filtered := make(completer.CompleterMap)
+	for _, group := range groups[goos] {
+		filtered.Merge(all.Filter(choices.Choice{Group: group}))
+	}
+	return filtered
+}
+
+func init() {
+	carapace.Gen(generateAllCmd).Standalone()
+
+	rootCmd.AddCommand(generateAllCmd)
+
+	carapace.Gen(generateAllCmd).PositionalCompletion(
+		carapace.ActionDirectories(),
+	)
+}
--- cmd/carapace/main.go
+++ cmd/carapace/main.go
@@ -11,23 +11,7 @@ var commit, date string
 var version = "develop"
 
 //go:generate go build -o ../carapace-generate/carapace-generate ../carapace-generate
-//go:generate ../carapace-generate/carapace-generate copy ../../completers
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release && !force_all &&  android" ../../completers android           --output cmd/completers/completers_termux.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release && !force_all && !android" ../../completers linux             --output cmd/completers/completers_linux.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release && !force_all"             ../../completers darwin            --output cmd/completers/completers_darwin.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release && !force_all"             ../../completers freebsd           --output cmd/completers/completers_freebsd.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release && !force_all"             ../../completers netbsd            --output cmd/completers/completers_netbsd.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release && !force_all"             ../../completers openbsd           --output cmd/completers/completers_openbsd.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release && !force_all"             ../../completers windows           --output cmd/completers/completers_windows.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag "!release &&  force_all"             ../../completers force_all         --output cmd/completers/completers_all.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release && !force_all &&  android" ../../completers_release android   --output cmd/completers/completers_release_termux.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release && !force_all && !android" ../../completers_release freebsd   --output cmd/completers/completers_release_freebsd.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release && !force_all && !android" ../../completers_release linux     --output cmd/completers/completers_release_linux.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release && !force_all && !android" ../../completers_release netbsd    --output cmd/completers/completers_release_netbsd.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release && !force_all && !android" ../../completers_release openbsd   --output cmd/completers/completers_release_openbsd.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release && !force_all"             ../../completers_release darwin    --output cmd/completers/completers_release_darwin.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release && !force_all"             ../../completers_release windows   --output cmd/completers/completers_release_windows.go
-//go:generate ../carapace-generate/carapace-generate completers --code --tag  "release &&  force_all"             ../../completers_release force_all --output cmd/completers/completers_release_all.go
+//go:generate ../carapace-generate/carapace-generate generate-all ../../completers
 //go:generate ../carapace-generate/carapace-generate macros --code github.com/carapace-sh/carapace-bin/pkg/actions github.com/carapace-sh/carapace-bridge/pkg/actions --output ../../pkg/actions/actions_generated.go
 //go:generate ../carapace-generate/carapace-generate conditions ../../pkg/conditions --output ../../pkg/conditions/conditions_generated.go
 func main() {
