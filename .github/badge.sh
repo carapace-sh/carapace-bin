5d0316f107077f81de602b95b0b3029e667d7878
rsteube <rsteube@users.noreply.github.com>
2026-06-13 12:05:05 +0200

    added `--all` flag to list builtin/excluded completers

--- .github/badge.sh
+++ .github/badge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-amount=$(carapace --list | jq '[ .[][] | select(.group != "bridge" and .group != "user" and .group != "system") ] | length')
+amount=$(carapace --list --all | jq '[ .[][] | select(.group != "bridge" and .group != "user" and .group != "system") ] | length')
 
 curl "https://img.shields.io/badge/completers-${amount}-orange"
