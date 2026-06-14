3a3da2e5f5183334588d59195c6e378d51d94e22
rsteube <rsteube@users.noreply.github.com>
2026-06-14 12:51:08 +0200

    add elvish builtin completers
    Assisted-by: Crush:deepseek-v4-flash

---
+++ completers/elvish/get-env_completer/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-bin/pkg/actions/os"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "get-env",
+	Short: "Get the value of an environment variable",
+	Long:  "https://elv.sh/ref/builtin.html#get-env",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func init() {
+	carapace.Gen(rootCmd).Standalone()
+
+	carapace.Gen(rootCmd).PositionalAnyCompletion(
+		os.ActionEnvironmentVariables().FilterArgs(),
+	)
+}
