b17ea07dbc970ec828a11e9274be5802c718d085
rsteube <rsteube@users.noreply.github.com>
2026-06-20 20:14:09 +0200

    feat: add CARAPACE_SHELL environment variables to all shell snippets
    Adds CARAPACE_SHELL and related variables (BUILTINS, FUNCTIONS, ALIASES,
    JOBS, VARIABLES) to shell snippets that were missing them. This ensures
    completion functions can identify the shell and access shell-specific
    context for better completion results.
    
    Added to: elvish, oil, powershell, tcsh, xonsh, nushell, cmd-clink
    
    Assisted-by: Crush:minimax-m2.7

--- cmd/carapace/cmd/snippet/tcsh.go
+++ cmd/carapace/cmd/snippet/tcsh.go
@@ -6,10 +6,9 @@ import (
 )
 
 func Tcsh(completers []string) string {
-	// TODO hardcoded for now
 	snippet := make([]string, len(completers))
 	for index, c := range completers {
-		snippet[index] = fmt.Sprintf("complete \"%v\" 'p@*@`echo \"$COMMAND_LINE'\"''\"'\" | xargs carapace %v tcsh `@@' ;", c, c)
+		snippet[index] = fmt.Sprintf("complete \"%v\" 'p@*@`echo \\\"${COMMAND_LINE}\\\"''\\\"'\\\" | xargs env CARAPACE_SHELL=tcsh CARAPACE_SHELL_BUILTINS=\\\"$(compgen -b | tr '\\\\n' ' ')\\\" CARAPACE_SHELL_FUNCTIONS=\\\"$(compgen -A function | tr '\\\\n' ' ')\\\" CARAPACE_SHELL_VARIABLES=\\\"$(compgen -v | tr '\\\\n' ' ')\\\" carapace %v tcsh `@@' ;", c, c)
 	}
 	return strings.Join(snippet, "\n")
 }
