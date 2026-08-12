ae5af6cf459721667d7b03ae78c4277437637b40
rsteube <rsteube@users.noreply.github.com>
2026-08-12 21:57:28 +0200

    go fmt

--- pkg/styles/lore.go
+++ pkg/styles/lore.go
@@ -3,10 +3,10 @@ package styles
 import "github.com/carapace-sh/carapace/pkg/style"
 
 var Lore = struct {
-	Branch       string `description:"lore branches"`
+	Branch        string `description:"lore branches"`
 	CurrentBranch string `description:"lore current branch"`
-	Revision     string `description:"lore revisions"`
-	Identity     string `description:"lore identities"`
+	Revision      string `description:"lore revisions"`
+	Identity      string `description:"lore identities"`
 }{
 	Branch:        style.Blue,
 	CurrentBranch: style.Bold,
