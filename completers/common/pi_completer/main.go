b3cbb57b372b62b99b6334d1914b29340a79a143
rsteube <rsteube@users.noreply.github.com>
2026-08-25 21:23:59 +0200

    pi: simplify main

--- completers/common/pi_completer/main.go
+++ completers/common/pi_completer/main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/carapace-sh/carapace-bin/completers/common/pi_completer/cmd"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
